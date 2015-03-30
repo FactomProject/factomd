175f9d5e8604b60cbdf876efe8a35fc464ad3be9
Michael <michael@mastercoin.org>
2015-03-30 19:06:30 -0400

    marshalling WIP

--- wire/fact-methods.go
+++ wire/fact-methods.go
@@ -17,8 +17,7 @@ import (
 )
 
 const (
-	maxTxOutPerTX = 1 // need some upper limit
-	maxTxInPerTX  = 1 // need some upper limit
+	inNout_cap = 16000 // per spec
 )
 
 // good check to run after deserialization
@@ -33,25 +32,6 @@ func readRCD(r io.Reader, pver uint32, rcd *RCD) error {
 	return nil
 }
 
-/*
-// readOutPoint reads the next sequence of bytes from r as an OutPoint.
-func factom_readOutPoint(r io.Reader, pver uint32, op *OutPoint) error {
-	_, err := io.ReadFull(r, op.txid[:])
-	if err != nil {
-		return err
-	}
-
-	var buf [4]byte
-	_, err = io.ReadFull(r, buf[:])
-	if err != nil {
-		return err
-	}
-	op.idx, _ = readVarInt(r, pver)
-
-	return nil
-}
-*/
-
 // readOutPoint reads the next sequence of bytes from r as an OutPoint.
 func readOutPoint(r io.Reader, pver uint32, op *OutPoint) error {
 	_, err := io.ReadFull(r, op.Hash[:])
@@ -64,24 +44,34 @@ func readOutPoint(r io.Reader, pver uint32, op *OutPoint) error {
 	if err != nil {
 		return err
 	}
-	op.Index = binary.LittleEndian.Uint32(buf[:])
-	return nil
+
+	// op.Index = binary.LittleEndian.Uint32(buf[:])
+
+	// varint on the wire, but easily fits into uint32
+	index, err := readVarInt(r, pver)
+
+	if inNout_cap < index {
+		return fmt.Errorf("OutPoint trouble, index too large: %d", index)
+	}
+
+	op.Index = uint32(index)
+
+	//	return nil
+	return err
 }
 
-// writeOutPoint encodes op to the bitcoin protocol encoding for an OutPoint
-// to w.
+// writeOutPoint encodes op to the protocol encoding for an OutPoint to w.
 func writeOutPoint(w io.Writer, pver uint32, op *OutPoint) error {
 	_, err := w.Write(op.Hash[:])
 	if err != nil {
 		return err
 	}
 
-	var buf [4]byte
-	binary.LittleEndian.PutUint32(buf[:], op.Index)
-	_, err = w.Write(buf[:])
+	err = writeVarInt(w, pver, uint64(op.Index))
 	if err != nil {
 		return err
 	}
+
 	return nil
 }
 
@@ -141,7 +131,7 @@ func readECOut(r io.Reader, pver uint32, eco *TxEntryCreditOut) error {
 	return nil
 }
 
-// BtcDecode decodes r using the bitcoin protocol encoding into the receiver.
+// BtcDecode decodes r using the protocol encoding into the receiver.
 // This is part of the Message interface implementation.
 // See Deserialize for decoding transactions stored to disk, such as in a
 // database, as opposed to decoding transactions from the wire.
@@ -173,10 +163,10 @@ func (msg *MsgTx) BtcDecode(r io.Reader, pver uint32) error {
 	// Prevent more input transactions than could possibly fit into a
 	// message.  It would be possible to cause memory exhaustion and panics
 	// without a sane upper bound on this count.
-	if outcount > uint64(maxTxOutPerTX) {
+	if outcount > uint64(inNout_cap) {
 		str := fmt.Sprintf("too many input transactions to fit into "+
 			"max message size [count %d, max %d]", outcount,
-			maxTxOutPerTX)
+			inNout_cap)
 		return messageError("MsgTx.BtcDecode maxtxout", str)
 	}
 
@@ -198,20 +188,20 @@ func (msg *MsgTx) BtcDecode(r io.Reader, pver uint32) error {
 	// Prevent more input transactions than could possibly fit into a
 	// message.  It would be possible to cause memory exhaustion and panics
 	// without a sane upper bound on this count.
-	if eccount > uint64(maxTxOutPerTX) {
+	if eccount > uint64(inNout_cap) {
 		str := fmt.Sprintf("too many input transactions to fit into "+
 			"max message size [count %d, max %d]", eccount,
-			maxTxOutPerTX)
+			inNout_cap)
 		return messageError("MsgTx.BtcDecode maxtxout", str)
 	}
 
 	// Prevent more input transactions than could possibly fit into a
 	// message.  It would be possible to cause memory exhaustion and panics
 	// without a sane upper bound on this count.
-	if eccount > uint64(maxTxInPerTX) {
+	if eccount > uint64(inNout_cap) {
 		str := fmt.Sprintf("too many input transactions to fit into "+
 			"max message size [count %d, max %d]", eccount,
-			maxTxInPerTX)
+			inNout_cap)
 		return messageError("MsgTx.BtcDecode maxtxout", str)
 	}
 
@@ -244,10 +234,10 @@ func (msg *MsgTx) BtcDecode(r io.Reader, pver uint32) error {
 
 	rcdcount, err := readVarInt(r, pver)
 
-	if rcdcount > uint64(maxTxInPerTX) {
+	if rcdcount > uint64(inNout_cap) {
 		str := fmt.Sprintf("too many RCDs to fit into "+
 			"max message size [count %d, max %d]", rcdcount,
-			maxTxInPerTX)
+			inNout_cap)
 		return messageError("MsgTx.BtcDecode max rcd", str)
 	}
 
@@ -269,33 +259,50 @@ func (msg *MsgTx) BtcDecode(r io.Reader, pver uint32) error {
 	return nil
 }
 
-// FactoidEncode encodes the receiver to w using the bitcoin protocol encoding.
+// FactoidEncode encodes the receiver to w using the protocol encoding.
 // This is part of the Message interface implementation.
 // See Serialize for encoding transactions to be stored to disk, such as in a
 // database, as opposed to encoding transactions for the wire.
 func (msg *MsgTx) BtcEncode(w io.Writer, pver uint32) error {
-	util.Trace(" NOT IMPLEMENTED !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+
+	util.Trace(" WIP !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+
+	var buf [1]byte
+
+	buf[0] = msg.Version
+
+	_, err := w.Write(buf[:])
+	if err != nil {
+		return err
+	}
+
+	var buf8 [8]byte
+	binary.LittleEndian.PutUint64(buf8[:], uint64(msg.LockTime)) // FIXME: must do 5 bytes here
+	_, err = w.Write(buf8[:])
+	if err != nil {
+		return err
+	}
+
+	txoutcount := uint64(len(msg.TxOut))
+	err = writeVarInt(w, pver, txoutcount)
+	if err != nil {
+		return err
+	}
+
+	for _, to := range msg.TxOut {
+		err = writeTxOut(w, pver, to)
+		if err != nil {
+			return err
+		}
+	}
+
+	ecoutcount := uint64(len(msg.ECOut))
+	err = writeVarInt(w, pver, ecoutcount)
+	if err != nil {
+		return err
+	}
+
 	/*
-		var buf [4]byte
-		binary.BigEndian.PutUint32(buf[:], uint32(msg.Version))
-		_, err := w.Write(buf[:])
-		if err != nil {
-			return err
-		}
-
-		count := uint64(len(msg.TxIn))
-		err = writeVarInt(w, pver, count)
-		if err != nil {
-			return err
-		}
-
-		for _, ti := range msg.TxIn {
-			err = writeTxIn(w, pver, ti)
-			if err != nil {
-				return err
-			}
-		}
-
 		count = uint64(len(msg.TxOut))
 		err = writeVarInt(w, pver, count)
 		if err != nil {
@@ -319,7 +326,7 @@ func (msg *MsgTx) BtcEncode(w io.Writer, pver uint32) error {
 	return nil
 }
 
-// writeTxIn encodes ti to the bitcoin protocol encoding for a transaction
+// writeTxIn encodes ti to the protocol encoding for a transaction
 // input (TxIn) to w.
 func writeTxIn(w io.Writer, pver uint32, ti *TxIn) error {
 	util.Trace(" NOT IMPLEMENTED !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
@@ -346,7 +353,7 @@ func writeTxIn(w io.Writer, pver uint32, ti *TxIn) error {
 	return nil
 }
 
-// writeTxOut encodes to into the bitcoin protocol encoding for a transaction
+// writeTxOut encodes to into the protocol encoding for a transaction
 // output (TxOut) to w.
 func writeTxOut(w io.Writer, pver uint32, to *TxOut) error {
 	util.Trace(" NOT IMPLEMENTED !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
