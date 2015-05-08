f69eb6f6db64cffc733de5d2e49914397c798979
michaelbeam <michaeljbeam@gmail.com>
2015-05-08 11:59:54 -0500

    msgCommitEntry

--- wire/msgcommitentry.go
+++ wire/msgcommitentry.go
@@ -7,53 +7,56 @@ package wire
 import (
 	"bytes"
 	"encoding/binary"
-	"fmt"
 	"github.com/FactomProject/FactomCode/common"
 	"github.com/agl/ed25519"
 	"io"
 )
 
 // MsgCommitEntry implements the Message interface and represents a factom
-// Commit-Entry message.  It is used by client to commit the entry before revealing it.
+// Commit-Entry message.  It is used by client to commit the entry before
+// revealing it.
 type MsgCommitEntry struct {
-	ECPubKey  *common.Hash
+	Version   int8
+	MilliTime *[6]byte
 	EntryHash *common.Hash
-	Credits   uint32
-	Timestamp uint64
-	Sig       []byte
+	Credits   uint8
+	ECPubKey  *[32]byte
+	Sig       *[64]byte
 }
 
 // BtcEncode encodes the receiver to w using the bitcoin protocol encoding.
 // This is part of the Message interface implementation.
 func (msg *MsgCommitEntry) BtcEncode(w io.Writer, pver uint32) error {
+	// Version
+	if err := writeElement(w, &msg.Version); err != nil {
+		return err
+	}
 
-	//ECPubKey
-	err := writeVarBytes(w, uint32(common.HASH_LENGTH), msg.ECPubKey.Bytes)
-	if err != nil {
+	//MilliTime
+	if err := writeVarBytes(w, pver, msg.MilliTime[:]); err != nil {
 		return err
 	}
 
 	//EntryHash
-	err = writeVarBytes(w, uint32(common.HASH_LENGTH), msg.EntryHash.Bytes)
-	if err != nil {
+	if err := writeVarBytes(w, uint32(common.HASH_LENGTH), msg.EntryHash.Bytes);
+		err != nil {
 		return err
 	}
 
 	//Credits
-	err = writeElement(w, &msg.Credits) // change it to varint??
-	if err != nil {
+	if err := writeElement(w, &msg.Credits); err != nil {
 		return err
 	}
 
-	//Timestamp
-	err = writeVarInt(w, pver, msg.Timestamp)
-	if err != nil {
+	//ECPubKey
+	if err := writeVarBytes(w, uint32(ed25519.PublicKeySize), msg.ECPubKey[:]);
+		err != nil {
 		return err
 	}
 
 	//Signature
-	err = writeVarBytes(w, uint32(ed25519.SignatureSize), msg.Sig)
-	if err != nil {
+	if err := writeVarBytes(w, uint32(ed25519.SignatureSize), msg.Sig[:]);
+		err != nil {
 		return err
 	}
 
@@ -63,39 +66,48 @@ func (msg *MsgCommitEntry) BtcEncode(w io.Writer, pver uint32) error {
 // BtcDecode decodes r using the bitcoin protocol encoding into the receiver.
 // This is part of the Message interface implementation.
 func (msg *MsgCommitEntry) BtcDecode(r io.Reader, pver uint32) error {
-	//ECPubKey
-	bytes, err := readVarBytes(r, pver, uint32(common.HASH_LENGTH), CmdCommitEntry)
-	if err != nil {
+	// Version
+	if err := readElement(r, &msg.Version); err != nil {
 		return err
 	}
 
-	msg.ECPubKey = new(common.Hash)
-	msg.ECPubKey.SetBytes(bytes)
-
-	//EntryHash
-	bytes, err = readVarBytes(r, pver, uint32(common.HASH_LENGTH), CmdCommitEntry)
-	if err != nil {
+	// MilliTime
+	if bytes, err := readVarBytes(r, pver, uint32(6), CmdCommitEntry);
+		err != nil {
 		return err
-	}
-	msg.EntryHash = new(common.Hash)
-	msg.EntryHash.SetBytes(bytes)
-
-	//Credits
-	err = readElement(r, &msg.Credits) // change it to varint??
-	if err != nil {
-		return err
+	} else {
+		copy(msg.MilliTime[:], bytes)
 	}
 
-	//Timestamp
-	msg.Timestamp, err = readVarInt(r, pver)
-	if err != nil {
+	// EntryHash
+	if bytes, err := readVarBytes(r, pver, uint32(common.HASH_LENGTH),
+		CmdCommitEntry); err != nil {
+		return err
+	} else {
+		copy(msg.EntryHash.Bytes, bytes[:32])
+	}
+
+	// Credits
+	if err := readElement(r, &msg.Credits); err != nil {
 		return err
 	}
 
-	//Signature
-	msg.Sig, err = readVarBytes(r, pver, uint32(ed25519.SignatureSize), CmdCommitEntry)
-	if err != nil {
+	// ECPubKey
+	if bytes, err := readVarBytes(r, pver, uint32(ed25519.PublicKeySize),
+		CmdCommitEntry); err != nil {
 		return err
+	} else {
+		msg.ECPubKey = new([32]byte)
+		copy(msg.ECPubKey[:], bytes)
+	}
+
+	// Signature
+	if bytes, err := readVarBytes(r, pver, uint32(ed25519.SignatureSize),
+		CmdCommitEntry); err != nil {
+		return err
+	} else {
+		msg.Sig = new([64]byte)
+		copy(msg.Sig[:], bytes)
 	}
 
 	return nil
@@ -113,26 +125,30 @@ func (msg *MsgCommitEntry) MaxPayloadLength(pver uint32) uint32 {
 	return MaxAppMsgPayload
 }
 
-// NewMsgInv returns a new bitcoin inv message that conforms to the Message
-// interface.  See MsgInv for details.
+// NewMsgCommitEntry returns a new bitcoin Commit Entry message that conforms to
+// the Message interface.
 func NewMsgCommitEntry() *MsgCommitEntry {
-	return &MsgCommitEntry{}
+	m := new(MsgCommitEntry)
+	m.MilliTime = new([6]byte)
+	m.EntryHash = new(common.Hash)
+	m.EntryHash.Bytes = make([]byte, 32)
+	m.ECPubKey = new([32]byte)
+	m.Sig = new([64]byte)
+	
+	return m
 }
 
 // Check whether the msg can pass the message level validations
 // such as timestamp, signiture and etc
 func (msg *MsgCommitEntry) IsValid() bool {
-	//Verify signature (timestamp + entry hash + credits)
-	var buf bytes.Buffer
-	binary.Write(&buf, binary.BigEndian, msg.Timestamp)
+	// Verify signature (Version + MilliTime + EntryHash + Credits)
+	buf := new(bytes.Buffer)
+	binary.Write(buf, binary.BigEndian, msg.Version)
+	buf.Write(msg.MilliTime[:])
 	buf.Write(msg.EntryHash.Bytes)
-	binary.Write(&buf, binary.BigEndian, msg.Credits)
-	if !common.VerifySlice(msg.ECPubKey.Bytes, buf.Bytes(), msg.Sig) {
-		fmt.Println("Error in verifying signature for msg:" + fmt.Sprintf("%+v", msg))
-		return false
-	}
-
-	return true
+	binary.Write(buf, binary.BigEndian, msg.Credits)
+	
+	return ed25519.Verify(msg.ECPubKey, buf.Bytes(), msg.Sig)
 }
 
 // Create a sha hash from the message binary (output of BtcEncode)
