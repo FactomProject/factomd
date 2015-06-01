a7db66fec487f9e302dda118e40ee3a8b146dbc5
Paul Snow <snow.paul@gmail.com>
2015-06-01 10:32:22 -0500

    fixing number of arguments to genesis block

--- block/genesisblock_test.go
+++ block/genesisblock_test.go
@@ -21,7 +21,7 @@ var _ = rand.New
 var _ = binary.Write
 
 func Test_create_genesis_block(test *testing.T) {
-    gb := GetGenesisBlock()
+    gb := GetGenesisBlock(1000000,10,200000000000)
     txt,err := gb.MarshalText()
     if err != nil { test.Fail() }
     sc.Prtln(string(txt))
