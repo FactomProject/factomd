1d09111a063dc4bb06028753c4a303f01a6358e4
SirHalsell <Sir.Halsell@gmail.com>
2015-06-22 19:33:17 -0500

    add more debug info

--- factomblockmanager.go
+++ factomblockmanager.go
@@ -34,20 +34,21 @@ type dirInvMsg struct {
 // We examine the inventory advertised by the remote peer and act accordingly.
 func (b *blockManager) handleDirInvMsg(imsg *dirInvMsg) {
 	util.Trace()
-	/*
-		// Ignore invs from peers that aren't the sync if we are not current.
-		// Helps prevent fetching a mass of orphans.
-		if imsg.peer != b.syncPeer && !b.current() {
-			return
-		}
-	*/
+
+	// Ignore invs from peers that aren't the sync if we are not current.
+	// Helps prevent fetching a mass of orphans.
+	if imsg.peer != b.syncPeer && !b.current() {
+		return
+	}
 
 	// Attempt to find the final block in the inventory list.  There may
 	// not be one.
 	lastBlock := -1
 	invVects := imsg.inv.InvList
+	bmgrLog.Debugf("len(InvVects)=%d", len(invVects))
 	for i := len(invVects) - 1; i >= 0; i-- {
 		if invVects[i].Type == wire.InvTypeFactomDirBlock {
+			util.Trace()
 			lastBlock = i
 			bmgrLog.Debugf("lastBlock=%d", lastBlock)
 			break
@@ -83,10 +84,12 @@ func (b *blockManager) handleDirInvMsg(imsg *dirInvMsg) {
 		}
 		if !haveInv {
 			// Add it to the request queue.
+			util.Trace("!haveInv")
 			imsg.peer.requestQueue = append(imsg.peer.requestQueue, iv)
 			continue
 		}
-
+		
+		util.Trace("haveInv")
 		if iv.Type == wire.InvTypeFactomDirBlock {
 			// The block is an orphan block that we already have.
 			// When the existing orphan was processed, it requested
