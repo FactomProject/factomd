854d11b70ae350449b22f89d1bbdf09450e629bb
SirHalsell <sir.halsell@gmail.com>
2015-07-16 18:35:09 -0500

    fix download freeze issue

--- factompeer.go
+++ factompeer.go
@@ -6,6 +6,7 @@ package btcd
 
 import (
 	"encoding/hex"
+	"time"
 
 	"github.com/FactomProject/FactomCode/common"
 	"github.com/FactomProject/FactomCode/database"
@@ -450,7 +451,7 @@ func (p *peer) pushDirBlockMsg(sha *wire.ShaHash, doneChan, waitChan chan struct
 	if p.continueHash != nil && p.continueHash.IsEqual(sha) {
 		peerLog.Debug("continueHash: " + spew.Sdump(sha))
 		// Sleep for 5 seconds for the peer to catch up
-		//time.Sleep(5 * time.Second)
+		time.Sleep(5 * time.Second)
 
 		//
 		// Note: Rather than the latest block height, we should pass
