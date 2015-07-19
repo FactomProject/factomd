949f5536e6b482bd0a93cdc4d57f57379f7f7886
Paul Snow <snow.paul@gmail.com>
2015-07-19 03:10:02 -0500

    Added ability to view transactions under construction.  Calculates Fee required, and Fee being paid.  Updates to help. Coinbase support

---
+++ block/coinbase.go
@@ -0,0 +1,47 @@
+// Copyright 2015 Factom Foundation
+// Use of this source code is governed by the MIT
+// license that can be found in the LICENSE file.
+
+package block
+
+import (
+    fct "github.com/FactomProject/factoid"
+    "github.com/FactomProject/factoid/wallet"
+    "fmt"
+)
+
+var _ = fct.Prt
+var _ = fmt.Println
+
+var adrs []fct.IAddress 
+var amount     uint64 = 25000000           // One Factoid (remember, fixed point math!
+var addressCnt int    = 10                  // 10 addresses for now.
+
+// This routine generates the Coinbase.  This is a fixed amount to be
+// paid to the federated servers.  
+//
+// Currently we are paying just a few fixed addresses.
+//
+func GetCoinbase(ftime uint64) fct.ITransaction {
+    
+    if adrs == nil {
+        var w wallet.ISCWallet 
+        w = new (wallet.SCWallet)
+        w.Init()
+        
+        adrs = make([]fct.IAddress,addressCnt)
+        for i:=0;i<addressCnt;i++ {
+            adr,_ := w.GenerateFctAddress([]byte("adr"+string(i)),1,1)
+            adrs[i] = adr
+        }
+    }
+    
+    coinbase := new(fct.Transaction)
+    coinbase.SetMilliTimestamp(ftime)
+        
+    for _,adr := range adrs {
+        coinbase.AddOutput(adr,amount)   // add specified amount
+    }
+        
+    return coinbase
+}
