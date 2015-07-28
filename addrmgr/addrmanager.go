6bd025cd1ebdc38b2e93218dcc2fd12defbbdef0
Paul Snow <snow.paul@gmail.com>
2015-07-27 19:03:27 -0500

    degrade to just a simple random pick when less than 20 choices in btcd/addrmgr #577

--- addrmgr/addrmanager.go
+++ addrmgr/addrmanager.go
@@ -748,10 +748,22 @@ func (a *AddrManager) GetAddress(class string, newBias int) *KnownAddress {
 	a.mtx.Lock()
 	defer a.mtx.Unlock()
 
+    
 	if a.numAddresses() == 0 {
 		return nil
 	}
 
+	// Really.  If there are less than some small number of choices, just picking
+	// one and getting on with life will work better than trying to be smart.
+    if a.numAddresses() < 20 {
+        pick := rand.Int()%5
+        i :=0
+        for _,value := range a.addrIndex {
+            if i == pick { return value }
+            i ++
+        }
+    }
+                
 	if newBias > 100 {
 		newBias = 100
 	}
