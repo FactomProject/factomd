907834007be06a42e93d51162b046858dba9f76e
ThePiachu <ThePiachu@gmail.com>
2015-09-22 16:13:33 -0700

    go fmt ./...

--- common/factoid/database/bytestore_test.go
+++ common/factoid/database/bytestore_test.go
@@ -5,9 +5,9 @@
 package database
 
 import (
+	"bytes"
 	"encoding/binary"
 	"fmt"
-	"bytes"
 	"github.com/FactomProject/ed25519"
 	"math/rand"
 	"testing"
@@ -18,34 +18,32 @@ var _ = ed25519.Sign
 var _ = rand.New
 var _ = binary.Read
 
-
-
 func Test_bytestore(t *testing.T) {
 	b1 := new(ByteStore)
 	d := []byte("ksdafljljkaglajkgljkagljkRW")
 	b1.SetBytes(d)
-	data,err := b1.MarshalBinary()
+	data, err := b1.MarshalBinary()
 	if err != nil {
 		t.Fail()
 		return
 	}
-	
+
 	b2 := new(ByteStore)
 	err = b2.UnmarshalBinary(data)
 	if err != nil {
 		t.Fail()
 		return
 	}
-	
+
 	if b1.IsEqual(b2) != nil {
 		t.Fail()
 		return
 	}
-	
-	if !bytes.Equal(b2.Bytes(),d) {
+
+	if !bytes.Equal(b2.Bytes(), d) {
 		fmt.Println(string(b2.Bytes()))
 		t.Fail()
 		return
 	}
-		
+
 }
