8a48a64455c052879605b97a9123ce8b168a875d
ThePiachu <ThePiachu@gmail.com>
2015-10-13 14:49:15 -0700

    Added database initialization. go fmt.

--- state/state.go
+++ state/state.go
@@ -1,39 +1,40 @@
 package state
 
 import (
+	. "github.com/FactomProject/factomd/common/interfaces"
+	. "github.com/FactomProject/factomd/database"
+	. "github.com/FactomProject/factomd/database/hybridDB"
+	"github.com/FactomProject/factomd/util"
 	"log"
 	"sync"
-	. "github.com/FactomProject/factomd/common/interfaces"
-	"github.com/FactomProject/factomd/util"
 )
 
 type State struct {
-	
-	once                    sync.Once
-	cfg						IFactomConfig
-	inMsgQueue   			chan IMsg
-	outMsgQueue  			chan IMsg
+	once        sync.Once
+	cfg         IFactomConfig
+	inMsgQueue  chan IMsg
+	outMsgQueue chan IMsg
 	//Network
-	networkNumber			int					// Encoded into Directory Blocks
-	
+	networkNumber int // Encoded into Directory Blocks
+
 	// Number of Servers acknowledged by Factom
-	totalServers			int 
-	serverState  		  	int      // (0 if client, 1 if server, 2 if audit server
-	matryoshka   		  	[]IHash  // Reverse Hash
-	
+	totalServers int
+	serverState  int     // (0 if client, 1 if server, 2 if audit server
+	matryoshka   []IHash // Reverse Hash
+
 	// Database
-	db                    	IDatabase
-	
+	db *database.DBOverlay
+
 	// Directory Block State
-	currentDirectoryBlock 	IDirectoryBlock
-	dBHeight              	int
-	
+	currentDirectoryBlock IDirectoryBlock
+	dBHeight              int
+
 	// Message State
-	lastAck				  	IMsg					// Return the last Acknowledgement set by this server
-	
+	lastAck IMsg // Return the last Acknowledgement set by this server
+
 }
 
-var _ IState = (*State)(nil)
+//var _ IState = (*State)(nil)
 
 // Getting the cfg state for Factom doesn't force a read of the config file unless
 // it hasn;t been read yet.
@@ -72,33 +73,57 @@ func (s *State) Matryoshka() []IHash {
 func (s *State) LastAck() IMsg {
 	return s.lastAck
 }
-	
 
 func (s *State) Init() {
-	
+
 	// Get our factomd configuration information.
 	cfg := s.Cfg().(*util.FactomdConfig)
 
-	s.inMsgQueue      = make(chan IMsg, 10000) //incoming message queue for factom application messages
-	s.outMsgQueue     = make(chan IMsg, 10000) //outgoing message queue for factom application messages
-	
+	s.inMsgQueue = make(chan IMsg, 10000)  //incoming message queue for factom application messages
+	s.outMsgQueue = make(chan IMsg, 10000) //outgoing message queue for factom application messages
+
 	//Database
-	
-	
+
 	//Network
 	switch cfg.App.Network {
-		case "MAIN" :
-			s.networkNumber = 0
-		case "TEST" :
-			s.networkNumber = 1
-		case "LOCAL" :
-			s.networkNumber = 2
-		case "CUSTOM" :
-			s.networkNumber = 3
-		default :
-			panic("Bad value for Network in factomd.conf")
+	case "MAIN":
+		s.networkNumber = 0
+	case "TEST":
+		s.networkNumber = 1
+	case "LOCAL":
+		s.networkNumber = 2
+	case "CUSTOM":
+		s.networkNumber = 3
+	default:
+		panic("Bad value for Network in factomd.conf")
 	}
-	
+
+}
+
+func (s *State) InitLevelDB() error {
+	path := cfg.App.LdbPath + "/" + cfg.App.Network + "/" + "factoid_level.db"
+	dbase, err := hybridDB.NewLevelMapHybridDB(path, false)
+
+	if err != nil {
+		return err
+	}
+
+	if dbase == nil {
+		dbase, err = hybridDB.NewLevelMapHybridDB(path, true)
+		if err != nil {
+			return err
+		}
+	}
+
+	s.db = databaseOverlay.NewOverlay(dbase)
+	return nil
+}
+
+func (s *State) InitBoltDB() error {
+	path := cfg.App.BoltDBPath + "/" + cfg.App.Network + "/" + "factoid_bolt.db"
+	dbase := hybridDB.NewBoltMapHybridDB(nil, path)
+	s.db = dbase
+	return nil
 }
 
 func (s *State) String() string {
@@ -107,11 +132,10 @@ func (s *State) String() string {
 
 func (s *State) NetworkName() string {
 	return (s.Cfg().(util.FactomdConfig)).App.Network
-	
+
 }
 
-
-func (s *State) NetworkPublicKey()		[]byte	{
+func (s *State) NetworkPublicKey() []byte {
 	return nil // TODO add our keys here...
 }
 
@@ -130,7 +154,6 @@ func (s *State) DB() IDatabase {
 func (s *State) SetDB(db IDatabase) {
 	s.db = db
 }
-
 
 func (s *State) DBHeight() int {
 	return s.dBHeight
