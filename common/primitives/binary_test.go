e2255203e25d203966c543a62042603c102bc8fb
ThePiachu <ThePiachu@gmail.com>
2015-10-20 14:57:13 -0700

    Removed unused code

--- common/primitives/binary_test.go
+++ common/primitives/binary_test.go
@@ -19,47 +19,6 @@ func init() {
 	testBytes = h
 }
 
-func TestBAMisc(t *testing.T) {
-	ba := new(ByteArray)
-
-	prefix, err := hex.DecodeString("0000000000000040")
-	if err != nil {
-		t.Error(err)
-	}
-
-	err = ba.UnmarshalBinary(append(prefix, testBytes...))
-	if err != nil {
-		t.Error(err)
-	}
-
-	h, err := ba.MarshalBinary()
-	if err != nil {
-		t.Error(err)
-	}
-	if areBytesIdentical(h, append(prefix, testBytes...)) == false {
-		t.Errorf("Failed MarshalBinary. Expected %x, got %x", append(prefix, testBytes...), h)
-	}
-
-	ba2, err := NewByteArray(testBytes)
-	if err != nil {
-		t.Error(err)
-	}
-	h2, err := ba2.MarshalBinary()
-	if err != nil {
-		t.Error(err)
-	}
-	if areBytesIdentical(h2, h) == false {
-		t.Errorf("Failed NewByteArray")
-	}
-
-	if areBytesIdentical(ba2.Bytes(), testBytes) == false {
-		t.Errorf("Failed NewByteArray")
-	}
-	if ba2.MarshalledSize() != 72 {
-		t.Error("Failed MarshalledSize")
-	}
-}
-
 func areBytesIdentical(a, b []byte) bool {
 	if len(a) != len(b) {
 		return false
