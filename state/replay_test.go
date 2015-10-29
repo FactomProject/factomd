a4c71a2b5b32b5ce6d58f6f04da7d9e040325dee
Paul Snow <snow.paul@gmail.com>
2015-10-28 19:38:11 -0500

    Removed redundant acknowledgement messages

--- state/replay_test.go
+++ state/replay_test.go
@@ -25,24 +25,38 @@ func Test_Replay(test *testing.T) {
 		time int64 
 	}
 	
-	var h [5000] *mh
+	XTrans := 15000
 	
-	for i := 0; i<5000; i++ {
+	h := make([] *mh,XTrans)
+	
+	for i := 0; i<XTrans; i++ {
+		
+		// We are going to remember some large set of transactions.
 		h[i] = new(mh)
 		h[i].hash = fct.Sha([]byte(fmt.Sprintf("h%d",i))).Bytes()
+		
+		// Build a valid transaction somewhere +/- 12 hours of now
 		h[i].time = now + (rand.Int63()%24*hour)-12*hour
 		
+		// The first time we test, it should be valid.
 		if !IsTSValid_(h[i].hash, h[i].time,now) { 
 			fmt.Println("Failed Test ",i,"first")
 			test.Fail() 
 			return
 		}
+		
+		// An immediate replay!  Should fail!
 		if IsTSValid_(h[i].hash, h[i].time,now) { 
 			fmt.Println("Failed Test ",i,"second")
 			test.Fail()
 			return
 		}
-		now += rand.Int63()%hour
+		
+		// Move time forward somewhere between 0 to 15 minutes
+		now += rand.Int63()%hour/4
+		
+		// Now replay all the transactions we have collected.  NONE of them
+		// should work.
 		for j:=0; j<i; j++ {
 			if IsTSValid_(h[i].hash, h[i].time,hour) {
 				fmt.Println("Failed Test ",i,j,"repeat")
@@ -51,5 +65,7 @@ func Test_Replay(test *testing.T) {
 			}
 		}
 	}
-			
+
+	fmt.Println("Simulation ran from",time.Now(), "to", time.Unix(now,0))
+	
 }
