16a940edff109917a755aaf0c74bc755056274ee
ThePiachu <ThePiachu@gmail.com>
2015-11-05 14:24:05 -0800

    Added placeholder test for initializing state with map db.

--- state/state_test.go
+++ state/state_test.go
@@ -6,11 +6,22 @@ package state
 
 import (
 	"github.com/FactomProject/factomd/log"
+	"github.com/FactomProject/factomd/util"
 	"testing"
 )
 
 func TestInit(t *testing.T) {
 	state := new(State)
+
+	cfg := util.ReadConfig("")
+	cfg.App.DBType = "Map"
+
+	state.Cfg = cfg
+
 	state.Init("")
+
+	if state.Cfg.(*util.FactomdConfig).App.DBType != "Map" {
+		t.Error("CFG has been overwritten")
+	}
 	log.Println(state.String())
 }
