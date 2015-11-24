a506eba850ef2da1e3fedb470c2b0490c0c517d3
ThePiachu <ThePiachu@gmail.com>
2015-11-24 13:06:57 -0800

    Added first wsapi test.

--- wsapi/wsapi_test.go
+++ wsapi/wsapi_test.go
@@ -4,6 +4,8 @@ import (
 	"github.com/FactomProject/factomd/testHelper"
 	. "github.com/FactomProject/factomd/wsapi"
 	"github.com/hoisie/web"
+	"net/http"
+	"strings"
 	"testing"
 )
 
@@ -12,17 +14,45 @@ func TestHandleDirectoryBlockHead(t *testing.T) {
 
 	HandleDirectoryBlockHead(context)
 
-	t.Logf("context - %v", context)
-	t.Fail()
+	if strings.Contains(GetBody(context), "0376c747a7c0cd92fb03acfefee39fc9ba49c33784ef0be39be2ae0a1bea9a52") == false {
+		t.Errorf("Context does not contain proper DBlock Head - %v", GetBody(context))
+	}
 }
 
 func createWebContext() *web.Context {
 	context := new(web.Context)
 	context.Server = new(web.Server)
-
 	context.Server.Env = map[string]interface{}{}
-
 	context.Server.Env["state"] = testHelper.CreateAndPopulateTestState()
+	context.ResponseWriter = new(TestResponseWriter)
 
 	return context
+}
+
+type TestResponseWriter struct {
+	HeaderCode int
+	Head       map[string][]string
+	Body       string
+}
+
+var _ http.ResponseWriter = (*TestResponseWriter)(nil)
+
+func (t *TestResponseWriter) Header() http.Header {
+	if t.Head == nil {
+		t.Head = map[string][]string{}
+	}
+	return (http.Header)(t.Head)
+}
+
+func (t *TestResponseWriter) WriteHeader(h int) {
+	t.HeaderCode = h
+}
+
+func (t *TestResponseWriter) Write(b []byte) (int, error) {
+	t.Body = t.Body + string(b)
+	return len(b), nil
+}
+
+func GetBody(context *web.Context) string {
+	return context.ResponseWriter.(*TestResponseWriter).Body
 }
