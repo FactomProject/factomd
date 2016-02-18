73bc9791570c0472e201e7ce1ac9449264570853
Michael <michaeljbeam@gmail.com>
2016-02-18 15:57:21 -0600

    added profiler instructions

--- profiler.go
+++ profiler.go
@@ -10,6 +10,11 @@ import (
 	_ "net/http/pprof"
 )
 
+// StartProfiler runs pprof. get pprof data by running the go tool pprof
+// commands.
+// go tool pprof -pdf http://localhost:6060/debug/pprof/heap >/tmp/heap.pdf
+// go tool pprof -pdf http://localhost:6060/debug/pprof/profile >/tmp/profile.pdf
+// go tool pprof -pdf http://localhost:6060/debug/pprof/block >/tmp/block.pdf
 func StartProfiler() {
 	log.Println("DEBUG: Starting profiler")
 	log.Println(http.ListenAndServe("localhost:6060", nil))
