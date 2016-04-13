cac7a687038aed6f379c17d68277b1e5ae4ce37f
Nirvana <nirvana@myster.io>
2016-04-13 14:00:02 -0500

    sample code heartbeat working

--- net_test/test.sh
+++ net_test/test.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 cd .. 
+echo "Compiling..."
 go install -a 
+echo "Running..."
 if [ $? -eq 0 ]; then
     factomd -count=1 -net=tree -folder="test1-" -port=8089 -serve=40899 -follower=true & node0=$!
     factomd -count=1 -net=tree -folder="test2-" -port=8090 -connect="tcp://217.0.0.1:40899" -leader=true & node1=$!
