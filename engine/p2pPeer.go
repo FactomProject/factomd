53cae94d0bb4b3f06119ef1f57bce4fa0896eff4
Nirvana <nirvana@myster.io>
2016-04-14 21:54:39 -0500

    WIP CHECKPOINT - Replaced Send/Recieve with Channel versions, still using test code heart beat. (Channels not hooked up to network code yet.)

--- engine/p2pPeer.go
+++ engine/p2pPeer.go
@@ -12,6 +12,7 @@ import (
 	"unicode"
 
 	"github.com/FactomProject/factomd/common/interfaces"
+	"github.com/FactomProject/factomd/common/messages"
 	"github.com/go-mangos/mangos"
 	"github.com/go-mangos/mangos/protocol/bus"
 	"github.com/go-mangos/mangos/transport/tcp"
@@ -21,9 +22,6 @@ var _ = fmt.Print
 
 var (
 	p2pSocket mangos.Socket // BUGBUG JAYJAY TODO This is a global. This needs to migrate to p2p package.
-	// Frequency of issuing debug print statements in netowkr code-- 2 = %100, 100 = %1 of the time.
-	send_freq    = 2
-	recieve_freq = 200
 )
 
 type P2PPeer struct {
@@ -49,75 +47,29 @@ func (f *P2PPeer) GetNameFrom() string {
 func (f *P2PPeer) GetNameTo() string {
 	return f.ToName
 }
-
 func (f *P2PPeer) Send(msg interfaces.IMsg) error {
+	data, err := msg.MarshalBinary()
+	if err != nil {
+		return err
+	}
 
-	// fmt.Printf("P2PPeer.Send for:\n %+v\n\n", msg)
-
-	// data, err := msg.MarshalBinary()
-	// if err != nil {
-	// 	note("P2PPeer.Send !!!!!!!!!!!! FAILED TO MARSHALL BINARY for:\n %+v\n\n", msg)
-	// 	return err
-	// }
-	// if err = p2pSocket.Send(data); err != nil {
-	// 	note("P2PPeer.Send ERROR: %s", err.Error())
-	// 	return err
-	// }
-	// if 1 == rand.Intn(send_freq) {
-	// 	fmt.Printf("%d -- P2PPeer.SEND %s -> %s\t %s \n data: %s", os.Getpid(), f.FromName, f.ToName, msg, string(data))
-	// }
+	f.BroadcastOut <- data
 	return nil
 }
 
+// Non-blocking return value from channel.
 func (f *P2PPeer) Recieve() (interfaces.IMsg, error) {
-
-	// if 1 == rand.Intn(recieve_freq) {
-	// 	fmt.Printf("%d -- P2PPeer.RECIEVE %s -> %s\n", os.Getpid(), f.FromName, f.ToName)
-	// }
-
-	// if data, err := p2pSocket.Recv(); err != nil {
-	// 	note("P2PPeer.Recv ERROR: %s", err.Error())
-	// } else {
-	// 	note("P2PPeer.Recieve ######## \"%s\" FROM BUS", string(data))
-	// if len(data) > 0 {
-	// 	msg, err := messages.UnmarshalMessage(data)
-	// 	if nil == err {
-	// 		note("P2PPeer.Recieve $$$$$$$$$$$$ GOT VALID MESSAGE:\t %+v\n", msg)
-	// 		return msg, err
-	// 	} else {
-	// 		note("P2PPeer.Recieve Got invalid MESSAGE:\t %+v\n", string(data))
-	// 		return nil, nil
-	// 	}
-
-	// } else {
-	// 	note("P2PPeer.Recieve Zero byte message???? \"%s\"", string(data))
-	// }
-	// }
-
+	select {
+	case data, ok := <-f.BroadcastIn:
+		if ok {
+			msg, err := messages.UnmarshalMessage(data)
+			return msg, err
+		}
+	default:
+	}
 	return nil, nil
 }
 
-// 100ms Timeout
-// f.Socket.SetOption(mangos.OptionRecvDeadline, 100*time.Millisecond)
-// // Minimal blocking
-// // f.Socket.SetOption(mangos.OptionRecvDeadline, 1*time.Millisecond)
-// var data []byte
-// var err error
-// if data, err = f.Socket.Recv(); err == nil {
-// 	// if len(data) > 0 {
-// 	msg, err := messages.UnmarshalMessage(data)
-// 	if nil == err {
-// 		fmt.Printf("%d -- P2PPeer.Recieve $$$$$$$$$$$$ GOT VALID MESSAGE:\t %+v\n", os.Getpid(), msg)
-// 	} else {
-// 		fmt.Printf("%d -- P2PPeer.Recieve Got invalid MESSAGE:\t %+v\n", os.Getpid(), string(data))
-// 		// }
-// 		return msg, err
-// 	}
-// }
-// beat := "HEARTBEAT"
-// if err = f.Socket.Send([]byte(beat)); err != nil {
-// 	fmt.Printf("%d -- P2PPeer.Connect ###### error from f.Socket.Send(data) for:\n %+v\n\n", os.Getpid(), beat)
-// }
 // Is this connection equal to parm connection
 func (f *P2PPeer) Equals(ff interfaces.IPeer) bool {
 	f2, ok := ff.(*P2PPeer)
@@ -157,52 +109,6 @@ func (f *P2PPeer) Len() int {
 //////////////////////////////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////////////////////////////
 
-// Mangos example code below:
-
-///// PAIR EXAMPLE:
-
-// func sendMessage(sock mangos.Socket, payload []byte) {
-// 	fmt.Printf("%d: SENDING \"%s\"\n", os.Getpid(), string(payload))
-// 	if err := sock.Send(payload); err != nil {
-// 		die("failed sending: %s", err)
-// 	}
-// }
-
-// func recvMessage(sock mangos.Socket) {
-// 	var msg []byte
-// 	var err error
-// 	if msg, err = sock.Recv(); err == nil {
-// 		fmt.Printf("%d -- RECEIVED: \"%s\"\n", os.Getpid(), string(msg))
-// 	}
-// }
-
-// func heartbeat(sock mangos.Socket, name string) {
-// 	for {
-// 		sock.SetOption(mangos.OptionRecvDeadline, 100*time.Millisecond)
-// 		recvMessage(sock)
-// 		time.Sleep(time.Second)
-// 		sendMessage(sock, []byte(name))
-// 	}
-// }
-
-// func listen(url string) {
-// 	var err error
-// 	if err = p2pSocket.Listen(url); err != nil {
-// 		die("can't listen on pair socket: %s", err.Error())
-// 	}
-// 	go heartbeat(p2pSocket, "Listener Says Hello")
-// }
-
-// func dial(url string) {
-// 	var err error
-
-// 	// sock.AddTransport(ipc.NewTransport())
-// 	p2pSocket.AddTransport(tcp.NewTransport())
-// 	if err = p2pSocket.Dial(url); err != nil {
-// 		die("can't dial on pair socket: %s", err.Error())
-// 	}
-// 	go heartbeat(p2pSocket, "Caller says hello")
-// }
 func die(format string, v ...interface{}) {
 	fmt.Fprintln(os.Stderr, fmt.Sprintf("%d:", os.Getpid()), fmt.Sprintf(format, v...))
 	os.Exit(1)
@@ -230,7 +136,7 @@ func P2PNetworkStart(address string, peers string) {
 
 	note("P2PNetworkStart- Sleep for a few seconds to let peers wake up.")
 	// wait for everyone to start listening
-	time.Sleep(time.Second * 3)
+	time.Sleep(time.Second)
 
 	// Parse the peers into an array.
 	parseFunc := func(c rune) bool {
@@ -256,31 +162,45 @@ func P2PNetworkStart(address string, peers string) {
 
 func heartbeat(address string) {
 	beat := ""
-	var msg []byte
-	var err error
 	for i := 0; i < 500; i++ {
 		beat = fmt.Sprintf("Heartbeat FROM %s. Beat #%d", address, i)
-		// note("%s: SENDING >>>>>>>>>> '%s' ONTO BUS\n", address, beat)
-		if err = p2pSocket.Send([]byte(beat)); err != nil {
-			note("heartbeat.Send ERROR: %s", err.Error())
-		}
-		time.Sleep(time.Second * 10)
-		if msg, err = p2pSocket.Recv(); err != nil {
-			note("heartbeat.Recv ERROR: %s", err.Error())
-		} else {
-			note("RECEIVED \"%s\" FROM BUS", string(msg))
-		}
+		sendP2P([]byte(beat))
+		recieveP2P()
 		i += i
+		time.Sleep(time.Second)
 	}
+}
+
+func sendP2P(msg []byte) {
+	if err := p2pSocket.Send(msg); err != nil {
+		note("sendP2P.Send ERROR: %s", err.Error())
+	}
+}
+
+func recieveP2P() []byte {
+	data, err := p2pSocket.Recv()
+	if err != nil {
+		note("recieveP2P.Recv ERROR: %s", err.Error())
+	} else {
+		note("recieveP2P RECEIVED \"%s\"", string(data))
+	}
+	return data
 }
 
 // Thought process:
 // X leader listens, follower connects.
 // X Change message format to binary
 // X Make this file P2PPeer  and make it work like iPeer
+// X we listen always on the given port (And we dial out to the peers we know about) (this requires we be probably in VMs)
+// X no leadership awareness in p2p
+// X Go back to Pauls' Send/Recieve from SimPeer
+// X Verify sample code heart beat
+// X Split out the send and recieve functions from sample code (no channels)
+// X Verify heartbeat still works
+// -- Make the send and recieve functions from run as goroutines and work on channels (STILL WITH HeARTBEAT SAMPLE CODE)
+// -- Switch the channels over to the ones that P2PPeer uses (copied from simpeers)
+
 // -- Split out the P@PNetworkStart and Send/Recoeve into a P2PNetowrk File
-// XX we listen always on the given port (And we dial out to the peers we know about) (this requires we be probably in VMs)
-// XX no leadership awareness in p2p
 
 // Add a config file in .factom (peers.json?) and read it for a list of peers to connect to.
 
