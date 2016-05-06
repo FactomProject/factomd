943fab0a9293d4933cdb5f1c231be1548fd89be3
ThePiachu <ThePiachu@gmail.com>
2016-05-06 15:32:14 -0700

    More tweaks.

--- Utilities/DatabasePorter/porter.go
+++ Utilities/DatabasePorter/porter.go
@@ -63,6 +63,7 @@ mainloop:
 		dBlockList := make([]interfaces.IDirectoryBlock, int(dBlock.GetDatabaseHeight())+1)
 		dBlockList[int(dBlock.GetDatabaseHeight())] = dBlock
 
+		fmt.Printf("\t\tFetching DBlocks\n")
 		for {
 			keymr = dBlock.GetHeader().GetPrevKeyMR().String()
 			if keymr == endKeyMR {
@@ -182,6 +183,7 @@ mainloop:
 			fmt.Printf("Saved block height %v\n", v.GetDatabaseHeight())
 		}
 	}
+	fmt.Printf("\t\tRebulding DirBlockInfo\n")
 	err := dbo.RebuildDirBlockInfo()
 	if err != nil {
 		panic(err)
