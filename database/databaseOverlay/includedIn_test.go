ce2c3411fd35f51f3a4011ecdb1aebdb0dfe9eeb
ThePiachu <ThePiachu@gmail.com>
2016-05-06 18:56:28 -0700

    Added tests for entry overwriting, etc.

--- database/databaseOverlay/includedIn_test.go
+++ database/databaseOverlay/includedIn_test.go
@@ -43,3 +43,70 @@ func TestIncludedIn(t *testing.T) {
 		}
 	}
 }
+
+func TestIncludedInOverwriting(t *testing.T) {
+	blocks := []*EBlock{}
+	max := 10
+	var prev *EBlock = nil
+	dbo := NewOverlay(new(mapdb.MapDB))
+	defer dbo.Close()
+
+	for i := 0; i < max; i++ {
+		prev, _ = CreateTestEntryBlockWithContentN(prev, 1)
+		blocks = append(blocks, prev)
+		err := dbo.SaveEBlockHead(prev, true)
+		if err != nil {
+			t.Error(err)
+		}
+	}
+
+	for i, block := range blocks {
+		for _, entry := range block.GetEntryHashes() {
+			blockHash, err := dbo.LoadIncludedIn(entry)
+			if err != nil {
+				t.Error(err)
+			}
+			if i < 2 {
+				if blockHash.IsSameAs(block.DatabasePrimaryIndex()) == false {
+					t.Error("Wrong IncludedIn result")
+				}
+			} else {
+				if blockHash.IsSameAs(blocks[1].DatabasePrimaryIndex()) == false {
+					t.Error("Wrong IncludedIn result")
+				}
+			}
+		}
+	}
+
+	blocks = []*EBlock{}
+	prev = nil
+	dbo.Close()
+	dbo = NewOverlay(new(mapdb.MapDB))
+
+	for i := 0; i < max; i++ {
+		prev, _ = CreateTestEntryBlockWithContentN(prev, 1)
+		blocks = append(blocks, prev)
+		err := dbo.SaveEBlockHead(prev, false)
+		if err != nil {
+			t.Error(err)
+		}
+	}
+
+	for i, block := range blocks {
+		for _, entry := range block.GetEntryHashes() {
+			blockHash, err := dbo.LoadIncludedIn(entry)
+			if err != nil {
+				t.Error(err)
+			}
+			if i < 1 {
+				if blockHash.IsSameAs(block.DatabasePrimaryIndex()) == false {
+					t.Error("Wrong IncludedIn result")
+				}
+			} else {
+				if blockHash.IsSameAs(blocks[max-1].DatabasePrimaryIndex()) == false {
+					t.Error("Wrong IncludedIn result")
+				}
+			}
+		}
+	}
+}
