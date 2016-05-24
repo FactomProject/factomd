705d93e7d92a567d599db30b205a381254e48a09
ThePiachu <ThePiachu@gmail.com>
2016-05-24 14:28:16 -0700

    Created test for dbOverlay PaidFor

--- database/databaseOverlay/ecblock.go
+++ database/databaseOverlay/ecblock.go
@@ -9,24 +9,32 @@ import (
 )
 
 // ProcessECBlockBatch inserts the ECBlock and update all it's cbentries in DB
-func (db *Overlay) ProcessECBlockBatch(block interfaces.DatabaseBlockWithEntries) error {
+func (db *Overlay) ProcessECBlockBatch(block interfaces.IEntryCreditBlock, checkForDuplicateEntries bool) error {
 	err := db.ProcessBlockBatch([]byte{byte(ENTRYCREDITBLOCK)},
 		[]byte{byte(ENTRYCREDITBLOCK_NUMBER)},
 		[]byte{byte(ENTRYCREDITBLOCK_KEYMR)}, block)
 	if err != nil {
 		return err
 	}
-	return db.SaveIncludedInMultiFromBlock(block, false)
+	err = db.SaveIncludedInMultiFromBlock(block, false)
+	if err != nil {
+		return err
+	}
+	return db.SavePaidForMultiFromBlock(block, checkForDuplicateEntries)
 }
 
-func (db *Overlay) ProcessECBlockMultiBatch(block interfaces.DatabaseBlockWithEntries) error {
+func (db *Overlay) ProcessECBlockMultiBatch(block interfaces.IEntryCreditBlock, checkForDuplicateEntries bool) error {
 	err := db.ProcessBlockMultiBatch([]byte{byte(ENTRYCREDITBLOCK)},
 		[]byte{byte(ENTRYCREDITBLOCK_NUMBER)},
 		[]byte{byte(ENTRYCREDITBLOCK_KEYMR)}, block)
 	if err != nil {
 		return err
 	}
-	return db.SaveIncludedInMultiFromBlockMultiBatch(block, false)
+	err = db.SaveIncludedInMultiFromBlockMultiBatch(block, false)
+	if err != nil {
+		return err
+	}
+	return db.SavePaidForMultiFromBlockMultiBatch(block, checkForDuplicateEntries)
 }
 
 // FetchECBlockByHeaderHash gets an Entry Credit block by hash from the database.
@@ -71,8 +79,8 @@ func toECBlocksList(source []interfaces.BinaryMarshallableAndCopyable) []interfa
 	return answer
 }
 
-func (db *Overlay) SaveECBlockHead(block interfaces.DatabaseBlockWithEntries) error {
-	return db.ProcessECBlockBatch(block)
+func (db *Overlay) SaveECBlockHead(block interfaces.IEntryCreditBlock, checkForDuplicateEntries bool) error {
+	return db.ProcessECBlockBatch(block, checkForDuplicateEntries)
 }
 
 func (db *Overlay) FetchECBlockHead() (interfaces.IEntryCreditBlock, error) {
