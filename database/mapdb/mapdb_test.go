2a8d87be52d75cd52cf8171cb0b57ca470c54d6a
ThePiachu <ThePiachu@gmail.com>
2016-05-30 19:35:03 -0700

    Added a map DB brute force test.

--- database/mapdb/mapdb_test.go
+++ database/mapdb/mapdb_test.go
@@ -2,9 +2,12 @@ package mapdb_test
 
 import (
 	"fmt"
+	"math/rand"
+	"testing"
+	"time"
+
 	"github.com/FactomProject/factomd/common/interfaces"
 	. "github.com/FactomProject/factomd/database/mapdb"
-	"testing"
 )
 
 type TestData struct {
@@ -129,5 +132,41 @@ func TestMultiValue(t *testing.T) {
 	}
 	if len(keys) != 0 {
 		t.Error("Keys not cleared from database properly")
+	}
+}
+
+func TestParallelAccess(t *testing.T) {
+	threads := 100
+	m := new(MapDB)
+	bucket := []byte{byte(0x01)}
+	c := make(chan int)
+	closed := make(chan int, threads)
+	for i := 0; i < threads; i++ {
+		go func(j int) {
+			for {
+				select {
+				case <-c:
+					closed <- 1
+					return
+				default:
+					str := new(TestData)
+					str.Str = fmt.Sprintf("%v", rand.Int())
+					err := m.Put(bucket, []byte{byte(rand.Uint32() % 255)}, str)
+					if err != nil {
+						t.Errorf("Got error - %v", err)
+					}
+					_, err = m.Get(bucket, []byte{byte(rand.Uint32() % 255)}, str)
+					if err != nil {
+						t.Errorf("Got error - %v", err)
+					}
+				}
+			}
+		}(i)
+	}
+	time.Sleep(10 * time.Second)
+	close(c)
+	time.Sleep(1 * time.Second)
+	for i := 0; i < threads; i++ {
+		<-closed
 	}
 }
