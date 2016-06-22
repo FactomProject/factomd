31087ddb7a5659606f6a98f5d54bb18652f0ef3a
Matthew Whittington <matt@mattwhittington.com>
2016-06-21 19:41:04 -0500

    Authority Updates

--- state/factoidstate.go
+++ state/factoidstate.go
@@ -242,7 +242,8 @@ func (fs *FactoidState) ProcessEndOfBlock(state interfaces.IState) {
 	}
 
 	LoadIdentityByDirectoryBlockHeight(fs.DBHeight, fs.State, true)
-
+	LoadAuthorityByAdminBlockHeight(fs.DBHeight, fs.State, true)
+	
 	fs.DBHeight++
 }
 
