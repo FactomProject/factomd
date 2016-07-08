270f741d2bc9a8a0b7173edb9ea2ce1b54e40cce
ThePiachu <ThePiachu@gmail.com>
2016-07-08 05:19:54 -0700

    Added more integrity tests

--- Utilities/DatabaseIntegrityCheck/DatabaseIntegrityCheck.go
+++ Utilities/DatabaseIntegrityCheck/DatabaseIntegrityCheck.go
@@ -69,33 +69,102 @@ func CheckDatabase(db interfaces.IDatabase) {
 
 	next := FetchBlockSet(dbo, dBlock.DatabasePrimaryIndex())
 
+	fmt.Printf("\tStarting consecutive block analysis\n")
+
+	hashMap := map[string]string{}
+
 	var i int
 	for {
 		prev := FetchBlockSet(dbo, next.DBlock.GetHeader().GetPrevKeyMR())
+
+		hashMap[next.DBlock.DatabasePrimaryIndex().String()] = "OK"
 		err = directoryBlock.CheckBlockPairIntegrity(next.DBlock, prev.DBlock)
 		if err != nil {
 			fmt.Printf("Error for DBlock %v %v - %v\n", next.DBlock.GetHeader().GetDBHeight(), next.DBlock.DatabasePrimaryIndex(), err)
 		}
+
+		hashMap[next.ABlock.DatabasePrimaryIndex().String()] = "OK"
 		err = adminBlock.CheckBlockPairIntegrity(next.ABlock, prev.ABlock)
 		if err != nil {
 			fmt.Printf("Error for ABlock %v %v - %v\n", next.ABlock.GetDatabaseHeight(), next.ABlock.DatabasePrimaryIndex(), err)
 		}
+
+		hashMap[next.ECBlock.DatabasePrimaryIndex().String()] = "OK"
 		err = entryCreditBlock.CheckBlockPairIntegrity(next.ECBlock, prev.ECBlock)
 		if err != nil {
 			fmt.Printf("Error for ECBlock %v %v - %v\n", next.ECBlock.GetDatabaseHeight(), next.ECBlock.DatabasePrimaryIndex(), err)
 		}
+
+		hashMap[next.FBlock.DatabasePrimaryIndex().String()] = "OK"
 		err = factoid.CheckBlockPairIntegrity(next.FBlock, prev.FBlock)
 		if err != nil {
 			fmt.Printf("Error for FBlock %v %v - %v\n", next.FBlock.GetDatabaseHeight(), next.FBlock.DatabasePrimaryIndex(), err)
 		}
+
+		i++
 		if prev.DBlock == nil {
 			break
 		}
 		next = prev
-		i++
 	}
 
-	fmt.Printf("Finished analysing %v sets of blocks\n", i)
+	fmt.Printf("\tFinished analysing %v sets of blocks\n", i)
+
+	fmt.Printf("\tLooking for free-floating blocks\n")
+
+	dBlocks, err := dbo.FetchAllDBlocks()
+	if err != nil {
+		panic(err)
+	}
+	if len(dBlocks) != i {
+		fmt.Printf("Found %v dBlocks, expected %v\n", len(dBlocks), i)
+	}
+	for _, block := range dBlocks {
+		if hashMap[block.DatabasePrimaryIndex().String()] == "" {
+			fmt.Printf("Free-floating DBlock - %v, %v\n", block.DatabasePrimaryIndex().String(), block.String())
+		}
+	}
+
+	aBlocks, err := dbo.FetchAllABlocks()
+	if err != nil {
+		panic(err)
+	}
+	if len(aBlocks) != i {
+		fmt.Printf("Found %v aBlocks, expected %v\n", len(aBlocks), i)
+	}
+	for _, block := range aBlocks {
+		if hashMap[block.DatabasePrimaryIndex().String()] == "" {
+			fmt.Printf("Free-floating ABlock - %v, %v\n", block.DatabasePrimaryIndex().String(), block.String())
+		}
+	}
+
+	fBlocks, err := dbo.FetchAllFBlocks()
+	if err != nil {
+		panic(err)
+	}
+	if len(fBlocks) != i {
+		fmt.Printf("Found %v fBlocks, expected %v\n", len(fBlocks), i)
+	}
+	for _, block := range fBlocks {
+		if hashMap[block.DatabasePrimaryIndex().String()] == "" {
+			fmt.Printf("Free-floating FBlock - %v, %v\n", block.DatabasePrimaryIndex().String(), block.String())
+		}
+	}
+
+	ecBlocks, err := dbo.FetchAllECBlocks()
+	if err != nil {
+		panic(err)
+	}
+	if len(ecBlocks) != i {
+		fmt.Printf("Found %v ecBlocks, expected %v\n", len(ecBlocks), i)
+	}
+	for _, block := range ecBlocks {
+		if hashMap[block.DatabasePrimaryIndex().String()] == "" {
+			fmt.Printf("Free-floating ECBlock - %v, %v\n", block.DatabasePrimaryIndex().String(), block.String())
+		}
+	}
+
+	fmt.Printf("\tFinished looking for free-floating blocks\n")
 }
 
 type BlockSet struct {
