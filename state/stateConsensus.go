c155accb3eb21a52f3ffe4d4a408f00f369dec47
Paul Snow <snow.paul@gmail.com>
2016-07-14 19:42:19 -0500

    lower priority of review processing

--- state/stateConsensus.go
+++ state/stateConsensus.go
@@ -78,7 +78,7 @@ func (s *State) Process() (progress bool) {
 	s.ReviewHolding()
 
 	// Reprocess any stalled Acknowledgements
-	for i := 0; i < 5 && len(s.XReview) > 0; i++ {
+	for i := 0; i < 1 && len(s.XReview) > 0; i++ {
 		msg := s.XReview[0]
 		executeMsg(msg)
 		s.XReview = s.XReview[1:]
