087f49c1534927951c9f1d6517af260268af2e18
Nirvana <nirvana@myster.io>
2016-07-19 16:48:44 -0500

    added exclusive cluster test, more metrics for control panel and cleaned up a bit for testnet deployment

--- p2p/process_cluster_test.sh
+++ p2p/process_cluster_test.sh
@@ -7,13 +7,13 @@ go install
 if [ $? -eq 0 ]; then
     pkill factomd
     echo "Running..."
-    factomd -count=1 -folder="test1-" -network="TEST" -networkPort=8118 -peers="127.0.0.1:8121" -netdebug=2 -db=Map & node0=$!
+    factomd -count=1 -folder="test1-" -network="TEST" -networkPort=8118 -peers="127.0.0.1:8121" -netdebug=1 -db=Map & node0=$!
     sleep 6
-    factomd -count=1 -prefix="test2-" -port=9121 -networkPort=8119 -peers="127.0.0.1:8118" -netdebug=2 -db=Map & node1=$!
-    sleep 6
-    factomd -count=1 -prefix="test3-" -port=9122 -networkPort=8120 -peers="127.0.0.1:8119" -netdebug=2 -db=Map & node2=$!
-    sleep 6
-    factomd -count=1 -prefix="test4-" -port=9123 -networkPort=8121  -peers="127.0.0.1:8120" -netdebug=2 -db=Map & node3=$!
+    factomd -count=1 -prefix="test2-" -network="TEST" -port=9121 -networkPort=8119 -peers="127.0.0.1:8118" -netdebug=1 -db=Map & node1=$!
+    # sleep 6
+    # factomd -count=1 -prefix="test3-" -network="TEST" -port=9122 -networkPort=8120 -peers="127.0.0.1:8119" -netdebug=1 -db=Map & node2=$!
+    # sleep 6
+    # factomd -count=1 -prefix="test4-" -network="TEST" -port=9123 -networkPort=8121  -peers="127.0.0.1:8120" -netdebug=1 -db=Map & node3=$!
     echo
     echo
     sleep 480
