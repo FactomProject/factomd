137834562dc4c5e338a47ae541005ceaf94ca620
Steven Masley <stevenmasley@gmail.com>
2016-08-03 14:11:50 -0500

    Server faulted Audit servers should be able to sign

--- state/authority.go
+++ state/authority.go
@@ -56,9 +56,9 @@ func (auth *Authority) VerifySignature(msg []byte, sig *[constants.SIGNATURE_LEN
 // Also checks Identity list which contains pending Fed/Aud servers. TODO: Remove those
 func (st *State) VerifyFederatedSignature(msg []byte, sig *[constants.SIGNATURE_LENGTH]byte) (bool, error) {
 	for _, auth := range st.Authorities {
-		if !(auth.Status == constants.IDENTITY_FEDERATED_SERVER || auth.Status == constants.IDENTITY_PENDING_FEDERATED_SERVER) {
-			continue
-		}
+		//if !(auth.Status == constants.IDENTITY_FEDERATED_SERVER || auth.Status == constants.IDENTITY_PENDING_FEDERATED_SERVER) {
+		//	continue
+		//}
 		valid, err := auth.VerifySignature(msg, sig)
 		if err != nil {
 			continue
