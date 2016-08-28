5e6000ba61162c211b03cca116397be58dd701ec
Nirvana <nirvana@myster.io>
2016-08-28 14:13:55 -0500

    bump network version so we can reset the testnet chain

--- p2p/protocol.go
+++ p2p/protocol.go
@@ -70,9 +70,9 @@ var (
 
 const (
 	// ProtocolVersion is the latest version this package supports
-	ProtocolVersion uint16 = 04
+	ProtocolVersion uint16 = 05
 	// ProtocolVersionMinimum is the earliest version this package supports
-	ProtocolVersionMinimum uint16 = 04
+	ProtocolVersionMinimum uint16 = 05
 	// Don't think we need this.
 	// ProtocolCookie         uint32 = uint32([]bytes("Fact"))
 	// Used in generating message CRC values
