07a94ac039b9ab7c59cb089abdaa96986a078a45
Justin Hanneman <hannemennah@gmail.com>
2016-09-15 12:33:02 -0500

    Fix bug of bugfix (revert to old working way)

--- state/stateConsensus.go
+++ state/stateConsensus.go
@@ -1200,17 +1200,7 @@ func (s *State) ProcessDBSig(dbheight uint32, msg interfaces.IMsg) bool {
 		if dbs.VMIndex == 0 {
 			s.SetLeaderTimestamp(dbs.GetTimestamp())
 		}
-		dbsBodyMR := dbs.DirectoryBlockHeader.GetBodyMR()
-		prevDBState := s.GetDBState(dbheight - 1)
-		var prevBodyMR interfaces.IHash
-		if prevDBState != nil {
-			db, err := s.DB.FetchDBlockByHeight(dbheight - 1)
-			if err != nil || db == nil {
-				panic("Missing previous Directory Block for DBSig")
-			}
-			prevBodyMR = db.GetHeader().GetBodyMR()
-		}
-		if !dbsBodyMR.IsSameAs(prevBodyMR) {
+		if !dbs.DirectoryBlockHeader.GetBodyMR().IsSameAs(s.GetDBState(dbheight - 1).DirectoryBlock.GetHeader().GetBodyMR()) {
 			fmt.Println(s.FactomNodeName, "JUST COMPARED", dbs.DirectoryBlockHeader.GetBodyMR().String()[:10], " : ", s.GetDBState(dbheight - 1).DirectoryBlock.GetHeader().GetBodyMR().String()[:10])
 			pl.IncrementDiffSigTally()
 		}
