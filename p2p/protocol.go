47124a0ba8a29eac43e892ddfb8cd279671bda2c
Nirvana <nirvana@myster.io>
2016-09-19 14:40:20 -0500

    rev protocol version

--- p2p/protocol.go
+++ p2p/protocol.go
@@ -74,9 +74,9 @@ var (
 
 const (
 	// ProtocolVersion is the latest version this package supports
-	ProtocolVersion uint16 = 05
+	ProtocolVersion uint16 = 06
 	// ProtocolVersionMinimum is the earliest version this package supports
-	ProtocolVersionMinimum uint16 = 05
+	ProtocolVersionMinimum uint16 = 06
 	// Don't think we need this.
 	// ProtocolCookie         uint32 = uint32([]bytes("Fact"))
 	// Used in generating message CRC values
