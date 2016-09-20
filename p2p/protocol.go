605ed90c221ab20f0049ac369f0d113a03c20051
Nirvana <nirvana@myster.io>
2016-09-20 16:02:50 -0500

    Correct default network deadline, turn on dot

--- p2p/protocol.go
+++ p2p/protocol.go
@@ -49,7 +49,7 @@ var (
 	BannedQualityScore            int32  = -2147000000 // Used to ban a peer
 	MinumumSharingQualityScore    int32  = 20          // if a peer's score is less than this we don't share them.
 	OnlySpecialPeers                     = false
-	NetworkDeadline                      = time.Duration(1000) * time.Millisecond
+	NetworkDeadline                      = time.Duration(10000) * time.Millisecond
 	NumberPeersToConnect                 = 8
 	MaxNumberIncommingConnections        = 150
 	MaxNumberOfRedialAttempts            = 15
@@ -133,7 +133,7 @@ var LoggingLevels = map[uint8]string{
 }
 
 func dot(dot string) {
-	if 5 < CurrentLoggingLevel {
+	if 0 < CurrentLoggingLevel {
 		switch dot {
 		case "":
 			fmt.Printf(".")
