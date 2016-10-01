f2f7e357c49b20fcd40396182acdd456b7c36d76
Justin Hanneman <hannemennah@gmail.com>
2016-10-01 03:27:10 -0500

    Add shownetwork.sh script (for easy arbor.js halfviz)

---
+++ scripts/shownetwork.sh
@@ -0,0 +1,12 @@
+#!/bin/bash         
+echo "; Default node color: Gray";
+echo "{color:#555555}";
+head -1000 out.txt | gawk -f scripts/showconnections.awk
+echo "; Federated Servers: Blue";
+echo "{color:#3333ff}"; 
+tail -10000 out.txt | gawk -f scripts/status.awk | awk '{if ($2 ~ /FNode.*\[/) printf "%s\n", $0; else if ($3 ~ /FNode.*\[/) printf "%s\n", $0;}' | sed 's/\[/ /g' | awk '{if ($5 == "L") print $2; else if ($6 == "L") print $3;}' | sed 's/FNode//g'
+echo "; Audit Servers: Green";
+echo "{color:#338833}";
+#tail -10000 out.txt | gawk -f scripts/status.awk | awk '{if ($2 ~ /FNode.*\[/) printf "%s\n", $0;}' | sed 's/\[/ /g' | awk '{if ($4 == "A") print $2;}' | sed 's/FNode//g'
+tail -10000 out.txt | gawk -f scripts/status.awk | awk '{if ($2 ~ /FNode.*\[/) printf "%s\n", $0; else if ($3 ~ /FNode.*\[/) printf "%s\n", $0;}' | sed 's/\[/ /g' | awk '{if ($4 == "A") print $2; else if ($5 == "A") print $3;}' | sed 's/FNode//g'
+
