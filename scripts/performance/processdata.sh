73c095df7f0cda6960dbf96196f341d3824d47d0
Paul Snow <snow.paul@gmail.com>
2016-10-03 12:00:43 -0500

    Updates to performance scripts, and to the testing script

--- scripts/performance/processdata.sh
+++ scripts/performance/processdata.sh
@@ -1,6 +1,9 @@
 if [ -z "$1" ]; then
     	echo "Usage:"
-    	echo "    ./processdata.sh blktime=120 <test name>"
+    	echo "    ./processdata.sh <test name>"
 else
-	gawk -f process.awk $1 $2.out > $2.process.out
+        grep "0     FNode0" $1.out.txt | tail -n 1 > $1.process.out
+	echo "Real Time (days)	Time (Days)	%CPU	%MEM	% of One Core	Memory" >> $1.process.out
+	gawk -f process.awk $1.out >> $1.process.out
+
 fi
