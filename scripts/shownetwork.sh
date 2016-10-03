9d9500eb7135f2dded37fb203e11e89bccf18088
Justin Hanneman <hannemennah@gmail.com>
2016-10-03 01:53:05 -0500

    Improve shownetwork script

--- scripts/shownetwork.sh
+++ scripts/shownetwork.sh
@@ -4,7 +4,7 @@ echo "{color:#555555}";
 head -1000 out.txt | gawk -f scripts/showconnections.awk
 echo "; Federated Servers: Blue";
 echo "{color:#3333ff}"; 
-tail -10000 out.txt | gawk -f scripts/status.awk | awk '{if ($2 ~ /FNode.*\[/) printf "%s\n", $0; else if ($3 ~ /FNode.*\[/) printf "%s\n", $0;}' | sed 's/\[/ /g' | awk '{if ($5 == "L") print $2; else if ($6 == "L") print $3;}' | sed 's/FNode//g'
+tac out.txt | awk '/SummaryEnd/ {p=1; split($0, a, "SummaryEnd"); $0=a[1]}; /SummaryStart/ {p=0; split($0, a, "SummaryStart"); $0=a[2]; print; exit}; p' | tac | awk '{if ($2 ~ /FNode.*\[/) printf "%s\n", $0; else if ($3 ~ /FNode.*\[/) printf "%s\n", $0;}' | sed 's/\[/ /g' | awk '{if ($5 == "L") print $2; else if ($6 == "L") print $3;}' | sed 's/FNode//g'
 echo "; Audit Servers: Green";
 echo "{color:#338833}";
-tail -10000 out.txt | gawk -f scripts/status.awk | awk '{if ($2 ~ /FNode.*\[/) printf "%s\n", $0; else if ($3 ~ /FNode.*\[/) printf "%s\n", $0;}' | sed 's/\[/ /g' | awk '{if ($4 == "A") print $2; else if ($5 == "A") print $3;}' | sed 's/FNode//g'
+tac out.txt | awk '/SummaryEnd/ {p=1; split($0, a, "SummaryEnd"); $0=a[1]}; /SummaryStart/ {p=0; split($0, a, "SummaryStart"); $0=a[2]; print; exit}; p' | tac | awk '{if ($2 ~ /FNode.*\[/) printf "%s\n", $0; else if ($3 ~ /FNode.*\[/) printf "%s\n", $0;}' | sed 's/\[/ /g' | awk '{if ($4 == "A") print $2; else if ($5 == "A") print $3;}' | sed 's/FNode//g'
