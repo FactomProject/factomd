6e890a4901634d083fdab1362489e8a9848ba0cc
Justin Hanneman <hannemennah@gmail.com>
2016-10-03 04:18:58 -0500

    Clean up shownetwork

--- scripts/shownetwork.sh
+++ scripts/shownetwork.sh
@@ -10,4 +10,4 @@ echo "{color:#338833}";
 tac out.txt | grep -m1 -B3000 'SummaryStart' | tac | sed '/SummaryEnd/q' | awk '{if ($2 ~ /FNode.*\[/) printf "%s\n", $0; else if ($3 ~ /FNode.*\[/) printf "%s\n", $0;}' | sed 's/\[/ /g' | awk '{if ($4 ~ "A") print $2; else if ($5 ~ "A") print $3;}' | sed 's/FNode//g'
 echo "; Offline Servers: Red";
 echo "{color:#ff0000}";
-tac out.txt | grep -m1 -B3000 'SummaryStart' | tac | sed '/SummaryEnd/q' | awk '{if ($2 ~ /FNode.*\[/) printf "%s\n", $0; else if ($3 ~ /FNode.*\[/) printf "%s\n", $0;}' | sed 's/\[/ /g' | awk '{if ($4 ~ "X") print $2; else if ($5 ~ "X") print $2;}' | sed 's/FNode//g'
+tac out.txt | grep -m1 -B3000 'SummaryStart' | tac | sed '/SummaryEnd/q' | awk '{if ($2 ~ /FNode.*\[/) printf "%s\n", $0; else if ($3 ~ /FNode.*\[/) printf "%s\n", $0;}' | sed 's/\[/ /g' | awk '{if ($4 == "AX") print $2; else if ($5 == "AX") print $3; else if ($5== "LX") print $2; else if ($6 == "LX") print $3; else if ($4 == "X") print $2; else if ($5 == "X") print $3;}' | sed 's/FNode//g'
