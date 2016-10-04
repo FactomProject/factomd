a9c9c848e461317ee8602f4df2529a079bdb5cef
Paul Snow <snow.paul@gmail.com>
2016-10-03 19:40:50 -0500

    load test script

--- scripts/entryload2.sh
+++ scripts/entryload2.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-for j in `seq 1 2800`;
+for j in `seq 1 2000`;
 do
  echo $loopi $j "entry" | factom-cli2 put -c ${chain1} e1 &
  echo $loopi $j "entry" | factom-cli2 put -c ${chain2} e1 &
  echo $loopi $j "entry" | factom-cli2 put -c ${chain3} e1 &
- sleep .02s
+ sleep .024s
 done
 echo $loopi complete!
 
