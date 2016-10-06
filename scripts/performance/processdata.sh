41bf6ba10e55b38ab3e9df4eab687c044dad504e
Paul Snow <snow.paul@gmail.com>
2016-10-06 11:44:42 -0500

    Updated process data to better handle state in status

--- scripts/performance/processdata.sh
+++ scripts/performance/processdata.sh
@@ -2,7 +2,7 @@ if [ -z "$1" ]; then
     	echo "Usage:"
     	echo "    ./processdata.sh <test name>"
 else
-        grep "0     FNode0" $1.out.txt | tail -n 1 > $1.process.out
+        grep "0 ... FNode0" $1.out.txt | tail -n 1 > $1.process.out
 	echo "Real Time (days)	Time (Days)	%CPU	%MEM	% of One Core	Memory" >> $1.process.out
 	gawk -f process.awk $1.out >> $1.process.out
 
