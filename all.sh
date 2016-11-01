8141c86cbc4e7dc5d9555a55bffafa4a6b9a0d1d
Brian Deery <brian@factom.org>
2016-10-31 21:57:29 -0500

    fix all.sh not getting new branches

--- all.sh
+++ all.sh
@@ -59,6 +59,7 @@ checkout() {
     cd $1 $2 > /dev/null 2>&1
     if [ $? -eq 0 ]; then
         echo $1 | awk "{printf(\"%15s\",\"$1\")}"
+        git fetch -q
         git checkout -q $2 > /dev/null 2>&1
         if [ $? -eq 0 ]; then
             echo -e -n " now on" $2    # checkout did not fail
