fe6caa7905eb60f511da3547e283888be3f96ec7
Paul Snow <snow.paul@gmail.com>
2016-12-20 21:05:23 -0600

    Change custom ids and keys to match main net

--- state/state.go
+++ state/state.go
@@ -1893,7 +1893,8 @@ func (s *State) GetNetworkBootStrapKey() interfaces.IHash {
 		key, _ := primitives.HexToHash("cc1985cdfae4e32b5a454dfda8ce5e1361558482684f3367649c3ad852c8e31a")
 		return key
 	case constants.NETWORK_CUSTOM:
-		key, _ := primitives.HexToHash("cc1985cdfae4e32b5a454dfda8ce5e1361558482684f3367649c3ad852c8e31a")
+		key, _ := primitives.HexToHash("0426a802617848d4d16d87830fc521f4d136bb2d0c352850919c2679f189613a") // main net
+		//key, _ := primitives.HexToHash("cc1985cdfae4e32b5a454dfda8ce5e1361558482684f3367649c3ad852c8e31a") // same as local
 		return key
 	}
 	return primitives.NewZeroHash()
@@ -1910,7 +1911,8 @@ func (s *State) GetNetworkBootStrapIdentity() interfaces.IHash {
 		id, _ := primitives.HexToHash("38bab1455b7bd7e5efd15c53c777c79d0c988e9210f1da49a99d95b3a6417be9")
 		return id
 	case constants.NETWORK_CUSTOM:
-		id, _ := primitives.HexToHash("38bab1455b7bd7e5efd15c53c777c79d0c988e9210f1da49a99d95b3a6417be9")
+		id, _ := primitives.HexToHash("0000000000000000000000000000000000000000000000000000000000000000")	// production
+		//id, _ := primitives.HexToHash("38bab1455b7bd7e5efd15c53c777c79d0c988e9210f1da49a99d95b3a6417be9") // same as local
 		return id
 	}
 	return primitives.NewZeroHash()
