6e8aff21ef008a7aa7eacabfa6a74613bd56c515
Paul Snow <snow.paul@gmail.com>
2016-12-22 13:08:55 -0600

    Getting some fixes back

--- state/stateConsensus.go
+++ state/stateConsensus.go
@@ -38,6 +38,13 @@ func (s *State) executeMsg(vm *VM, msg interfaces.IMsg) (ret bool) {
 	s.SetString()
 	msg.ComputeVMIndex(s)
 
+	if s.IgnoreMissing {
+		now := s.GetTimestamp().GetTimeSeconds()
+		if now-msg.GetTimestamp().GetTimeSeconds() > 60*15 {
+			return
+		}
+	}
+
 	switch msg.Validate(s) {
 	case 1:
 		if s.RunLeader &&
@@ -125,7 +132,14 @@ ackLoop:
 		case ack := <-s.ackQueue:
 			a := ack.(*messages.Ack)
 			if a.DBHeight >= s.LLeaderHeight && ack.Validate(s) == 1 {
-				ack.FollowerExecute(s)
+				if s.IgnoreMissing {
+					now := s.GetTimestamp().GetTimeSeconds()
+					if now-a.GetTimestamp().GetTimeSeconds() < 60*15 {
+						ack.FollowerExecute(s)
+					}
+				} else {
+					ack.FollowerExecute(s)
+				}
 			}
 			progress = true
 		default:
