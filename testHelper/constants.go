6d0d43adeb591a8aa70b06033c74f562755b40eb
ThePiachu <ThePiachu@gmail.com>
2017-01-06 17:15:29 -0800

    Added some constants to TestHelper

---
+++ testHelper/constants.go
@@ -0,0 +1,21 @@
+package testHelper
+
+const (
+	DBlockHeadPrimaryIndex   = "eb222f3c8fc097edd39acce8a38f01580947a5e8e24e3a9a90a75f8eaafd4dc2"
+	DBlockHeadSecondaryIndex = "d852ca5c299b15ad155db7015377b70a1dca008d2192f3ba84f3669298c79f3d"
+
+	ABlockHeadPrimaryIndex   = "956c41312070f58c628ca8027297e0af0aaaa7b8af7f84283fc5ad21a49cc00a"
+	ABlockHeadSecondaryIndex = "b07a252e7ff13ef3ae6b18356949af34f535eca0383a03f71f5f4c526c58b562"
+
+	ECBlockHeadPrimaryIndex   = "69bd749e7697642c218ac33ebb4283e477de540b6dfd00033a7301d8f4cc3ac6"
+	ECBlockHeadSecondaryIndex = "10ab25c2420ad107ec3056f964687ceece50a1d752cd13ac2fbf44241bae9d55"
+
+	FBlockHeadPrimaryIndex   = "c6cd2ab21d75af1e8589e1eb441411838a508d0674eb294bac4efdc591c3fef4"
+	FBlockHeadSecondaryIndex = "e6e8b0a9808bf9ffb53d04acff0dcafc2d5fc7139ef850ab1a5fc94dfd87931e"
+
+	EBlockHeadPrimaryIndex   = "6e7e64ac45ff57edbf8537a0c99fba2e9ee351ef3d3f4abd93af9f01107e592c"
+	EBlockHeadSecondaryIndex = "1127ed78303976572f25dfba2a058e475234c079ea0d0f645280d03caff08347"
+
+	AnchorBlockHeadPrimaryIndex   = "df3ade9eec4b08d5379cc64270c30ea7315d8a8a1a69efe2b98a60ecdd69e604"
+	AnchorBlockHeadSecondaryIndex = "03897216bbdfb49f7fa17f6e86e9e1044499c94a49cfe1c3ddd02a2ca00521da"
+)
