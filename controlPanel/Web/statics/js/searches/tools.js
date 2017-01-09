5e82c043b7e6170e05a556e413cd37ec1c7beba0
Steven Masley <stevenmasley@gmail.com>
2017-01-09 14:55:44 -0600

    Removed the "uncompressed statics", it was marginally larger than the compressed, and easier to read, so now our js is no longer compressed.
    Compiled those files into the go files

--- controlPanel/Web/statics/js/searches/tools.js
+++ controlPanel/Web/statics/js/searches/tools.js
@@ -1,1 +1,40 @@
-function convertFromHex(a){for(var a=a.toString(),b="",c=0;c<a.length;c+=2)b+=String.fromCharCode(parseInt(a.substr(c,2),16));return b}function convertToHex(a){for(var b="",c=0;c<a.length;c++)b+=""+a.charCodeAt(c).toString(16);return b}$("#entry-external-id > #encoding").click(function(){encoding=jQuery(this).text(),encoding.indexOf("Hex")>-1?(jQuery(this).find("a").text("Ascii: "),str=convertFromHex(jQuery(this).parent().find("#data").text()),jQuery(this).parent().find("#data").text(str)):encoding.indexOf("Ascii")>-1&&(jQuery(this).find("a").text("Hex : "),str=convertToHex(jQuery(this).parent().find("#data").text()),jQuery(this).parent().find("#data").text(str))}),$("td > #entry-content-summary > a").click(function(){td=jQuery(this).parent().parent(),td.find("#entry-content-summary").hide(),td.find("#entry-content-body").show()}),$("td > #entry-content-body > a").click(function(){td=jQuery(this).parent().parent(),td.find("#entry-content-body").hide(),td.find("#entry-content-summary").show()});
+$("#entry-external-id > #encoding").click(function() {
+  encoding = jQuery(this).text()
+  if(encoding.indexOf("Hex") > -1){
+  	jQuery(this).find("a").text("Ascii: ")
+  	str = convertFromHex(jQuery(this).parent().find("#data").text())
+	jQuery(this).parent().find("#data").text(str)
+  } else if (encoding.indexOf("Ascii") > -1){
+	jQuery(this).find("a").text("Hex : ")
+	str = convertToHex(jQuery(this).parent().find("#data").text())
+	jQuery(this).parent().find("#data").text(str)
+  }
+})
+
+function convertFromHex(hex) {
+    var hex = hex.toString();//force conversion
+    var str = '';
+    for (var i = 0; i < hex.length; i += 2)
+        str += String.fromCharCode(parseInt(hex.substr(i, 2), 16));
+    return str;
+}
+
+function convertToHex(str) {
+    var hex = '';
+    for(var i=0;i<str.length;i++) {
+        hex += ''+str.charCodeAt(i).toString(16);
+    }
+    return hex;
+}
+
+$("td > #entry-content-summary > a").click(function() {
+  td = jQuery(this).parent().parent()
+  td.find("#entry-content-summary").hide()
+  td.find("#entry-content-body").show()//slideDown(100)
+})
+
+$("td > #entry-content-body > a").click(function() {
+  td = jQuery(this).parent().parent()
+    td.find("#entry-content-body").hide()
+  td.find("#entry-content-summary").show()//slideDown(100)
+})
