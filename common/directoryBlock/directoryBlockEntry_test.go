d8146c9b29d7973d7ff10d7e94bb869a1d6d470a
ThePiachu <ThePiachu@gmail.com>
2017-01-18 05:33:47 -0800

    Testing DBlock

--- common/directoryBlock/directoryBlockEntry_test.go
+++ common/directoryBlock/directoryBlockEntry_test.go
@@ -5,10 +5,30 @@
 package directoryBlock_test
 
 import (
+	"testing"
+
 	. "github.com/FactomProject/factomd/common/directoryBlock"
 	"github.com/FactomProject/factomd/common/primitives"
-	"testing"
 )
+
+func TestUnmarshalNilDBEntry(t *testing.T) {
+	defer func() {
+		if r := recover(); r != nil {
+			t.Errorf("Panic caught during the test - %v", r)
+		}
+	}()
+
+	a := new(DBEntry)
+	err := a.UnmarshalBinary(nil)
+	if err == nil {
+		t.Errorf("Error is nil when it shouldn't be")
+	}
+
+	err = a.UnmarshalBinary([]byte{})
+	if err == nil {
+		t.Errorf("Error is nil when it shouldn't be")
+	}
+}
 
 func TestDBSEMisc(t *testing.T) {
 	dbe := new(DBEntry)
--- common/directoryBlock/directoryBlock_test.go
+++ common/directoryBlock/directoryBlock_test.go
@@ -17,6 +17,25 @@ import (
 	"github.com/FactomProject/factomd/common/primitives"
 )
 
+func TestUnmarshalNilDirectoryBlock(t *testing.T) {
+	defer func() {
+		if r := recover(); r != nil {
+			t.Errorf("Panic caught during the test - %v", r)
+		}
+	}()
+
+	a := new(DirectoryBlock)
+	err := a.UnmarshalBinary(nil)
+	if err == nil {
+		t.Errorf("Error is nil when it shouldn't be")
+	}
+
+	err = a.UnmarshalBinary([]byte{})
+	if err == nil {
+		t.Errorf("Error is nil when it shouldn't be")
+	}
+}
+
 func TestMarshalUnmarshalDirectoryBlockHeader(t *testing.T) {
 	header := createTestDirectoryBlockHeader()
 
