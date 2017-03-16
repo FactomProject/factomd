27b1b5f5587098b092ed4bcd76b33d69e5b90d8a
Justin Hanneman <hannemennah@gmail.com>
2017-03-16 13:37:55 -0500

    Move dbhash and keymr to top of cli output for 'get dbheight' calls

--- common/directoryBlock/directoryBlock.go
+++ common/directoryBlock/directoryBlock.go
@@ -17,13 +17,13 @@ import (
 var _ = fmt.Print
 
 type DirectoryBlock struct {
-	//Marshalized
-	Header    interfaces.IDirectoryBlockHeader
-	DBEntries []interfaces.IDBEntry
-
 	//Not Marshalized
 	DBHash interfaces.IHash
 	KeyMR  interfaces.IHash
+
+	//Marshalized
+	Header    interfaces.IDirectoryBlockHeader
+	DBEntries []interfaces.IDBEntry
 }
 
 var _ interfaces.Printable = (*DirectoryBlock)(nil)
