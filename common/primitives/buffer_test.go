265965d29f56770bf5052571186ccd16fd4709c3
ThePiachu <ThePiachu@gmail.com>
2017-04-05 06:16:36 -0700

    Added more unit tests

--- common/primitives/buffer_test.go
+++ common/primitives/buffer_test.go
@@ -115,7 +115,7 @@ func TestPushPopUInt64(t *testing.T) {
 	}
 }
 
-func TestPushPopVArInt(t *testing.T) {
+func TestPushPopVarInt(t *testing.T) {
 	b := NewBuffer(nil)
 
 	var i uint64
@@ -156,6 +156,106 @@ func TestPushPopVArInt(t *testing.T) {
 		}
 		if i != r {
 			t.Errorf("Received wrong number - %v vs %v", i, r)
+		}
+	}
+}
+
+func TestPushPopString(t *testing.T) {
+	b := NewBuffer(nil)
+	for i := 0; i < 1000; i++ {
+		str := random.RandomString()
+		err := b.PushString(str)
+		if err != nil {
+			t.Errorf("%v", err)
+		}
+		r, err := b.PopString()
+		if err != nil {
+			t.Errorf("%v", err)
+		}
+		if str != r {
+			t.Errorf("Received wrong string - %v vs %v", str, r)
+		}
+	}
+}
+
+func TestPushPopBytes(t *testing.T) {
+	buf := NewBuffer(nil)
+	for i := 0; i < 1000; i++ {
+		b := random.RandByteSlice()
+		err := buf.PushBytes(b)
+		if err != nil {
+			t.Errorf("%v", err)
+		}
+		r, err := buf.PopBytes()
+		if err != nil {
+			t.Errorf("%v", err)
+		}
+		if AreBytesEqual(b, r) == false {
+			t.Errorf("Received wrong byte slice - %x vs %x", b, r)
+		}
+	}
+}
+
+func TestPushPopUInt32(t *testing.T) {
+	b := NewBuffer(nil)
+
+	var i uint32
+	err := b.PushUInt32(i)
+	if err != nil {
+		t.Errorf("%v", err)
+	}
+	r, err := b.PopUInt32()
+	if err != nil {
+		t.Errorf("%v", err)
+	}
+	if i != r {
+		t.Errorf("Received wrong number - %v vs %v", i, r)
+	}
+
+	i = math.MaxUint32
+	err = b.PushUInt32(i)
+	if err != nil {
+		t.Errorf("%v", err)
+	}
+	r, err = b.PopUInt32()
+	if err != nil {
+		t.Errorf("%v", err)
+	}
+	if i != r {
+		t.Errorf("Received wrong number - %v vs %v", i, r)
+	}
+
+	for j := 0; j < 1000; j++ {
+		i = random.RandUInt32()
+		err = b.PushUInt32(i)
+		if err != nil {
+			t.Errorf("%v", err)
+		}
+		r, err = b.PopUInt32()
+		if err != nil {
+			t.Errorf("%v", err)
+		}
+		if i != r {
+			t.Errorf("Received wrong number - %v vs %v", i, r)
+		}
+	}
+}
+
+func TestPushPopBinaryMarshallable(t *testing.T) {
+	b := NewBuffer(nil)
+	for i := 0; i < 1000; i++ {
+		h := RandomHash()
+		err := b.PushBinaryMarshallable(h)
+		if err != nil {
+			t.Errorf("%v", err)
+		}
+		h2 := new(Hash)
+		err = b.PopBinaryMarshallable(h2)
+		if err != nil {
+			t.Errorf("%v", err)
+		}
+		if h.IsSameAs(h2) == false {
+			t.Errorf("Received wrong hash - %v vs %v", h, h2)
 		}
 	}
 }
