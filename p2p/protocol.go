9023f6059f46297764c0f4277c0d4fee4a66c751
Brian Deery <brian@factom.org>
2017-04-21 18:34:40 -0500

    bump protocol version

--- p2p/protocol.go
+++ p2p/protocol.go
@@ -74,9 +74,9 @@ var (
 
 const (
 	// ProtocolVersion is the latest version this package supports
-	ProtocolVersion uint16 = 07
+	ProtocolVersion uint16 = 08
 	// ProtocolVersionMinimum is the earliest version this package supports
-	ProtocolVersionMinimum uint16 = 07
+	ProtocolVersionMinimum uint16 = 08
 	// Don't think we need this.
 	// ProtocolCookie         uint32 = uint32([]bytes("Fact"))
 	// Used in generating message CRC values
