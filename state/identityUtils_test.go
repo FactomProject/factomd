86e5ed67ea14832ee48a76b0e99be63ff57cecca
ThePiachu <ThePiachu@gmail.com>
2017-04-26 13:20:45 -0700

    Fixed some unit tests

--- state/identityUtils_test.go
+++ state/identityUtils_test.go
@@ -122,36 +122,6 @@ func TestCheckTimestamp(t *testing.T) {
 	}
 }
 
-func TestCheckExternalIDsLength(t *testing.T) {
-	extIDs := [][]byte{
-		{0x00, 0x00, 0x00, 0x00, 0x00},                               // 5
-		{0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}, // 10
-		{0x00, 0x00, 0x00},                                           // 3
-		{0x00},                                                       // 1
-		{},                                                           // 0
-	}
-	lengths := []int{5, 10, 3, 1, 0}
-	lengthsBad := []int{5, 10, 3, 1, 1}
-	if CheckExternalIDsLength(extIDs, lengthsBad) {
-		t.Error("1: CheckExternalIDsLength check failed")
-	}
-
-	lengthsBad = []int{}
-	if CheckExternalIDsLength(extIDs, lengthsBad) {
-		t.Error("2: CheckExternalIDsLength check failed")
-	}
-
-	lengthsBad = []int{5, 10, 3, 1}
-	if CheckExternalIDsLength(extIDs, lengthsBad) {
-		t.Error("3: CheckExternalIDsLength check failed")
-	}
-
-	if !CheckExternalIDsLength(extIDs, lengths) {
-		t.Error("4: CheckExternalIDsLength check failed")
-	}
-
-}
-
 func TestCheckLength(t *testing.T) {
 	// Invalid
 	x := []byte{0x00, 0x00, 0x00, 0x00, 0x00}
