6f2b92aaba2764912f7b9d4996f1d44107f05935
ThePiachu <ThePiachu@gmail.com>
2017-04-27 12:28:13 -0700

    Adding some tests

--- common/meta/authority_test.go
+++ common/meta/authority_test.go
@@ -50,3 +50,23 @@ func TestAuthoritySignature(t *testing.T) {
 		t.Error("Authority Test Failed when checking sigs")
 	}
 }
+
+func TestMarshalJSON(t *testing.T) {
+	s := testHelper.CreateAndPopulateTestState()
+	idindex := s.CreateBlankFactomIdentity(primitives.NewZeroHash())
+	s.Identities[idindex].ManagementChainID = primitives.NewZeroHash()
+
+	index := s.AddAuthorityFromChainID(primitives.NewZeroHash())
+	s.Authorities[index].SigningKey = *(s.GetServerPublicKey())
+	s.Authorities[index].Status = 1
+
+	j, err := s.Authorities[index].MarshalJSON()
+	if err != nil {
+		t.Errorf("%v", err)
+	}
+
+	expected := `{"chainid":"0000000000000000000000000000000000000000000000000000000000000000","manageid":"0000000000000000000000000000000000000000000000000000000000000000","matroyshka":null,"signingkey":"cc1985cdfae4e32b5a454dfda8ce5e1361558482684f3367649c3ad852c8e31a","status":"federated","anchorkeys":null}`
+	if string(j) != expected {
+		t.Errorf("Invalid json returned - %v vs %v", string(j), expected)
+	}
+}
