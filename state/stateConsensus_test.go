cf970f99449c9873e578c6e4cfb66e06e0c62696
Paul Snow <snow.paul@gmail.com>
2017-04-28 13:21:29 -0500

    udpate test

--- state/stateConsensus_test.go
+++ state/stateConsensus_test.go
@@ -10,6 +10,8 @@ import (
 	"github.com/FactomProject/factomd/common/constants"
 	"github.com/FactomProject/factomd/common/messages"
 	"github.com/FactomProject/factomd/testHelper"
+	"github.com/FactomProject/factomd/common/primitives"
+	"time"
 )
 
 
@@ -26,7 +28,25 @@ func TestReveiwHolding(t *testing.T) {
 	// get rid of any "leavings"
 	s.XReview = s.XReview[:0]
 
+	for k := range s.Holding {
+		delete(s.Holding,k)
+	}
+
 	s.Holding[msg.GetHash().Fixed()] = msg
+
+	{
+		m := messages.NewServerFault(primitives.NewHash([]byte("ha")),primitives.NewHash([]byte("ha")),0,10,0,0,s.GetTimestamp())
+		s.Holding[m.GetHash().Fixed()]=m
+	}
+	{
+		m := messages.NewMissingMsgResponse(s,msg,msg)
+		s.Holding[primitives.Sha([]byte("missing message")).Fixed()]= m
+	}
+	{
+		sf := messages.NewServerFault(primitives.NewHash([]byte("ha")),primitives.NewHash([]byte("ha")),0,10,0,0,s.GetTimestamp())
+		m := messages.NewFullServerFault(nil,sf,nil,0)
+		s.Holding[m.GetHash().Fixed()]=m
+	}
 
 	s.ReviewHolding()
 
@@ -37,20 +57,21 @@ func TestReveiwHolding(t *testing.T) {
 	// set the review to now
 	s.ResendHolding = s.GetTimestamp()
 
+	time.Sleep(400 * time.Millisecond)
+
+	// get rid of any "leavings"
+	s.XReview = s.XReview[:0]
+
 	s.InMsgQueue().Enqueue(msg)
 
 	s.ReviewHolding()
-
-	if len(s.XReview) > 0 {
-		t.Error("Have too many entries in the InMsgQueue;  should not execute")
-	}
-
+	
 	s.InMsgQueue().Dequeue()
 
 	s.ReviewHolding()
 
 	if len(s.XReview) == 0 {
-	//	t.Error("Should have queued a message for reprocessing")
+		t.Error("Should have queued a message for reprocessing")
 	}
 
 	s.ReviewHolding()
