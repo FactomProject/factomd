5bc01640b164679cf889d92d3dca717f20ec48ed
Steven Masley <steven@factom.com>
2017-05-09 12:32:47 -0500

    Added more to test

--- engine/pluginMap_test.go
+++ engine/pluginMap_test.go
@@ -15,6 +15,46 @@ func TestNoPanicCompleteManager(t *testing.T) {
 	cm.ClearTo(10000)
 }
 
+func TestCompleteManagerBadInput(t *testing.T) {
+	cm := NewCompletedHeightManager()
+	// Test the underflow
+	if cm.CompleteHeight(-1) {
+		t.Error("Should be false")
+	}
+	if !cm.CompleteHeight(0) {
+		t.Error("Should be true")
+	}
+
+	// Test on the end
+	for i := 0; i < 10; i++ {
+		if !cm.CompleteHeight(len(cm.Completed)) {
+			t.Error("Should be true")
+		}
+	}
+
+	// Move the base
+	cm.ClearTo(1000)
+
+	// Test on the end + move
+	for i := 0; i < 10; i++ {
+		if !cm.CompleteHeight(1000 + len(cm.Completed)) {
+			t.Error("Should be true")
+		}
+	}
+
+	// First one is true, so throw that first for each checking
+	if !cm.CompleteHeight(1000 + len(cm.Completed) - 1) {
+		t.Error("Should be true")
+	}
+
+	// Test on the end -1
+	for i := 0; i < 10; i++ {
+		if cm.CompleteHeight(1000 + len(cm.Completed) - 1) {
+			t.Error("Should be false")
+		}
+	}
+}
+
 func TestCompleteManager(t *testing.T) {
 	cm := NewCompletedHeightManager()
 
