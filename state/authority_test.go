b65e8c9cb7abd6c3f038fd7edb5bb09396aa8d94
Justin Hanneman <hannemennah@gmail.com>
2017-05-16 23:01:43 -0500

    Add TestAuthorityType

--- state/authority_test.go
+++ state/authority_test.go
@@ -50,3 +50,31 @@ func TestAuthoritySignature(t *testing.T) {
 		t.Error("Authority Test Failed when checking sigs")
 	}
 }
+
+func TestAuthorityType(t *testing.T) {
+	s := testHelper.CreateAndPopulateTestState()
+	idindex := s.CreateBlankFactomIdentity(primitives.NewZeroHash())
+	s.Identities[idindex].ManagementChainID = primitives.NewZeroHash()
+
+	index := s.AddAuthorityFromChainID(primitives.NewZeroHash())
+	s.Authorities[index].SigningKey = *(s.GetServerPublicKey())
+	s.Authorities[index].Status = 1
+
+	if s.Authorities[index].Type() != 1 {
+		t.Error("Authority's Type isn't Leader even though Status is 1")
+	}
+
+	if s.GetAuthorityServerType(primitives.NewZeroHash()) != 0 {
+		t.Error("GetAuthorityServerType isn't 0 even though authority is Leader")
+	}
+
+	s.Authorities[index].Status = 2
+
+	if s.Authorities[index].Type() != 0 {
+		t.Error("Authority's Type isn't Audit even though Status is 0")
+	}
+
+	if s.GetAuthorityServerType(primitives.NewZeroHash()) != 1 {
+		t.Error("GetAuthorityServerType isn't 1 even though authority is Audit")
+	}
+}
