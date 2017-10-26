50c7c550bb11884bd256fb1866b704ee03d06311
Paul Snow <snow.paul@gmail.com>
2017-10-26 13:52:31 -0500

    Add sync2 flag

--- engine/NetStart.go
+++ engine/NetStart.go
@@ -216,6 +216,10 @@ func NetStart(s *state.State, p *FactomParams, listenToStdin bool) {
 	s.Init()
 	s.SetDropRate(p.DropRate)
 
+	if p.Sync2 > 0 {
+		s.EntryDBHeightComplete = uint32(p.Sync2)
+	}
+
 	mLog.Init(p.RuntimeLog, p.Cnt)
 
 	setupFirstAuthority(s)
@@ -249,6 +253,8 @@ func NetStart(s *state.State, p *FactomParams, listenToStdin bool) {
 	os.Stderr.WriteString(fmt.Sprintf("%20s %v\n", "tls", s.FactomdTLSEnable))
 	os.Stderr.WriteString(fmt.Sprintf("%20s %v\n", "selfaddr", s.FactomdLocations))
 	os.Stderr.WriteString(fmt.Sprintf("%20s \"%s\"\n", "rpcuser", s.RpcUser))
+	os.Stderr.WriteString(fmt.Sprintf("%20s %d\n", "Start 2nd Sync at ht", s.EntryDBHeightComplete))
+
 	if "" == s.RpcPass {
 		os.Stderr.WriteString(fmt.Sprintf("%20s %s\n", "rpcpass", "is blank"))
 	} else {
