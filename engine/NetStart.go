6bdc6d824e387c246e4f89c0946465acc922074c
Paul Snow <snow.paul@gmail.com>
2018-01-11 17:15:24 -0600

    Removed unnecessary go routine call to init wsapi

--- engine/NetStart.go
+++ engine/NetStart.go
@@ -493,7 +493,7 @@ func NetStart(s *state.State, p *FactomParams, listenToStdin bool) {
 	}
 
 	// Start the webserver
-	go wsapi.Start(fnodes[0].State)
+	wsapi.Start(fnodes[0].State)
 
 	// Start prometheus on port
 	launchPrometheus(9876)
