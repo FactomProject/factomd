71f0804c37ed4dd25474c37ee72b87a0c1d6362e
Paul Snow <snow.paul@gmail.com>
2018-01-27 14:22:29 -0600

    go fmt

--- common/adminBlock/EntryIncreaseServerCount.go
+++ common/adminBlock/EntryIncreaseServerCount.go
@@ -34,7 +34,7 @@ func (e *IncreaseServerCount) Type() byte {
 
 func (e *IncreaseServerCount) MarshalBinary() ([]byte, error) {
 	var buf primitives.Buffer
-	
+
 	e.AdminIDType = uint32(e.Type())
 
 	err := buf.PushByte(e.Type())
