b3b60a1075c9fe6e97382a14c40de85dd9caeb2f
Emyrk <stevenmasley@gmail.com>
2018-02-08 09:48:12 -0600

    Added more helper functions

---
+++ testhelper/authsethelper_test.go
@@ -0,0 +1,60 @@
+package testhelper_test
+
+import (
+	"testing"
+
+	"github.com/FactomProject/electiontesting/messages"
+	"github.com/FactomProject/electiontesting/primitives"
+	. "github.com/FactomProject/electiontesting/testhelper"
+)
+
+func TestMajority(t *testing.T) {
+	ah := NewAuthSetHelper(5, 5)
+	if ah.Majority() != 3 {
+		t.Errorf("majority should be 3, found %d", ah.Majority())
+	}
+
+	ah = NewAuthSetHelper(100, 5)
+	if ah.Majority() != 51 {
+		t.Errorf("majority should be 51, found %d", ah.Majority())
+	}
+
+	ah = NewAuthSetHelper(1, 5)
+	if ah.Majority() != 1 {
+		t.Error("majority should be 1, found %d", ah.Majority())
+	}
+}
+
+func TestVoteFactory(t *testing.T) {
+	ah := NewAuthSetHelper(5, 5)
+	var loc primitives.ProcessListLocation
+	eom := messages.NewEomMessage(ah.GetFeds()[0], loc)
+	vol := messages.NewVolunteerMessage(eom, ah.GetAuds()[0])
+	vf := ah.NewVoteFactory(vol)
+	for i := 0; i < len(ah.GetFeds()); i++ {
+		vote := vf.NextVote()
+		if vote.Volunteer != vol {
+			t.Error("Different volunteer that was passed")
+		}
+	}
+
+	if ah.Majority() != vf.Majority() {
+		t.Error("Majorties should be the same")
+	}
+
+	if len(vf.VotesListWithMajority()) != ah.Majority() {
+		t.Errorf("Number of votes in majority is not correct. Exp %d, got %d", len(vf.VotesListWithMajority()), ah.Majority())
+	}
+
+	if len(vf.MajorityDecisionListWithMajority()) != ah.Majority() {
+		t.Errorf("Number of votes in majority is not correct. Exp %d, got %d", len(vf.MajorityDecisionListWithMajority()), ah.Majority())
+	}
+
+	if len(vf.InsistenceListWithMajority()) != ah.Majority() {
+		t.Errorf("Number of votes in majority is not correct. Exp %d, got %d", len(vf.InsistenceListWithMajority()), ah.Majority())
+	}
+
+	if len(vf.VotesListWithMajority()) != ah.Majority() {
+		t.Errorf("Number of votes in majority is not correct. Exp %d, got %d", len(vf.VotesListWithMajority()), ah.Majority())
+	}
+}
