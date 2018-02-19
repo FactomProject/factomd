c8afd2218f2a21676e4b85d552c7a4a7e76739c3
Emyrk <stevenmasley@gmail.com>
2018-02-19 12:22:55 -0600

    added a little interpreter shell main to play with

--- interpreter/main.go
+++ interpreter/main.go
@@ -1,1 +1,35 @@
-package interpreter
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+
+	//"github.com/FactomProject/electiontesting/interpreter/interpreter"
+	"github.com/FactomProject/electiontesting/interpreter/primitives"
+)
+
+func main() {
+	i := primitives.NewPrimitives()
+	Shell(i)
+}
+
+func grabInput(in *bufio.Reader) string {
+	input, err := in.ReadString('\n')
+	if err != nil {
+		fmt.Println("Error: ", err)
+		return ""
+	}
+	return strings.TrimRight(input, "\n")
+}
+
+func Shell(i *primitives.Primitives) {
+	in := bufio.NewReader(os.Stdin)
+	for {
+		fmt.Print("> ")
+		input := grabInput(in)
+		i.Interpret(strings.NewReader(input))
+
+	}
+}
