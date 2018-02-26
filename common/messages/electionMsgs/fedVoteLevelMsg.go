cda9abd7e1d3a254f4439cfb1784fab5c441fcae
Emyrk <stevenmasley@gmail.com>
2018-02-26 14:38:11 -0600

    added level message for elections

---
+++ common/messages/electionMsgs/fedVoteLevelMsg.go
@@ -0,0 +1,156 @@
+// Copyright 2017 Factom Foundation
+// Use of this source code is governed by the MIT
+// license that can be found in the LICENSE file.
+
+package electionMsgs
+
+import (
+	"bytes"
+	"fmt"
+
+	"github.com/FactomProject/factomd/common/constants"
+	"github.com/FactomProject/factomd/common/interfaces"
+	"github.com/FactomProject/factomd/common/primitives"
+	"github.com/FactomProject/factomd/elections"
+	"github.com/FactomProject/goleveldb/leveldb/errors"
+	log "github.com/sirupsen/logrus"
+	//"github.com/FactomProject/factomd/state"
+	"time"
+)
+
+var _ = fmt.Print
+
+// FedVoteLevelMsg
+// We can vote on a majority of votes seen and issuing a vote at a particular
+// level. Only 1 vote per level can be issued per leader.
+type FedVoteLevelMsg struct {
+	FedVoteMsg
+	// Volunteer fields
+	EOM        bool             // True if an EOM, false if a DBSig
+	Name       string           // Server name
+	FedIdx     uint32           // Server faulting
+	FedID      interfaces.IHash // Server faulting
+	ServerIdx  uint32           // Index of Server replacing
+	ServerID   interfaces.IHash // Volunteer Server ChainID
+	ServerName string           // Volunteer Name
+	Weight     interfaces.IHash // Computed Weight at this DBHeight, Minute, Round
+	Missing    interfaces.IMsg  // The Missing DBSig or EOM
+	Ack        interfaces.IMsg  // The acknowledgement for the missing message
+
+	Committed bool
+	// Information about the vote for comparing
+	Level uint32
+	Rank  uint32
+
+	// Need a majority of these to justify our vote
+	Justification []FedVoteLevelMsg
+
+	// Previous Vote is used to help spread around information
+	PreviousVote FedVoteLevelMsg
+
+	messageHash interfaces.IHash
+}
+
+var _ interfaces.IMsg = (*FedVoteVolunteerMsg)(nil)
+var _ interfaces.IElectionMsg = (*FedVoteVolunteerMsg)(nil)
+
+func (m *FedVoteLevelMsg) ElectionProcess(is interfaces.IState, elect interfaces.IElections) {
+
+}
+
+var _ interfaces.IMsg = (*FedVoteVolunteerMsg)(nil)
+
+func (a *FedVoteLevelMsg) IsSameAs(msg interfaces.IMsg) bool {
+
+	return false
+}
+
+func (m *FedVoteLevelMsg) GetServerID() interfaces.IHash {
+	return m.ServerID
+}
+
+func (m *FedVoteLevelMsg) LogFields() log.Fields {
+	return log.Fields{"category": "message", "messagetype": "FedVoteVolunteerMsg", "dbheight": m.DBHeight, "newleader": m.ServerID.String()[4:12]}
+}
+
+func (m *FedVoteLevelMsg) GetRepeatHash() interfaces.IHash {
+	return m.GetMsgHash()
+}
+
+// We have to return the haswh of the underlying message.
+
+func (m *FedVoteLevelMsg) GetHash() interfaces.IHash {
+	return m.GetMsgHash()
+}
+
+func (m *FedVoteLevelMsg) GetTimestamp() interfaces.Timestamp {
+	return m.TS
+}
+
+func (m *FedVoteLevelMsg) GetMsgHash() interfaces.IHash {
+	if m.MsgHash == nil {
+		data, err := m.MarshalBinary()
+		if err != nil {
+			return nil
+		}
+		m.MsgHash = primitives.Sha(data)
+	}
+	return m.MsgHash
+}
+
+func (m *FedVoteLevelMsg) Type() byte {
+	return constants.VOLUNTEERAUDIT
+}
+
+func (m *FedVoteLevelMsg) Validate(state interfaces.IState) int {
+	return 1
+}
+
+// Returns true if this is a message for this server to execute as
+// a leader.
+func (m *FedVoteLevelMsg) ComputeVMIndex(state interfaces.IState) {
+}
+
+// Execute the leader functions of the given message
+// Leader, follower, do the same thing.
+func (m *FedVoteLevelMsg) LeaderExecute(state interfaces.IState) {
+	m.FollowerExecute(state)
+}
+
+func (m *FedVoteLevelMsg) FollowerExecute(state interfaces.IState) {
+	state.ElectionsQueue().Enqueue(m)
+}
+
+// Acknowledgements do not go into the process list.
+func (e *FedVoteLevelMsg) Process(dbheight uint32, state interfaces.IState) bool {
+	panic("Ack object should never have its Process() method called")
+}
+
+func (e *FedVoteLevelMsg) JSONByte() ([]byte, error) {
+	return primitives.EncodeJSON(e)
+}
+
+func (e *FedVoteLevelMsg) JSONString() (string, error) {
+	return primitives.EncodeJSONString(e)
+}
+
+func (m *FedVoteLevelMsg) UnmarshalBinaryData(data []byte) (newData []byte, err error) {
+
+	return
+}
+
+func (m *FedVoteLevelMsg) UnmarshalBinary(data []byte) error {
+	_, err := m.UnmarshalBinaryData(data)
+	return err
+}
+
+func (m *FedVoteLevelMsg) MarshalBinary() (data []byte, err error) {
+	var buf primitives.Buffer
+
+	data = buf.DeepCopyBytes()
+	return data, nil
+}
+
+func (m *FedVoteLevelMsg) String() string {
+	return ""
+}
