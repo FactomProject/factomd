fd35c6d2ed50bca994dda993896e69b716d73519
Emyrk <stevenmasley@gmail.com>
2018-03-01 11:33:04 -0600

    Bring in Pauls changes from 'pas' branch

--- ET2/dive/dive.go
+++ ET2/dive/dive.go
@@ -11,6 +11,8 @@ import (
 
 	"crypto/sha256"
 
+	"time"
+
 	"github.com/FactomProject/electiontesting/messages"
 	"github.com/FactomProject/electiontesting/primitives"
 	"github.com/dustin/go-humanize"
@@ -30,7 +32,8 @@ var failuresAt []int
 var hitlimit int
 var maxdepth int
 var failure int
-var errConclusions int
+var errCollision int
+var winners []int
 
 var globalRunNumber = 0
 
@@ -40,11 +43,14 @@ var audsMap = make(map[primitives.Identity]int)
 var extraPrints = true
 var extraPrints1 = true
 var extraPrints2 = true
+var extraPrints3 = true
 var insanePrints = false
+
+var last time.Time
 
 //================ main =================
 func Main() {
-	recurse(2, 5, 100)
+	recurse(5, 4, 90)
 }
 
 // newElections will return an array of elections (1 per leader) and an array
@@ -58,7 +64,7 @@ func Main() {
 //			controller *Controller  This can used for debugging (Printing votes)
 //			elections []*election   Nodes you can execute on (returns msg, statchange)
 //			volmsgs   []*VoluntMsg	Volunteer msgs you can start things with
-func newElections(feds, auds int, noDisplay bool) (*controller.Controller, []*election.Election, []*controller.DirectedMsg) {
+func newElections(feds, auds int, noDisplay bool) (*controller.Controller, []*election.Election, []*mymsg) {
 	con := controller.NewController(feds, auds)
 
 	if noDisplay {
@@ -67,15 +73,15 @@ func newElections(feds, auds int, noDisplay bool) (*controller.Controller, []*el
 		}
 		con.GlobalDisplay = nil
 	}
-	var msgs []*controller.DirectedMsg
+	var msgs []*mymsg
 	fmt.Println("Starting")
 	for _, v := range con.Volunteers {
 		for i, _ := range con.Elections {
-			my := new(controller.DirectedMsg)
-			my.LeaderIdx = i
-			my.Msg = v
+			my := new(mymsg)
+			my.leaderIdx = i
+			my.msg = v
 			msgs = append(msgs, my)
-			fmt.Println(my.Msg.String(), my.LeaderIdx)
+			fmt.Println(my.msg.String(), my.leaderIdx)
 		}
 	}
 
@@ -103,7 +109,7 @@ type mymsg struct {
 
 var cnt = 0
 
-// Dive
+// dive
 // Pass a list of messages to process, to a set of leaders, at a current depth, with a particular limit.
 // Provided a msgPath, and updated for recording purposes.
 // Returns
@@ -113,83 +119,25 @@ var cnt = 0
 // Note that we actually dive 100 levels beyond our limit, and declare seeSuccess past our limit as proof we are
 // in a loop.
 // Hitting the limit and seeSuccess is proof of a loop that none the less can resolve.
-func Dive(msgs []*controller.DirectedMsg, leaders []*election.Election, depth int, limit int, msgPath []*controller.DirectedMsg) (limitHit bool, leaf bool, seeSuccess bool) {
+func dive(msgs []*mymsg, leaders []*election.Election, depth int, limit int, msgPath []*mymsg) (limitHit bool, leaf bool, seeSuccess bool) {
 	depths = incCounter(depths, depth)
 	depth++
 
-	if globalRunNumber < 1000 || globalRunNumber%50000 == 0 {
+	now := time.Now()
+
+	prtTime := now.Unix()-last.Unix() > 10
+	if globalRunNumber < 1000 || prtTime {
 		extraPrints = true
 		extraPrints1 = true
 		extraPrints2 = true
-	}
-
-	printState := func() {
-		fmt.Printf("%s%s%4d%s%4d %s %12s %s%12s %s%3d %s%12s %12s  %s %12s %s %12s %s %12s %s %12s %s %12s", "=============== ",
-			" Depth=", depth, "/", maxdepth,
-			"| Multiple Conclusions", humanize.Comma(int64(errConclusions)),
-			"| Failures=", humanize.Comma(int64(failure)),
-			"| MsgQ=", len(msgs),
-			"| Mirrors=", humanize.Comma(int64(mirrors)), humanize.Comma(int64(len(mirrorMap))),
-			"| Hit the Limits=", humanize.Comma(int64(hitlimit)),
-			"| Breadth=", humanize.Comma(int64(breadth)),
-			"| solutions so far =", humanize.Comma(int64(solutions)),
-			"| global count= ", humanize.Comma(int64(globalRunNumber)),
-			"| loops detected=", humanize.Comma(int64(loops)))
-
-		prt := func(counter []int, msg string) {
-			fmt.Printf("\n=%20s", msg)
-			if len(counter) == 0 {
-				fmt.Println("\n=     None Found\n=")
-			}
-			for i, v := range counter {
-				if i%16 == 0 {
-					fmt.Println("")
-					fmt.Print("=")
-				}
-				str := fmt.Sprintf("%s[%3d]", humanize.Comma(int64(v)), i)
-				fmt.Printf("%12s ", str)
-			}
-		}
-		prt(deadMessagesAt, "Dead Messages")
-		prt(mirrorsAt, "Mirrors")
-		prt(solutionsAt, "Solutions")
-		prt(failuresAt, "Failures")
-		prt(depths, "Depths")
-		fmt.Println()
-
-		// Lots of printing... Not necessary....
-		fmt.Println(leaders[0].Display.Global.String())
-
-		for _, ldr := range leaders {
-			fmt.Println(ldr.Display.String())
-		}
-
-		if insanePrints {
-			// Example of a run that has a werid msg state
-			if globalRunNumber > -1 {
-				fmt.Println("Leader 0")
-				fmt.Println(leaders[0].PrintMessages())
-				fmt.Println("Leader 1")
-				fmt.Println(leaders[1].PrintMessages())
-				fmt.Println("Leader 2")
-				fmt.Println(leaders[2].PrintMessages())
-			}
-		}
-		fmt.Printf("%d %d setcon\n", len(leadersMap), len(audsMap))
-		for i, v := range msgPath {
-			fmt.Println(formatForInterpreter(v), "#", i, v.LeaderIdx, "<==", leaders[0].Display.FormatMessage(v.Msg))
-		}
-		fmt.Println("<b> # Pending:")
-		for i, v := range msgs {
-			fmt.Println(formatForInterpreter(v), "#", i, v.LeaderIdx, "<==", leaders[0].Display.FormatMessage(v.Msg))
-		}
-
+		extraPrints3 = true
+		last = now
 	}
 
 	if depth > limit {
 		if extraPrints {
-			fmt.Println(">>>>>>>>>>>>>>>>>> Hit Limit <<<<<<<<<<<<<<<<<")
-			printState()
+			fmt.Println("////////>>>>>>>>> Hit Limit <<<<<<<<<<<<<<<<<")
+			printState(depth, msgs, leaders, msgPath)
 			extraPrints = false
 		}
 		breadth++
@@ -202,8 +150,8 @@ func Dive(msgs []*controller.DirectedMsg, leaders []*election.Election, depth in
 	}
 
 	if depth < 4 {
-		fmt.Println("==================== Depth %d ====================")
-		printState()
+		fmt.Println("////////========== Depth %d ====================")
+		printState(depth, msgs, leaders, msgPath)
 	}
 
 	//done := 0
@@ -215,27 +163,40 @@ func Dive(msgs []*controller.DirectedMsg, leaders []*election.Election, depth in
 	if complete, err := nodesCompleted(leaders); complete { // done == len(leaders)/2+1 {
 		solutionsAt = incCounter(solutionsAt, depth)
 		if extraPrints {
-			fmt.Println(">>>>>>>>>>>>>>>>>>>>>>>>>> Solution Found @ ", depth)
+			//			fmt.Println(">>>>>>>>>>>>>>>>>>>>> Solution Found @ ", depth)
+		}
+		for _, ldr := range leaders {
+			if ldr.Committed {
+				win := int(ldr.CurrentVote.VolunteerPriority)
+				for len(winners) <= win {
+					winners = append(winners, 0)
+				}
+				winners[win]++
+				break
+			}
 		}
 		breadth++
 		solutions++
 		if extraPrints2 {
-			fmt.Println("!!!!!!!!!!!!!!!!!!  Success!")
-			printState()
+			fmt.Println("////////!!!!!!!  Success!")
+			printState(depth, msgs, leaders, msgPath)
 			extraPrints2 = false
 		}
 		return false, true, true
 
 	} else if err != nil {
 		// Bad! This means the algorithm is broken
-		errConclusions++
-		fmt.Println("^&**(^%$& Broken! @ depth:", depth)
-		fmt.Println(err.Error())
-		printState()
+		errCollision++
+		if extraPrints3 {
+			extraPrints3 = false
+			fmt.Println("// Fail with collision @ depth:", depth)
+			fmt.Println(err.Error())
+			printState(depth, msgs, leaders, msgPath)
+		}
 	}
 
 	// Look for mirrorMap, but only after we have been going a bit.
-	if depth > 0 {
+	if depth > 4 {
 		var hashes [][32]byte
 		var strings []string
 		for _, ldr := range leaders {
@@ -273,22 +234,27 @@ func Dive(msgs []*controller.DirectedMsg, leaders []*election.Election, depth in
 	}
 
 	leaf = true
-	d := 3
-	for range msgs {
+	d := 17
 
-		d += 3
-		d = d % len(msgs)
+	shuffle := make([]*mymsg, len(msgs))
+	copy(shuffle, msgs)
+	for a := 0; a < 6113; a++ {
+		d += 611953
+		i := a % len(shuffle)
+		j := d % len(shuffle)
+		shuffle[i], shuffle[j] = shuffle[j], shuffle[i]
+	}
 
-		v := msgs[d]
+	for d, v := range shuffle {
 
-		var msgs2 []*controller.DirectedMsg
+		var msgs2 []*mymsg
 		msgs2 = append(msgs2, msgs[0:d]...)
 		msgs2 = append(msgs2, msgs[d+1:]...)
 		ml2 := len(msgs2)
 
 		globalRunNumber++
 
-		cl := CloneElection(leaders[v.LeaderIdx])
+		cl := CloneElection(leaders[v.leaderIdx])
 
 		//if !spewSame(cl, leaders[v.leaderIdx]) {
 		//	fmt.Println("Clone Failed")
@@ -296,7 +262,7 @@ func Dive(msgs []*controller.DirectedMsg, leaders []*election.Election, depth in
 		//	os.Exit(0)
 		//}
 
-		msg, changed := leaders[v.LeaderIdx].Execute(v.Msg, depth)
+		msg, changed := leaders[v.leaderIdx].Execute(v.msg, depth)
 
 		msgPath2 := append(msgPath, v)
 
@@ -304,20 +270,20 @@ func Dive(msgs []*controller.DirectedMsg, leaders []*election.Election, depth in
 			leaf = false
 			if msg != nil {
 				for i, _ := range leaders {
-					if i != v.LeaderIdx {
-						my := new(controller.DirectedMsg)
-						my.LeaderIdx = i
-						my.Msg = msg
+					if i != v.leaderIdx {
+						my := new(mymsg)
+						my.leaderIdx = i
+						my.msg = msg
 						msgs2 = append(msgs2, my)
 					}
 				}
 			}
-			gl := leaders[v.LeaderIdx].Display.Global
+			gl := leaders[v.leaderIdx].Display.Global
 			for _, ldr := range leaders {
 				ldr.Display.Global = gl
 			}
 			// Recursive Dive
-			lim, _, ss := Dive(msgs2, leaders, depth, limit, msgPath2)
+			lim, _, ss := dive(msgs2, leaders, depth, limit, msgPath2)
 			_ = lim || ss
 			seeSuccess = seeSuccess || ss
 			limitHit = limitHit || lim
@@ -328,7 +294,7 @@ func Dive(msgs []*controller.DirectedMsg, leaders []*election.Election, depth in
 		} else {
 			deadMessagesAt = incCounter(deadMessagesAt, depth)
 		}
-		leaders[v.LeaderIdx] = cl
+		leaders[v.leaderIdx] = cl
 	}
 	if limitHit {
 		leaf = false
@@ -342,9 +308,9 @@ func Dive(msgs []*controller.DirectedMsg, leaders []*election.Election, depth in
 				failure++
 				if extraPrints1 {
 					extraPrints1 = false
-					fmt.Println("/////////////// Loops Fail //////////////////////")
+					fmt.Println("////////>>>>>>>/// Loops Fail //////////////////////")
 					fmt.Printf("%d %d setcon\n", len(leadersMap), len(audsMap))
-					printState()
+					printState(depth, msgs, leaders, msgPath)
 				}
 			}
 			limitHit = false
@@ -359,13 +325,80 @@ func Dive(msgs []*controller.DirectedMsg, leaders []*election.Election, depth in
 				extraPrints1 = false
 				fmt.Println("/////////////// Fail //////////////////////")
 				fmt.Printf("%d %d setcon\n", len(leadersMap), len(audsMap))
-				printState()
+				printState(depth, msgs, leaders, msgPath)
 			}
 
 		}
 	}
 
 	return limitHit, leaf, seeSuccess
+}
+
+func printState(depth int, msgs []*mymsg, leaders []*election.Election, msgPath []*mymsg) {
+	fmt.Printf("%s%s%4d%s%4d %s %12s %s%12s %s%5d %s%12s %12s  %s %12s %s %12s %s %12s %s %12s %s %12s", "=============== ",
+		" Depth=", depth, "/", maxdepth,
+		"| Multiple Collision", humanize.Comma(int64(errCollision)),
+		"| Failures=", humanize.Comma(int64(failure)),
+		"| MsgQ=", len(msgs),
+		"| Mirrors=", humanize.Comma(int64(mirrors)), humanize.Comma(int64(len(mirrorMap))),
+		"| Hit the Limits=", humanize.Comma(int64(hitlimit)),
+		"| Breadth=", humanize.Comma(int64(breadth)),
+		"| solutions so far =", humanize.Comma(int64(solutions)),
+		"| global count= ", humanize.Comma(int64(globalRunNumber)),
+		"| loops detected=", humanize.Comma(int64(loops)))
+
+	prt := func(counter []int, msg string) {
+		fmt.Printf("\n=%20s", msg)
+		if len(counter) == 0 {
+			fmt.Println("\n=     None Found\n=")
+		}
+		for i, v := range counter {
+			if i%16 == 0 {
+				fmt.Println("")
+				fmt.Print("=")
+			}
+			str := fmt.Sprintf("%s[%3d]", humanize.Comma(int64(v)), i)
+			fmt.Printf("%12s ", str)
+		}
+	}
+	prt(winners, "Winning Volunteers")
+	prt(deadMessagesAt, "Dead Messages")
+	prt(mirrorsAt, "Mirrors")
+	prt(solutionsAt, "Solutions")
+	prt(failuresAt, "Failures")
+	prt(depths, "Depths")
+	fmt.Println()
+
+	fmt.Println("====Start List===")
+	// Lots of printing... Not necessary....
+	fmt.Println(leaders[0].Display.Global.String())
+	fmt.Println("====End Global===")
+
+	for _, ldr := range leaders {
+		fmt.Println(ldr.Display.String())
+	}
+	fmt.Println("====End List===")
+
+	if insanePrints {
+		// Example of a run that has a werid msg state
+		if globalRunNumber > -1 {
+			fmt.Println("Leader 0")
+			fmt.Println(leaders[0].PrintMessages())
+			fmt.Println("Leader 1")
+			fmt.Println(leaders[1].PrintMessages())
+			fmt.Println("Leader 2")
+			fmt.Println(leaders[2].PrintMessages())
+		}
+	}
+	fmt.Printf("%d %d setcon\n", len(leadersMap), len(audsMap))
+	for i, v := range msgPath {
+		fmt.Println(formatForInterpreter(v), "#", i, v.leaderIdx, "<==", leaders[0].Display.FormatMessage(v.msg))
+	}
+	fmt.Println("Pending:")
+	for i, v := range msgs {
+		fmt.Println(formatForInterpreter(v), "#", i, v.leaderIdx, "<==", leaders[0].Display.FormatMessage(v.msg))
+	}
+
 }
 
 func nodesCompleted(nodes []*election.Election) (bool, error) {
@@ -384,25 +417,25 @@ func nodesCompleted(nodes []*election.Election) (bool, error) {
 	return done >= (len(nodes)/2)+1, nil
 }
 
-func formatForInterpreter(my *controller.DirectedMsg) string {
-	msg := my.Msg
+func formatForInterpreter(my *mymsg) string {
+	msg := my.msg
 	switch msg.(type) {
 	case *messages.LeaderLevelMessage:
 		l := msg.(*messages.LeaderLevelMessage)
 		from := leadersMap[l.Signer]
 
-		return fmt.Sprintf("{ %d } %d { %d } <-l", from, l.Level, my.LeaderIdx)
+		return fmt.Sprintf("{ %d } %d { %d } <-l", from, l.Level, my.leaderIdx)
 	case *messages.VolunteerMessage:
 		a := msg.(*messages.VolunteerMessage)
 		from := audsMap[a.Signer]
 
-		return fmt.Sprintf("%d { %d } <-v", from, my.LeaderIdx)
+		return fmt.Sprintf("%d { %d } <-v", from, my.leaderIdx)
 	case *messages.VoteMessage:
 		l := msg.(*messages.VoteMessage)
 		from := leadersMap[l.Signer]
 		vol := audsMap[l.Volunteer.Signer]
 
-		return fmt.Sprintf("{ %d } %d { %d } <-o", from, vol, my.LeaderIdx)
+		return fmt.Sprintf("{ %d } %d { %d } <-o", from, vol, my.leaderIdx)
 	}
 	return "NA"
 }
@@ -418,8 +451,8 @@ func incCounter(counter []int, depth int) []int {
 func recurse(auds int, feds int, limit int) {
 
 	_, leaders, msgs := newElections(feds, auds, false)
-	var msgpath []*controller.DirectedMsg
-	Dive(msgs, leaders, 0, limit, msgpath)
+	var msgpath []*mymsg
+	dive(msgs, leaders, 0, limit, msgpath)
 }
 
 // reuse encoder/decoder so we don't recompile the struct definition
@@ -443,11 +476,11 @@ func CloneElection(src *election.Election) *election.Election {
 	dst := new(election.Election)
 	err := enc.Encode(src)
 	if err != nil {
-		errConclusions++
+		errCollision++
 	}
 	err = dec.Decode(dst)
 	if err != nil {
-		errConclusions++
+		errCollision++
 	}
 	return dst
 }
