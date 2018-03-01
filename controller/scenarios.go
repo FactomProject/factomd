6a68f3137adb69f2de017b3776730a1b2135e370
Emyrk <stevenmasley@gmail.com>
2018-02-28 18:55:33 -0600

    Added happy scenario

--- controller/scenarios.go
+++ controller/scenarios.go
@@ -16,6 +16,7 @@ var Scenarios = make(map[string]string)
 
 func init() {
 	Scenarios["flipfloph"] = HorizontalFlipflop
+	Scenarios["agree"] = AllAgree
 }
 
 // " flipfloph" runscene
@@ -50,6 +51,28 @@ fright 1 mid <-l
 { 1 } 2 { 0 } <-l
 { 1 } 4 { 0 } <-l
 { 2 } 2 { 0 } <-l
+
+.ca
+`
+
+var AllAgree string = `
+32 1 setcon
+# Setup some variables
+
+{ [ 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 ] } /all def
+
+# Pass around volunteer 1
+0 all <-v
+all 0 all <-o
+
+
+# /* levels */
+all 1 all <-l
+all 2 all <-l
+all 3 all <-l
+all 4 all <-l
+all 5 all <-l
+all 6 all <-l
 
 .ca
 `
