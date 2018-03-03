b551fba04d74bc1e38c5583a291bc8a631f1c640
Emyrk <stevenmasley@gmail.com>
2018-03-03 11:28:07 -0600

    Comment out build error

--- common/messages/messageTrace.go
+++ common/messages/messageTrace.go
@@ -2,10 +2,10 @@ package messages
 
 import (
 	"fmt"
-	"github.com/FactomProject/factomd/common/interfaces"
 	"os"
 	"sync"
-	"github.com/FactomProject/factomd/common/constants"
+
+	"github.com/FactomProject/factomd/common/interfaces"
 )
 
 var (
@@ -34,26 +34,26 @@ func getTraceFile(name string) (f *os.File) {
 }
 
 func LogMessage(name string, note string, msg interfaces.IMsg) {
-
-	traceMutex.Lock()
-	defer traceMutex.Unlock()
-	myfile := getTraceFile(name)
-	sequence++
-	seq := sequence
-	t := msg.Type()
-	embeddedHash := ""
-
-	switch msg.Type() {
-	case constants.ACK_MSG:
-		m := msg.(*Ack)
-		embeddedHash = fmt.Sprintf(" EmbeddedMsg %26v[%2v]:%v", MessageName(m.Type()), m.Type(), m.MessageHash.String()[:8])
-	case constants.MISSING_MSG_RESPONSE:
-		m := msg.(*MissingMsgResponse).MsgResponse
-		embeddedHash = fmt.Sprintf(" EmbeddedMsg %26v[%2v]:%v", MessageName(m.Type()), m.Type(), m.GetHash().String()[:8])
-	}
-
-	myfile.WriteString(fmt.Sprintf("%5v %20s %v %26s[%2v]:%v%v {%v}\n", seq, note, msg.GetMsgHash().String()[:8], MessageName(byte(t)), t,
-		msg.GetHash().String()[:8], embeddedHash, msg.String()))
+	//
+	//traceMutex.Lock()
+	//defer traceMutex.Unlock()
+	//myfile := getTraceFile(name)
+	//sequence++
+	//seq := sequence
+	//t := msg.Type()
+	//embeddedHash := ""
+	//
+	//switch msg.Type() {
+	//case constants.ACK_MSG:
+	//	m := msg.(*Ack)
+	//	embeddedHash = fmt.Sprintf(" EmbeddedMsg %26v[%2v]:%v", MessageName(m.Type()), m.Type(), m.MessageHash.String()[:8])
+	//case constants.MISSING_MSG_RESPONSE:
+	//	m := msg.(*MissingMsgResponse).MsgResponse
+	//	embeddedHash = fmt.Sprintf(" EmbeddedMsg %26v[%2v]:%v", MessageName(m.Type()), m.Type(), m.GetHash().String()[:8])
+	//}
+	//
+	//	myfile.WriteString(fmt.Sprintf("%5v %20s %v %26s[%2v]:%v%v {%v}\n", seq, note, msg.GetMsgHash().String()[:8], MessageName(byte(t)), t,
+	//		msg.GetHash().String()[:8], embeddedHash, msg.String()))
 }
 
 func LogPrint(name string, note string) {
@@ -63,7 +63,6 @@ func LogPrint(name string, note string) {
 	seq := sequence
 	myfile.WriteString(fmt.Sprintf("%5v %s\n", seq, note))
 }
-
 
 // stringify it in the caller to avoid having to deal with the import loop
 func LogParcel(name string, note string, msg string) {
