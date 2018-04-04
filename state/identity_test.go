260326eaf96d4fcfaba2a2f49f69f727b6a21296
Emyrk <stevenmasley@gmail.com>
2018-04-04 13:20:32 -0500

    Correct unit tests

--- state/identity_test.go
+++ state/identity_test.go
@@ -4,22 +4,11 @@
 
 package state_test
 
-import (
-	//"bytes"
-	//"encoding/binary"
-	"testing"
-	//"time"
+//"bytes"
+//"encoding/binary"
 
-	//"github.com/FactomProject/factomd/common/messages"
-	"github.com/FactomProject/factomd/common/primitives"
-	//. "github.com/FactomProject/factomd/state"
-	"github.com/FactomProject/factomd/testHelper"
-)
+//"time"
 
-func TestIdentity(t *testing.T) {
-	s := testHelper.CreateAndPopulateTestState()
-	index := s.CreateBlankFactomIdentity(primitives.NewZeroHash())
-	if len(s.Identities) == 0 || index != 0 {
-		t.Errorf("Failed making blank identity")
-	}
-}
+//"github.com/FactomProject/factomd/common/messages"
+
+//. "github.com/FactomProject/factomd/state"
