0fdfa521198c0f86b7c17e6b6e49bfd8191d8209
Emyrk <stevenmasley@gmail.com>
2018-04-23 10:43:14 -0500

    Update unit test to incorporate new identity entries in admin block processing

--- wsapi/wsapi_test.go
+++ wsapi/wsapi_test.go
@@ -6,10 +6,13 @@ import (
 	"strings"
 	"testing"
 
+	"fmt"
+
 	"github.com/FactomProject/factomd/common/entryBlock"
 	"github.com/FactomProject/factomd/common/interfaces"
 	"github.com/FactomProject/factomd/common/primitives"
 	"github.com/FactomProject/factomd/receipts"
+	"github.com/FactomProject/factomd/state"
 	"github.com/FactomProject/factomd/testHelper"
 	. "github.com/FactomProject/factomd/wsapi"
 )
@@ -256,6 +259,12 @@ func TestHandleChainHead(t *testing.T) {
 
 	testHelper.ClearContextResponseWriter(context)
 	HandleChainHead(context, hash)
+
+	s := context.Server.Env["state"]
+	st := s.(*state.State)
+	a, _ := st.DB.FetchABlockByHeight(0)
+	fmt.Println(a)
+	fmt.Println(string(testHelper.GetBody(context)))
 
 	if strings.Contains(testHelper.GetBody(context), testHelper.ABlockHeadPrimaryIndex) == false {
 		t.Errorf("Invalid admin block head: %v", testHelper.GetBody(context))
