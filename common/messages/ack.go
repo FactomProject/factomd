da7ce006fa54ee9cf4e0c80856f0624c382dacf0
Clay Douglass <clay@factom.com>
2018-04-26 16:29:04 -0500

    Moved future ack fix up

--- common/messages/ack.go
+++ common/messages/ack.go
@@ -89,6 +89,11 @@ func (m *Ack) Validate(s interfaces.IState) int {
 	if m.DBHeight <= s.GetHighestSavedBlk() {
 		return -1
 	}
+
+	if s.GetHighestAck() < m.DBHeight {
+		s.SetHighestAck(m.DBHeight) // assume the ack isn't lying. this will make us start requesting DBState blocks...
+	}
+
 	delta := (int(m.DBHeight)-int(s.GetLeaderPL().GetDBHeight()))*10 + (int(m.Minute) - int(s.GetCurrentMinute()))
 
 	if delta > 30 {
@@ -108,10 +113,6 @@ func (m *Ack) Validate(s interfaces.IState) int {
 		s.LogMessage("executeMsg", "Ack slot taken", m)
 		s.LogMessage("executeMsg", "found:", msg)
 		return -1
-	}
-
-	if s.GetHighestAck() < m.DBHeight {
-		s.SetHighestAck(m.DBHeight) // assume the ack isn't lying. this will make us start requesting DBState blocks...
 	}
 
 	if !m.authvalid {
