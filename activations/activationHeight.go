84b1eb7b862d8249edef3a8a1cbf3140cd80bdd7
Clay Douglass <clay@factom.com>
2018-06-22 11:48:01 -0500

    moved activivation height

--- activations/activationHeight.go
+++ activations/activationHeight.go
@@ -38,7 +38,7 @@ func init() {
 				"MAIN":                      146060 + 8*24*10 + 1, // On 6/20/18 11:45 mainnet was 146060, we want activation at 6/28/18 at ~12pm
 				"TEST":                      0,                    // On 6/20/18 11:45 testnet was 146060, we want activation at 6/22/18 at ~12pm
 				"LOCAL":                     10,                   // Must be > 6 for TestActivationHeightElection to pass
-				"CUSTOM:fct_community_test": 32908 + 2*24*10,      // On 6/21/18 2:04 testnet was 32908, we want activation at 6/23/18 at 2:00pm
+				"CUSTOM:fct_community_test": 33037 + 2*24*10 + 1,  // On 6/22/18 11:45 testnet was 33037, we want activation at 6/24/18 at 12:00pm
 			},
 		},
 	}
