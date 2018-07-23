3498eb2bf6e917df518491ec4f8236c3b5f3e254
Clay Douglass <clay@factom.com>
2018-07-23 17:44:46 -0500

    made defult payout dates work

--- state/grants.go
+++ state/grants.go
@@ -34,46 +34,43 @@ func GetHardCodedGrants() []HardGrant {
 
 	case "MAIN":
 		hardcodegrants = []HardGrant{
-            // Initial grants approved June 9, 2018 https://drive.google.com/drive/folders/1e_xmKgJb375EcAwwkz2d3mdYs0PVVih3
-            // https://factomize.com/forums/threads/implementing-the-initial-grants.428/
-            // USD/FCT rate calculated at $10.03567 using 7 day EMA from July 16, 2018 https://drive.google.com/drive/folders/1GgAtsTlZEMD77eUvQW3TSGULxIT0Ld03
+			// Initial grants approved June 9, 2018 https://drive.google.com/drive/folders/1e_xmKgJb375EcAwwkz2d3mdYs0PVVih3
+			// https://factomize.com/forums/threads/implementing-the-initial-grants.428/
+			// USD/FCT rate calculated at $10.03567 using 7 day EMA from July 16, 2018 https://drive.google.com/drive/folders/1GgAtsTlZEMD77eUvQW3TSGULxIT0Ld03
 
 			// USD denominated grants
-            // Legal Review Grant:
-            // $200,000 total = 19929 FCT split over 4 addresses
-            // part 1/4 6929 FCT
-            HardGrant{21, 692900000000, factoid.NewAddress(primitives.ConvertUserStrToAddress("FA2eNHCf6Sh8aPJrtiparZcxKHWENbvAVvm18Yw9tXyqHyVdxz6E"))},
-            // part 2/4 7500 FCT
-            HardGrant{21, 750000000000, factoid.NewAddress(primitives.ConvertUserStrToAddress("FA2Ls3yMt9gy8MxxxzvuBj4Zt6wjt15kEW3xoTKi1Npe4LQ1idDw"))},
-            // part 3/4 2000 FCT
-            HardGrant{21, 200000000000, factoid.NewAddress(primitives.ConvertUserStrToAddress("FA2apt71pNu4dfr7o1zFiCdKUJQvCYfnjXjiUveV8VJNJ9QmXg93"))},
-            // part 4/4 3500 FCT
-            HardGrant{21, 350000000000, factoid.NewAddress(primitives.ConvertUserStrToAddress("FA2LpYCTSxWssXghqtSkpsedB9zozPy9psMKfbJhcCV1qKLp4iZy"))},
-            
-            
-            // Voting System Grant:
-            // Sent to 4 addresses for 4 parties a total of 22658 FCT
+			// Legal Review Grant:
+			// $200,000 total = 19929 FCT split over 4 addresses
+			// part 1/4 6929 FCT
+			HardGrant{26, 692900000000, factoid.NewAddress(primitives.ConvertUserStrToAddress("FA2eNHCf6Sh8aPJrtiparZcxKHWENbvAVvm18Yw9tXyqHyVdxz6E"))},
+			// part 2/4 7500 FCT
+			HardGrant{26, 750000000000, factoid.NewAddress(primitives.ConvertUserStrToAddress("FA2Ls3yMt9gy8MxxxzvuBj4Zt6wjt15kEW3xoTKi1Npe4LQ1idDw"))},
+			// part 3/4 2000 FCT
+			HardGrant{26, 200000000000, factoid.NewAddress(primitives.ConvertUserStrToAddress("FA2apt71pNu4dfr7o1zFiCdKUJQvCYfnjXjiUveV8VJNJ9QmXg93"))},
+			// part 4/4 3500 FCT
+			HardGrant{26, 350000000000, factoid.NewAddress(primitives.ConvertUserStrToAddress("FA2LpYCTSxWssXghqtSkpsedB9zozPy9psMKfbJhcCV1qKLp4iZy"))},
+
+			// Voting System Grant:
+			// Sent to 4 addresses for 4 parties a total of 22658 FCT
 			// TFA $154291.2  ($105091.2 + $43200 + $6000) = 15374 FCT
-            // part 1/4 15374 FCT
-            HardGrant{21, 1537400000000, factoid.NewAddress(primitives.ConvertUserStrToAddress("FA3dxxVGuL3oPCBLoxmTPXrse3dyS1VyqTttGWJpcCPjnihFFqT5"))},
-            // Factomatic $45400 = 4524 FCT
-            // part 2/4 4524 FCT
-            HardGrant{21, 452400000000, factoid.NewAddress(primitives.ConvertUserStrToAddress("FA2944TXTDQKdJDp3TLSANjgMjwK2pQnTSkzE3kQcHWKetCCphcH"))},
-            // LUCIAP $24200 = 2411 FCT
-            // part 3/4 2411 FCT
-            HardGrant{21, 241100000000, factoid.NewAddress(primitives.ConvertUserStrToAddress("FA1yjNL71ddoQPMcgJpBK4TcuyRH8xpHSZWMLJ7DbkdjhHBVZhEm"))},
-            // Factoshi $3500 = 349 FCT
-            // part 4/4 349 FCT
-            HardGrant{21, 34900000000, factoid.NewAddress(primitives.ConvertUserStrToAddress("FA2EPEXRPLc6HE3py95f2bsouFTj7tDq9gd5RVdv1RVAUiUTXyJm"))},
-            
-            
-            
+			// part 1/4 15374 FCT
+			HardGrant{26, 1537400000000, factoid.NewAddress(primitives.ConvertUserStrToAddress("FA3dxxVGuL3oPCBLoxmTPXrse3dyS1VyqTttGWJpcCPjnihFFqT5"))},
+			// Factomatic $45400 = 4524 FCT
+			// part 2/4 4524 FCT
+			HardGrant{26, 452400000000, factoid.NewAddress(primitives.ConvertUserStrToAddress("FA2944TXTDQKdJDp3TLSANjgMjwK2pQnTSkzE3kQcHWKetCCphcH"))},
+			// LUCIAP $24200 = 2411 FCT
+			// part 3/4 2411 FCT
+			HardGrant{26, 241100000000, factoid.NewAddress(primitives.ConvertUserStrToAddress("FA1yjNL71ddoQPMcgJpBK4TcuyRH8xpHSZWMLJ7DbkdjhHBVZhEm"))},
+			// Factoshi $3500 = 349 FCT
+			// part 4/4 349 FCT
+			HardGrant{26, 34900000000, factoid.NewAddress(primitives.ConvertUserStrToAddress("FA2EPEXRPLc6HE3py95f2bsouFTj7tDq9gd5RVdv1RVAUiUTXyJm"))},
+
 			// FCT denominated grants
 
 			// Java Enterprise Client Library Grant:
 			// Blockchain Innovation Foundation 1200 FCT
-			HardGrant{21, 120000000000, factoid.NewAddress(primitives.ConvertUserStrToAddress("FA3YVoaN2D8xNitQ6BNhUDW6jH73MdYKyokJqs9LPJM8cuusM7fo"))},
-        }
+			HardGrant{26, 120000000000, factoid.NewAddress(primitives.ConvertUserStrToAddress("FA3YVoaN2D8xNitQ6BNhUDW6jH73MdYKyokJqs9LPJM8cuusM7fo"))},
+		}
 
 	default:
 		hardcodegrants = []HardGrant{}
