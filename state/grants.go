185f2b2dd50dac0ccd31931bd704c4d5108a26e0
Brian Deery <brian@factom.com>
2018-08-02 00:27:49 -0500

    adjusted activation height to Monday afternoon

--- state/grants.go
+++ state/grants.go
@@ -38,48 +38,48 @@ func GetHardCodedGrants() []HardGrant {
 			// https://factomize.com/forums/threads/implementing-the-initial-grants.428/
 			// USD/FCT rate calculated at $8.6218 using 7 day EMA from August 1, 2018 https://drive.google.com/drive/folders/1GgAtsTlZEMD77eUvQW3TSGULxIT0Ld03
 
-			/// activation block height of 152326 is expected on Friday, August 3 around 5pm UTC
+			/// activation block height of 152751 is expected on Monday, August 6 around 7pm UTC
 
 			// USD denominated grants
 			// Legal Review Grant:
 			// $200,000 total = 23197 FCT split over 4 addresses
 			// part 1/4 8065 FCT
-			HardGrant{152326, 806500000000, validateAddress("FA2eNHCf6Sh8aPJrtiparZcxKHWENbvAVvm18Yw9tXyqHyVdxz6E")},
+			HardGrant{152751, 806500000000, validateAddress("FA2eNHCf6Sh8aPJrtiparZcxKHWENbvAVvm18Yw9tXyqHyVdxz6E")},
 			// part 2/4 8730 FCT
-			HardGrant{152326, 873000000000, validateAddress("FA2Ls3yMt9gy8MxxxzvuBj4Zt6wjt15kEW3xoTKi1Npe4LQ1idDw")},
+			HardGrant{152751, 873000000000, validateAddress("FA2Ls3yMt9gy8MxxxzvuBj4Zt6wjt15kEW3xoTKi1Npe4LQ1idDw")},
 			// part 3/4 2328 FCT
-			HardGrant{152326, 232800000000, validateAddress("FA2apt71pNu4dfr7o1zFiCdKUJQvCYfnjXjiUveV8VJNJ9QmXg93")},
+			HardGrant{152751, 232800000000, validateAddress("FA2apt71pNu4dfr7o1zFiCdKUJQvCYfnjXjiUveV8VJNJ9QmXg93")},
 			// part 4/4 4074 FCT
-			HardGrant{152326, 407400000000, validateAddress("FA2LpYCTSxWssXghqtSkpsedB9zozPy9psMKfbJhcCV1qKLp4iZy")},
+			HardGrant{152751, 407400000000, validateAddress("FA2LpYCTSxWssXghqtSkpsedB9zozPy9psMKfbJhcCV1qKLp4iZy")},
 
 			// Voting System Grant:
 			// Sent to 4 addresses for 4 parties a total of 26374 FCT
 			// TFA $154291.2  ($105091.2 + $43200 + $6000) = 17895 FCT
 			// part 1/4 17895 FCT
-			HardGrant{152326, 1789500000000, validateAddress("FA3dxxVGuL3oPCBLoxmTPXrse3dyS1VyqTttGWJpcCPjnihFFqT5")},
+			HardGrant{152751, 1789500000000, validateAddress("FA3dxxVGuL3oPCBLoxmTPXrse3dyS1VyqTttGWJpcCPjnihFFqT5")},
 			// Factomatic $45400 = 5266 FCT
 			// part 2/4 5266 FCT
-			HardGrant{152326, 526600000000, validateAddress("FA2944TXTDQKdJDp3TLSANjgMjwK2pQnTSkzE3kQcHWKetCCphcH")},
+			HardGrant{152751, 526600000000, validateAddress("FA2944TXTDQKdJDp3TLSANjgMjwK2pQnTSkzE3kQcHWKetCCphcH")},
 			// LUCIAP $24200 = 2807 FCT
 			// part 3/4 2807 FCT
-			HardGrant{152326, 280700000000, validateAddress("FA1yjNL71ddoQPMcgJpBK4TcuyRH8xpHSZWMLJ7DbkdjhHBVZhEm")},
+			HardGrant{152751, 280700000000, validateAddress("FA1yjNL71ddoQPMcgJpBK4TcuyRH8xpHSZWMLJ7DbkdjhHBVZhEm")},
 			// Factoshi $3500 = 406 FCT
 			// part 4/4 406 FCT
-			HardGrant{152326, 40600000000, validateAddress("FA2EPEXRPLc6HE3py95f2bsouFTj7tDq9gd5RVdv1RVAUiUTXyJm")},
+			HardGrant{152751, 40600000000, validateAddress("FA2EPEXRPLc6HE3py95f2bsouFTj7tDq9gd5RVdv1RVAUiUTXyJm")},
 
 			// FCT denominated grants
 
 			// Java Enterprise Client Library Grant:
 			// Blockchain Innovation Foundation 1200 FCT
-			HardGrant{152326, 120000000000, validateAddress("FA3YVoaN2D8xNitQ6BNhUDW6jH73MdYKyokJqs9LPJM8cuusM7fo")},
+			HardGrant{152751, 120000000000, validateAddress("FA3YVoaN2D8xNitQ6BNhUDW6jH73MdYKyokJqs9LPJM8cuusM7fo")},
 
 			// Oracle Master Grant:
 			// Factom, Inc.  300 FCT
-			HardGrant{152326, 30000000000, validateAddress("FA3fpiZ91MCRRFjVGfNXK4pg7vx3BT3aSRyoVqgptZCX7N5BNR8P")},
+			HardGrant{152751, 30000000000, validateAddress("FA3fpiZ91MCRRFjVGfNXK4pg7vx3BT3aSRyoVqgptZCX7N5BNR8P")},
 
 			// Anchor Master grant:
 			// Factom, Inc.  One time 600 FCT plus 220 per month from June 9 -> Sep 9, 2018. Total of 1260 FCT
-			HardGrant{152326, 126000000000, validateAddress("FA3jySUFtLXb1VdAJJ5NRVNYEtZ4EBSkDB7yn6LuKGQ4P1ntARhx")},
+			HardGrant{152751, 126000000000, validateAddress("FA3jySUFtLXb1VdAJJ5NRVNYEtZ4EBSkDB7yn6LuKGQ4P1ntARhx")},
 
 			// Protocol Development Grant:
 			// Factom, Inc.  30000 per month from June 9 -> Sep 9, 2018, Total of 90000 FCT
@@ -93,25 +93,25 @@ func GetHardCodedGrants() []HardGrant {
 
 			// Dchapman
 			// 160 + 70.96 = 230.96 rounded to 231
-			HardGrant{152326, 23100000000, validateAddress("FA22C7H844H5TrWNyXerqp6ZAvdSxYEEUNZxexuZbLnBvcxTobit")},
+			HardGrant{152751, 23100000000, validateAddress("FA22C7H844H5TrWNyXerqp6ZAvdSxYEEUNZxexuZbLnBvcxTobit")},
 
 			// Centis BV total: 160 + 200 + 40 + 600 = 1000 FCT
-			HardGrant{152326, 100000000000, validateAddress("FA2hvRaci9Kks9cLNkEUFcxzUJuUFaaAE1eWYLqa2qk1k9pVFVBp")},
+			HardGrant{152751, 100000000000, validateAddress("FA2hvRaci9Kks9cLNkEUFcxzUJuUFaaAE1eWYLqa2qk1k9pVFVBp")},
 
 			// The 42nd Factoid total: 160 + 200 + 40 + 600 = 1000 FCT
-			HardGrant{152326, 100000000000, validateAddress("FA3QzobSZzMMYY1iL8SPWLzRsRggm9cKRg8SXkHxEhaQjqSSLY1o")},
+			HardGrant{152751, 100000000000, validateAddress("FA3QzobSZzMMYY1iL8SPWLzRsRggm9cKRg8SXkHxEhaQjqSSLY1o")},
 
 			// Factom Inc. Factoid total: 160 + 200 + 40 + 600 = 1000 FCT
-			HardGrant{152326, 100000000000, validateAddress("FA2teRURMYTdYAA97zdh7rZDkxNtR1nhjryo34aaskjYqsqRSwZq")},
+			HardGrant{152751, 100000000000, validateAddress("FA2teRURMYTdYAA97zdh7rZDkxNtR1nhjryo34aaskjYqsqRSwZq")},
 
 			// Matt Osborne/12 Lantern Solutions total: 160 + 200 = 360 FCT
-			HardGrant{152326, 36000000000, validateAddress("FA3dug63wat1WnaLSHB3vZw1dbsqTzmgaVqpm727UYKit4sdHgQJ")},
+			HardGrant{152751, 36000000000, validateAddress("FA3dug63wat1WnaLSHB3vZw1dbsqTzmgaVqpm727UYKit4sdHgQJ")},
 
 			// Canonical Ledgers total: 6.45 + 40 + 600 = 646.45 rounded to 646 FCT
-			HardGrant{152326, 64600000000, validateAddress("FA2PEXgRiPd14NzUP47XfVTgEnvjtLSebBZvnM8gM7cJAMuqWs89")},
+			HardGrant{152751, 64600000000, validateAddress("FA2PEXgRiPd14NzUP47XfVTgEnvjtLSebBZvnM8gM7cJAMuqWs89")},
 
 			// DBGrow total: 6.45 + 40 + 600 = 646.45 rounded to 646 FCT
-			HardGrant{152326, 64600000000, validateAddress("FA3HSuFo9Soa5ZnG82JHqyKiRi4Pw17LxPTo9AsCaFNLCGkXkgsu")},
+			HardGrant{152751, 64600000000, validateAddress("FA3HSuFo9Soa5ZnG82JHqyKiRi4Pw17LxPTo9AsCaFNLCGkXkgsu")},
 
 			// Guide Payments (anticipated) July 7 - Sep 7 2018
 			// Centis BV total: 600 + 600 = 1200 FCT
