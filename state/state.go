711df59d2086fd3387ae91c710f9d14527eca6bc
Joshua Brigati <brigati.joshua@gmail.com>
2018-09-14 08:13:45 -0500

    testinh

--- state/state.go
+++ state/state.go
@@ -196,6 +196,8 @@ type State struct {
 	factomdTLSCertFile string
 	FactomdLocations   string
 
+	CorsDomains []string
+
 	// Server State
 	StartDelay      int64 // Time in Milliseconds since the last DBState was applied
 	StartDelayLimit int64
@@ -517,6 +519,8 @@ func (s *State) Clone(cloneNumber int) interfaces.IState {
 	newState.factomdTLSCertFile = s.factomdTLSCertFile
 	newState.FactomdLocations = s.FactomdLocations
 
+	newState.CorsDomains = s.CorsDomains
+
 	switch newState.DBType {
 	case "LDB":
 		newState.StateSaverStruct.FastBoot = s.StateSaverStruct.FastBoot
@@ -603,6 +607,10 @@ func (s *State) SetNetStateOff(net bool) {
 
 func (s *State) GetRpcUser() string {
 	return s.RpcUser
+}
+
+func (s *State) GetCorsDomains() []string {
+	return s.CorsDomains
 }
 
 func (s *State) GetRpcPass() string {
@@ -730,6 +738,14 @@ func (s *State) LoadConfig(filename string, networkFlag string) {
 		s.StateSaverStruct.FastBootLocation = cfg.App.FastBootLocation
 		s.FastBoot = cfg.App.FastBoot
 		s.FastBootLocation = cfg.App.FastBootLocation
+
+		if len(cfg.App.CorsDomains) > 0 {
+			domains := strings.Split(cfg.App.CorsDomains, ",")
+			s.CorsDomains = make([]string, len(domains))
+			for _, domain := range domains {
+				s.CorsDomains = append(s.CorsDomains, strings.Trim(domain, " "))
+			}
+		}
 
 		s.FactomdTLSEnable = cfg.App.FactomdTlsEnabled
 		if cfg.App.FactomdTlsPrivateKey == "/full/path/to/factomdAPIpriv.key" {
