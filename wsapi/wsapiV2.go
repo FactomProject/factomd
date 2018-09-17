a9f102cd2857855867501c0d897d55e4d8fae9c9
Clay Douglass <clay@factom.com>
2018-09-17 09:49:11 -0500

    Merge FD-652_wsapi_test_fix

--- wsapi/wsapiV2.go
+++ wsapi/wsapiV2.go
@@ -72,7 +72,6 @@ func HandleV2Request(state interfaces.IState, j *primitives.JSON2Request) (*prim
 	var resp interface{}
 	var jsonError *primitives.JSONError
 	params := j.Params
-	state.LogPrintf("apilog", "request %v", j.String())
 	switch j.Method {
 	case "chain-head":
 		resp, jsonError = HandleV2ChainHead(state, params)
@@ -184,7 +183,6 @@ func HandleV2Request(state interfaces.IState, j *primitives.JSON2Request) (*prim
 		break
 	}
 	if jsonError != nil {
-		state.LogPrintf("apilog", "error %v", jsonError)
 		return nil, jsonError
 	}
 
@@ -192,7 +190,6 @@ func HandleV2Request(state interfaces.IState, j *primitives.JSON2Request) (*prim
 	jsonResp.ID = j.ID
 	jsonResp.Result = resp
 
-	state.LogPrintf("apilog", "responce %v", jsonResp.String())
 	return jsonResp, nil
 }
 
