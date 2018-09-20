59c7f07244b0852db093c20a7d805a922da6b615
stackdump <myork@stackdump.com>
2018-09-20 12:05:23 -0500

    fix fastboot memory spike bug loading too many process lists

--- state/saveAndRestore.go
+++ state/saveAndRestore.go
@@ -582,7 +582,9 @@ func (ss *SaveState) RestoreFactomdState(s *State) { //, d *DBState) {
 	}
 	// Set this, as we know it to be true
 	s.DBHeightAtBoot = ss.DBHeight
-	s.ProcessLists.Lists = s.ProcessLists.Lists[:0]
+
+	// set DBHeightBase to avoid recursively loading process lists
+	s.ProcessLists.DBHeightBase = ss.DBHeight
 	pl := s.ProcessLists.Get(ss.DBHeight)
 
 	// s.AddStatus(fmt.Sprintln("Index: ", index, "dbht:", ss.DBHeight, "lleaderheight", s.LLeaderHeight))
