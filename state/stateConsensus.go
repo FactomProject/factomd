7d371c079600b677854afec3ed444cfdf9b3c421
Clay Douglass <clay@factom.com>
2018-10-08 12:45:31 -0500

    cleaned up logging

--- state/stateConsensus.go
+++ state/stateConsensus.go
@@ -118,15 +118,18 @@ func (s *State) executeMsg(vm *VM, msg interfaces.IMsg) (ret bool) {
 			// Make sure we don't put in an old msg (outside our repeat range)
 			Delta := blktime - msgtime
 			if Delta > tlim || -Delta > tlim {
-				valid = -1 // Assume the message is bad.
-				s.LogPrintf("processList", "Drop message pair, because the msg is out of range")
 				// Delta is is negative its greater than blktime then it is future.
 				if Delta < 0 {
+					s.LogMessage("executeMsg", "Hold message from the future", msg)
 					valid = 0 // Future stuff I can hold for now.  It might be good later.
+				} else {
+					s.LogMessage("executeMsg", "Drop message because the msg is out of range", msg)
+					valid = -1 // Old messages are bad.
 				}
 			}
 		}
 	}
+
 	switch valid {
 	case 1:
 		// The highest block for which we have received a message.  Sometimes the same as
