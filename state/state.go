04710ecf3fcf6711f4a411abf138ce24703c527a
Brian Deery <brian@factom.com>
2018-10-11 16:10:12 -0500

    comment out loud logs

--- state/state.go
+++ state/state.go
@@ -2137,11 +2137,11 @@ func (s *State) GetLeaderTimestamp() interfaces.Timestamp {
 
 func (s *State) SetLeaderTimestamp(ts interfaces.Timestamp) {
 	s.LeaderTimestamp = ts //SetLeaderTimestamp()
-	s.LogPrintf("executeMsg", "Set LeaderTimeStamp %d %v for %s", s.LLeaderHeight, s.LeaderTimestamp.String(), atomic.WhereAmIString(1))
+	//s.LogPrintf("executeMsg", "Set LeaderTimeStamp %d %v for %s", s.LLeaderHeight, s.LeaderTimestamp.String(), atomic.WhereAmIString(1))
 }
 func (s *State) SetLLeaderHeight(height uint32) {
 	s.LLeaderHeight = height //SetLeaderHeight()
-	s.LogPrintf("executeMsg", "Set LeaderHeight %d for %s", s.LLeaderHeight, atomic.WhereAmIString(1))
+	//s.LogPrintf("executeMsg", "Set LeaderHeight %d for %s", s.LLeaderHeight, atomic.WhereAmIString(1))
 }
 
 func (s *State) SetFaultTimeout(timeout int) {
