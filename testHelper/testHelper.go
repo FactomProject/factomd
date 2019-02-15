3ded541c6e9f3a288b8a7c80d6b07a41afeb9c3b
Brian Deery <brian@factom.com>
2019-02-14 22:16:35 -0600

    update another test for unit test passing

--- testHelper/testHelper.go
+++ testHelper/testHelper.go
@@ -68,6 +68,7 @@ func CreateAndPopulateTestState() *state.State {
 	s.SetLeaderTimestamp(primitives.NewTimestampFromMilliseconds(0))
 	s.DB = CreateAndPopulateTestDatabaseOverlay()
 	s.LoadConfig("", "")
+	s.DBFinished = true
 
 	s.DirectoryBlockInSeconds = 20
 
