1585cc6e34627f6245dbc7f364757ec9c1c24e76
stackdump <myork@stackdump.com>
2019-02-19 14:07:03 -0600

    test swap leader & audit on circle

--- test.sh
+++ test.sh
@@ -1,20 +1,6 @@
 #!/usr/bin/env bash
 
-# run same tests as specified in .circleci/config.yml
-PACKAGES=$(glide nv | grep -v Utilities | grep -v LongTests | grep -v simTest) 
-FAIL=""
-
-for PKG in ${PACKAGES[*]} ; do
-  go test -v -vet=off $PKG
-  if [[ $? != 0 ]] ;  then
-    FAIL=1
-  fi
-done
-
-if [[ "${FAIL}x" != "x" ]] ; then
-  echo "TESTS FAIL"
-  exit 1
-else
-  echo "ALL TESTS PASS"
-  exit 0
-fi
+# KLUDGE: just test brainswap
+cd support/dev/simulator/brainSwap/
+nohup ./test0.sh &
+./test1.sh
