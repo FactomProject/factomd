4ff747f2e0949fde6035af140aedcf64d26abc30
Who Soup <who.soup@gmail.com>
2019-02-27 09:50:37 +0100

    improving handling of malformed messages

--- common/messages/ack.go
+++ common/messages/ack.go
@@ -115,10 +115,6 @@ func (m *Ack) Validate(s interfaces.IState) int {
 		return -1
 	}
 
-	if s.GetHighestAck() < m.DBHeight {
-		s.SetHighestAck(m.DBHeight) // assume the ack isn't lying. this will make us start requesting DBState blocks...
-	}
-
 	delta := (int(m.DBHeight)-int(s.GetLeaderPL().GetDBHeight()))*10 + (int(m.Minute) - int(s.GetCurrentMinute()))
 
 	if delta > 50 {
@@ -126,6 +122,10 @@ func (m *Ack) Validate(s interfaces.IState) int {
 		// when we get caught up we will either get a DBState with this message or we will missing message it.
 		// but if it was malicious then we don't want to keep it around filling up queues.
 		return -1
+	}
+
+	if s.GetHighestAck() < m.DBHeight {
+		s.SetHighestAck(m.DBHeight) // assume the ack isn't lying. this will make us start requesting DBState blocks...
 	}
 
 	if delta > 30 {
