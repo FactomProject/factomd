512ba4da11c90990a62d6f5d08a784b0778ac015
Justin Hanneman <hannemennah@gmail.com>
2019-05-03 12:29:58 -0500

    Fix hard-coded hashes in testHelper for new anchor contents

--- testHelper/constants.go
+++ testHelper/constants.go
@@ -1,14 +1,14 @@
 package testHelper
 
 const (
-	DBlockHeadPrimaryIndex   = "30060a6e8247e0ee085f29714a95d91f659c78ceea7ced49fd312c115e0440c3"
-	DBlockHeadSecondaryIndex = "376c6f28ffeaf08a61df390b4a9731f241d340d913564c3ca6eab32e113b5cd2"
+	DBlockHeadPrimaryIndex   = "e8e28ca4dfd65075cc113c522bf9476ffdf78235d553e8a192f477ee8c659cd0"
+	DBlockHeadSecondaryIndex = "5a0654af6690ff20f2a50970e2859daec94390a623e39598ffe79c76cbe45a4b"
 
 	ABlockHeadPrimaryIndex   = "073bd46185217fddbd6d422cc23f28fe8ea58bf63c2e7521b7de3623b52e3b41"
 	ABlockHeadSecondaryIndex = "4d4d40eff3c48e054226d7308ac6a8e2230dc489be7ca39631abac258f33902b"
 
-	ECBlockHeadPrimaryIndex   = "bdf70fe3bd42c74bb86668e10bff2c8c1c52c6c89f625d3d9b6e763081916a56"
-	ECBlockHeadSecondaryIndex = "c70b7f03068418741f6815f26560cbb2a11447c9b8717c4fe6ac2a0852750393"
+	ECBlockHeadPrimaryIndex   = "6b1aa04211ffd258c19f3ae4d0299f2b71ccabb8105489ed35dd881fc40a1e68"
+	ECBlockHeadSecondaryIndex = "25a030e3a0eb491f7b5c0be7ee31575e8c274effc80854eafb1bf6c4ec4ea1e2"
 
 	FBlockHeadPrimaryIndex   = "84c8ac94c639117ef1b80d00c48f03c4a14bd74f17d86d56a8d8a4a73e6f91a8"
 	FBlockHeadSecondaryIndex = "5f4d14e8b4bf5d8545d7ccaccccc99d2757b6ef5eaf5ffac03705457bb9d65ac"
@@ -17,5 +17,5 @@ const (
 	EBlockHeadSecondaryIndex = "e79fb46ad81f0b4fac7f1e66728b40b390f8fcc3806e93f94550eec041eecff2"
 
 	AnchorBlockHeadPrimaryIndex   = "df3ade9eec4b08d5379cc64270c30ea7315d8a8a1a69efe2b98a60ecdd69e604"
-	AnchorBlockHeadSecondaryIndex = "7ab57fdb07ad75f35cb8f85bf435f60d5eab381c9da253780978cdb9d7ea5fae"
+	AnchorBlockHeadSecondaryIndex = "c4e4c26a0f280425cc4acafa66205cf2643e8d9c2c13cf6905edcfa1e5f90983"
 )
