e12e6f9fe58db946ce56fa6f593285038ef26c44
sanderPostma <spostma@sphereon.com>
2019-07-18 16:53:48 +0200

    LiveAPI: building intermediate event types

--- common/messages/eventMsgs/eventMappers.go
+++ common/messages/eventMsgs/eventMappers.go
@@ -3,13 +3,27 @@ package eventMessages
 import (
 	"github.com/FactomProject/factomd/common/interfaces"
 	"github.com/FactomProject/factomd/common/messages"
+	"github.com/FactomProject/factomd/common/primitives"
 	"github.com/gogo/protobuf/types"
 	"github.com/golang/protobuf/proto"
+	"time"
 )
 
 func AnchoredEventFromDBState(dbStateMessage *messages.DBStateMsg) *AnchoredEvent {
 	event := &AnchoredEvent{}
 	event.DirectoryBlock = mapDirBlock(dbStateMessage.DirectoryBlock)
+	return event
+}
+
+func AddIntermediateEventFromMessage(eventSource IntermediateEvent_EventSource, msg interfaces.IMsg) *IntermediateEvent {
+	event := &IntermediateEvent{}
+	event.EventSource = eventSource
+	switch msg.(type) {
+	case *messages.CommitChainMsg:
+		event.Value = mapCommitChain(msg)
+	case *messages.CommitEntryMsg:
+		event.Value = mapCommitEvent(msg)
+	}
 	return event
 }
 
@@ -59,6 +73,49 @@ func mapDirEntry(entry interfaces.IDBEntry) *Entry {
 		},
 	}
 	return result
+}
+
+func mapCommitChain(msg interfaces.IMsg) *IntermediateEvent_CommitChain {
+	commitChain := msg.(*messages.CommitChainMsg).CommitChain
+	ecPubKey := commitChain.ECPubKey.Fixed()
+	sig := commitChain.Sig
+
+	result := &IntermediateEvent_CommitChain{
+		CommitChain: &CommitChain{
+			ChainIDHash: &Hash{
+				HashValue: commitChain.ChainIDHash.Bytes()},
+			EntryHash: &Hash{
+				HashValue: commitChain.EntryHash.Bytes(),
+			},
+			Timestamp: convertToTimestamp(commitChain.MilliTime),
+			Credits:   uint32(commitChain.Credits),
+			ECPubKey:  ecPubKey[:],
+			Sig:       sig[:],
+		}}
+	return result
+}
+
+func mapCommitEvent(msg interfaces.IMsg) *IntermediateEvent_CommitEntry {
+	commitEntry := msg.(*messages.CommitChainMsg).CommitChain
+	ecPubKey := commitEntry.ECPubKey.Fixed()
+	sig := commitEntry.Sig
+
+	result := &IntermediateEvent_CommitEntry{
+		CommitEntry: &CommitEntry{
+			EntryHash: &Hash{
+				HashValue: commitEntry.EntryHash.Bytes(),
+			},
+			Timestamp: convertToTimestamp(commitEntry.MilliTime),
+			Credits:   uint32(commitEntry.Credits),
+			ECPubKey:  ecPubKey[:],
+			Sig:       sig[:],
+		}}
+	return result
+}
+
+func convertToTimestamp(milliTime *primitives.ByteSlice6) *types.Timestamp {
+	time := time.Now() // TODO convert ByteSlice6 back to timestamp
+	return &types.Timestamp{Seconds: int64(time.Second()), Nanos: int32(time.Nanosecond())}
 }
 
 type Event interface {
--- state/stateConsensus.go
+++ state/stateConsensus.go
@@ -76,6 +76,9 @@ func (s *State) AddToHolding(hash [32]byte, msg interfaces.IMsg) {
 		s.Holding[hash] = msg
 		s.LogMessage("holding", "add", msg)
 		TotalHoldingQueueInputs.Inc()
+
+		go emitAddToHoldingEvent(msg, s)
+
 	}
 }
 
@@ -311,7 +314,6 @@ func (s *State) executeMsg(msg interfaces.IMsg) (ret bool) {
 				s.RunLeader, s.Leader, s.Saving, vm, vmh, vml, s.Syncing, vms, local, vmi, s.LeaderVMIndex, s.LeaderPL.DBHeight, hkb)
 			msg.FollowerExecute(s)
 		}
-
 		return true
 
 	case 0:
@@ -961,6 +963,7 @@ func (s *State) FollowerExecuteMsg(m interfaces.IMsg) {
 
 		pl := s.ProcessLists.Get(ack.DBHeight)
 		pl.AddToProcessList(s, ack, m)
+		go emitAddToProcessListEvent(m, s)
 
 		// Cross Boot Replay
 		s.CrossReplayAddSalt(ack.DBHeight, ack.Salt)
@@ -1222,13 +1225,7 @@ func (s *State) FollowerExecuteDBState(msg interfaces.IMsg) {
 		s.StatesReceived.Notify <- msg.(*messages.DBStateMsg)
 	}
 	s.DBStates.UpdateState()
-
-	go emitEvent(dbstatemsg, s)
-}
-
-func emitEvent(dbstatemsg *messages.DBStateMsg, state *State) {
-	anchoredEvent := eventMessages.AnchoredEventFromDBState(dbstatemsg)
-	state.EventsProxy.Send(anchoredEvent)
+	go emitDirectoryBlockEvent(dbstatemsg, s)
 }
 
 func (s *State) FollowerExecuteMMR(m interfaces.IMsg) {
@@ -2783,4 +2780,19 @@ func (s *State) NewAck(msg interfaces.IMsg, balanceHash interfaces.IHash) interf
 	ack.SetLocal(true)
 
 	return ack
+}
+
+func emitDirectoryBlockEvent(dbstatemsg *messages.DBStateMsg, state *State) {
+	anchoredEvent := eventMessages.AnchoredEventFromDBState(dbstatemsg)
+	state.EventsProxy.Send(anchoredEvent)
+}
+
+func emitAddToProcessListEvent(msg interfaces.IMsg, state *State) {
+	addToHoldingEvent := eventMessages.AddIntermediateEventFromMessage(eventMessages.IntermediateEvent_ADD_TO_PROCESSLIST, msg)
+	state.EventsProxy.Send(addToHoldingEvent)
+}
+
+func emitAddToHoldingEvent(msg interfaces.IMsg, state *State) {
+	addToHoldingEvent := eventMessages.AddIntermediateEventFromMessage(eventMessages.IntermediateEvent_ADD_TO_HOLDING, msg)
+	state.EventsProxy.Send(addToHoldingEvent)
 }
