71917ff290537ab0c0e0834a187a6a33cbb35f2c
lvandenbrink <lvdbrink@sphereon.com>
2019-07-22 15:47:49 +0200

    FD-450 discard logging when level is none

--- wsapi/log.go
+++ wsapi/log.go
@@ -6,6 +6,7 @@ package wsapi
 
 import (
 	"io"
+	"io/ioutil"
 	"os"
 
 	log "github.com/sirupsen/logrus"
@@ -19,23 +20,30 @@ var (
 
 // NewLogFromConfig outputs logs to a file given by logpath
 func NewLogFromConfig(logPath, logLevel, prefix string) *log.Entry {
-	var logFile io.Writer
-	if logPath == "stdout" {
-		logFile = os.Stdout
+	if logLevel == "none" {
+		// discard logging when level is none
+		logger := log.New()
+		logger.SetOutput(ioutil.Discard)
+		logger.SetLevel(log.PanicLevel) // discard the least amount of logging
+		return logger.WithField("prefix", prefix)
 	} else {
-		logFile, _ = os.OpenFile(logPath, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0660)
-	}
+		var logFile io.Writer
+		if logPath == "stdout" {
+			logFile = os.Stdout
+		} else {
+			logFile, _ = os.OpenFile(logPath, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0660)
+		}
 
-	logger := log.New()
-	logger.SetOutput(logFile)
-	if logLevel != "none" {
+		logger := log.New()
+		logger.SetOutput(logFile)
 		lvl, err := log.ParseLevel(logLevel)
 		if err != nil {
 			panic(err)
 		}
 		logger.SetLevel(lvl)
+
+		return logger.WithField("prefix", prefix)
 	}
-	return logger.WithField("prefix", prefix)
 }
 
 func InitLogs(logPath, logLevel string) {
