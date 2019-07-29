a788e142fe780af197c2251b54ad6d555f2dcd8f
sanderPostma <spostma@sphereon.com>
2019-07-29 13:12:07 +0200

    LiveAPI: EventInput field renames

--- events/eventmessages/input/eventInput.go
+++ events/eventmessages/input/eventInput.go
@@ -8,45 +8,45 @@ import (
 
 type EventInput struct {
 	eventSource    eventmessages.EventSource
-	messagePayload interfaces.IMsg
-	message        string
+	processMessage interfaces.IMsg
+	nodeMessage    string
 }
 
 func (srcEvent *EventInput) GetEventSource() eventmessages.EventSource {
 	return srcEvent.eventSource
 }
 
-func (srcEvent *EventInput) GetMessagePayload() interfaces.IMsg {
-	return srcEvent.messagePayload
+func (srcEvent *EventInput) GetProcessMessage() interfaces.IMsg {
+	return srcEvent.processMessage
 }
 
-func (srcEvent *EventInput) GetMessage() string {
-	return srcEvent.message
+func (srcEvent *EventInput) GetNodeMessage() string {
+	return srcEvent.nodeMessage
 }
 
 func EventFromMessage(eventSource eventmessages.EventSource, msg interfaces.IMsg) *EventInput {
 	return &EventInput{
 		eventSource:    eventSource,
-		messagePayload: msg}
+		processMessage: msg}
 }
 
 func NewInfoEvent(message string) *EventInput {
 	return &EventInput{
 		eventSource: eventmessages.EventSource_NODE_INFO,
-		message:     message,
+		nodeMessage: message,
 	}
 }
 
 func NewInfoEventF(format string, values ...interface{}) *EventInput {
 	return &EventInput{
 		eventSource: eventmessages.EventSource_NODE_INFO,
-		message:     fmt.Sprintf(format, values),
+		nodeMessage: fmt.Sprintf(format, values),
 	}
 }
 
 func NewErrorEvent(message string, error interface{}) *EventInput {
 	return &EventInput{
 		eventSource: eventmessages.EventSource_NODE_ERROR,
-		message:     fmt.Sprint(message, error),
+		nodeMessage: fmt.Sprint(message, error),
 	}
 }
