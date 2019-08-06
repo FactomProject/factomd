2759bb6d33c954190a80f249a9d81697e4002789
sanderPostma <spostma@sphereon.com>
2019-08-06 11:05:32 +0200

    LiveAPI: Completed event mapper unit tests

--- events/eventservices/eventMapper_test.go
+++ events/eventservices/eventMapper_test.go
@@ -1,6 +1,7 @@
 package eventservices_test
 
 import (
+	"fmt"
 	"github.com/FactomProject/factomd/common/interfaces"
 	"github.com/FactomProject/factomd/common/messages"
 	"github.com/FactomProject/factomd/common/primitives"
@@ -12,9 +13,17 @@ import (
 	"testing"
 )
 
+func TestEventMappers(t *testing.T) {
+	t.Run("TestDBStateMapping", TestDBStateMapping)
+	t.Run("TestCommitChainMapping", TestCommitChainMapping)
+	t.Run("TestCommitEntryMapping", TestCommitEntryMapping)
+	t.Run("TestRevealEntryMapping", TestRevealEntryMapping)
+}
+
 func TestDBStateMapping(t *testing.T) {
 	msg := newDBStateMsg()
-	msg.String()
+	data, _ := msg.MarshalBinary()
+	assert.Len(t, data, 2409, msgChangedMessage("DBStateMsg"))
 
 	inputEvent := events.EventFromMessage(eventmessages.EventSource_COMMIT_DIRECTORY_BLOCK, msg)
 	event, err := eventservices.MapToFactomEvent(inputEvent)
@@ -28,6 +37,9 @@ func TestDBStateMapping(t *testing.T) {
 	assertFactoidBlock(t, anchorEvent.FactoidBlock)
 	assertEntryBlocks(t, anchorEvent.EntryBlocks)
 	assertEntryBlockEntries(t, anchorEvent.EntryBlockEntries)
+
+	_, err = anchorEvent.Marshal()
+	assert.Nil(t, err)
 }
 
 func assertFactoidBlock(t *testing.T, factoidBlock *eventmessages.FactoidBlock) {
@@ -104,6 +116,103 @@ func newDBStateMsg() *messages.DBStateMsg {
 	for _, e := range set.Entries {
 		msg.Entries = append(msg.Entries, e)
 	}
-
 	return msg
+}
+
+func TestCommitChainMapping(t *testing.T) {
+	msg := newCommitChainMsg()
+	data, _ := msg.MarshalBinary()
+	assert.Len(t, data, 201, msgChangedMessage("CommitChainMsg"))
+
+	inputEvent := events.EventFromMessage(eventmessages.EventSource_ADD_TO_PROCESSLIST, msg)
+	event, err := eventservices.MapToFactomEvent(inputEvent)
+	if err != nil {
+		t.Error(err)
+	}
+
+	assert.IsType(t, &eventmessages.FactomEvent_CommitChain{}, event.Value)
+	commitChainEvent := event.Value.(*eventmessages.FactomEvent_CommitChain).CommitChain
+	assert.NotNil(t, commitChainEvent.ChainIDHash)
+	assert.NotNil(t, commitChainEvent.EcPubKey)
+	assert.NotNil(t, commitChainEvent.Credits)
+	assert.NotNil(t, commitChainEvent.Sig)
+	assert.NotNil(t, commitChainEvent.Timestamp)
+	assert.True(t, commitChainEvent.Timestamp.Nanos > 0)
+	_, err = commitChainEvent.Marshal()
+	assert.Nil(t, err)
+}
+
+func newCommitChainMsg() *messages.CommitChainMsg {
+	msg := new(messages.CommitChainMsg)
+	eBlock, _ := testHelper.CreateTestEntryBlock(nil)
+	eBlock, _ = testHelper.CreateTestEntryBlock(eBlock) // Create a second entry to make sure we have a time
+	msg.CommitChain = testHelper.NewCommitChain(eBlock)
+	return msg
+}
+
+func TestCommitEntryMapping(t *testing.T) {
+	msg := newCommitEntryMsg()
+	data, _ := msg.MarshalBinary()
+	assert.Len(t, data, 137, msgChangedMessage("CommitEntryMsg"))
+
+	inputEvent := events.EventFromMessage(eventmessages.EventSource_ADD_TO_HOLDING, msg)
+	event, err := eventservices.MapToFactomEvent(inputEvent)
+	if err != nil {
+		t.Error(err)
+	}
+
+	assert.IsType(t, &eventmessages.FactomEvent_CommitEntry{}, event.Value)
+	commitEntryEvent := event.Value.(*eventmessages.FactomEvent_CommitEntry).CommitEntry
+	assert.NotNil(t, commitEntryEvent.EntryHash)
+	assert.NotNil(t, commitEntryEvent.EcPubKey)
+	assert.NotNil(t, commitEntryEvent.Credits)
+	assert.NotNil(t, commitEntryEvent.Sig)
+	assert.NotNil(t, commitEntryEvent.Timestamp)
+	assert.True(t, commitEntryEvent.Timestamp.Nanos > 0)
+
+	_, err = commitEntryEvent.Marshal()
+	assert.Nil(t, err)
+}
+
+func newCommitEntryMsg() *messages.CommitEntryMsg {
+	msg := new(messages.CommitEntryMsg)
+	eBlock, _ := testHelper.CreateTestEntryBlock(nil)
+	eBlock, _ = testHelper.CreateTestEntryBlock(eBlock) // Create a second entry to make sure we have a time
+	msg.CommitEntry = testHelper.NewCommitEntry(eBlock)
+	return msg
+}
+
+func TestRevealEntryMapping(t *testing.T) {
+	msg := newRevealEntryMsg()
+	data, _ := msg.MarshalBinary()
+	assert.Len(t, data, 60, msgChangedMessage("CommitEntryMsg"))
+
+	inputEvent := events.EventFromMessage(eventmessages.EventSource_ADD_TO_PROCESSLIST, msg)
+	event, err := eventservices.MapToFactomEvent(inputEvent)
+	if err != nil {
+		t.Error(err)
+	}
+
+	assert.IsType(t, &eventmessages.FactomEvent_RevealEntry{}, event.Value)
+	revealEntryEvent := event.Value.(*eventmessages.FactomEvent_RevealEntry).RevealEntry
+	assert.NotNil(t, revealEntryEvent.Entry)
+	assert.NotNil(t, revealEntryEvent.Timestamp)
+	assert.True(t, revealEntryEvent.Timestamp.Nanos > 0)
+
+	_, err = revealEntryEvent.Marshal()
+	assert.Nil(t, err)
+}
+
+func newRevealEntryMsg() *messages.RevealEntryMsg {
+	msg := new(messages.RevealEntryMsg)
+	eBlock, _ := testHelper.CreateTestEntryBlock(nil)
+	eBlock, _ = testHelper.CreateTestEntryBlock(eBlock) // Create a second entry to make sure we have a time
+	msg.CommitChain = newCommitChainMsg()
+	msg.Entry = testHelper.CreateTestEntry(eBlock.Header.GetDBHeight())
+	msg.Timestamp = msg.CommitChain.GetTimestamp()
+	return msg
+}
+
+func msgChangedMessage(msgName string) string {
+	return fmt.Sprintf("%s changed, please reevalate properties used by this event and adjust the expected message length.", msgName)
 }
