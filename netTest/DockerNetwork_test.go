ed5a723f7d7e445d8daa4a25b7487e0f8f631af8
stackdump <myork@stackdump.com>
2019-08-20 16:29:50 -0500

    tweak docker netTest

--- netTest/DockerNetwork_test.go
+++ netTest/DockerNetwork_test.go
@@ -12,14 +12,11 @@ func TestDockerNetwork(t *testing.T) {
 	n := SetupNode(DOCKER_NETWORK, 1, t)
 	_ = n
 
-	// KLUDGE: waiting on factomd_0
-	// needs to be  more explicit
 	fmt.Printf("remote nodes: %v", n.fnodes)
 
-	/*
-		n.fnodes[0].RunCmd("R10")
-		n.fnodes[0].WaitBlocks(1)
-		n.fnodes[0].RunCmd("R0")
-	*/
+	n.fnodes[0].RunCmd("R10")
+	n.fnodes[0].WaitBlocks(1)
+	n.fnodes[0].RunCmd("R0")
+
 	//t.Logf("%v", n.fnodes)
 }
