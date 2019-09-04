5e9b951b5853a10a145ef5b5e7ff96405c9d35c8
stackdump <myork@stackdump.com>
2019-09-04 09:34:45 -0500

    WIP

--- simTest/LaggingAuditElection_test.go
+++ simTest/LaggingAuditElection_test.go
@@ -1,43 +1,43 @@
 package simtest
 
 import (
+	"github.com/FactomProject/factomd/engine"
 	. "github.com/FactomProject/factomd/testHelper"
 	"testing"
 )
 
 func LaggingAuditElection(t *testing.T, lag int, recovery int) {
-	state0 := SetupSim("LLLALFL", map[string]string{}, 15, 0, 0, t)
+	state0 := SetupSim("LAFL", map[string]string{ "--blktime": "30"}, 15, 0, 0, t)
+	state1 := engine.GetFnodes()[1].State
+
 	WaitForBlock(state0, 6)
 	WaitForAllNodes(state0)
 
-	RunCmd("3")
-	RunCmd("x")
-	WaitBlocks(state0, lag) // make audit lag behind
+	RunCmd("1")
+	RunCmd("x") // take out audit
 
-	RunCmd("4")
+    WaitBlocks(state0, lag) // make audit lag behind
+
+	RunCmd("0")
 	RunCmd("x") // take out a leader
 
-	// REVIEW: should the be relocated?
-	RunCmd("3")
+	RunCmd("1")
 	RunCmd("x") // bring back audit
 
-	WaitMinutes(state0, 2) // wait for audit to be elected
+	WaitForAllNodes(state1)
 
-	RunCmd("4")
+	WaitBlocks(state1, recovery) // give time to come back
+
+	RunCmd("0")
 	RunCmd("x") // bring back leader-should become Audit
 
-	// Do we need to wait by timeclock? rather than waitblocks
-	WaitBlocks(state0, recovery) // give time to come back
-
-	//WaitForAllNodes(state0) // REVIEW: is this desired? rather than using WaitBlocks as above?
-
-	AssertAuthoritySet(t, "LLLLAFL") // leader
+	AssertAuthoritySet(t, "ALFL")
 	Halt(t)
 }
 
 // test electing an audit that is 1 block behind
 func TestLaggingAuditElection1(t *testing.T) {
-	LaggingAuditElection(t, 1, 3) // KLUDGE: fails when recovery = 2
+	LaggingAuditElection(t, 1, 2) // KLUDGE: fails when recovery = 2
 }
 
 // test electing an audit that is 2 blocks behind
