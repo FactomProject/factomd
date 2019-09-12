b18689909269a1641e75c211307d4e328f309eca
stackdump <myork@stackdump.com>
2019-09-12 13:42:16 -0500

    WIP

--- simTest/factomd_test.go
+++ simTest/factomd_test.go
@@ -592,38 +592,6 @@ func TestSimCtrl(t *testing.T) {
 	ShutDownEverything(t)
 }
 
-func TestMultiple7Election(t *testing.T) {
-	if RanSimTest {
-		return
-	}
-
-	RanSimTest = true
-
-	state0 := SetupSim("LLLLLLLLLFLLFLFLLLFLAAFAAAAFA", map[string]string{"--blktime": "60"}, 10, 7, 7, t)
-
-	WaitForMinute(state0, 2)
-
-	// Take 7 nodes off line
-	for i := 1; i < 8; i++ {
-		RunCmd(fmt.Sprintf("%d", i))
-		RunCmd("x")
-	}
-	// force them all to be faulted
-	WaitMinutes(state0, 1)
-
-	// bring them back online
-	for i := 1; i < 8; i++ {
-		RunCmd(fmt.Sprintf("%d", i))
-		RunCmd("x")
-	}
-
-	// Wait till they should have updated by DBSTATE
-	WaitBlocks(state0, 2)
-	WaitMinutes(state0, 1)
-	WaitForAllNodes(state0)
-	ShutDownEverything(t)
-}
-
 func TestMultipleFTAccountsAPI(t *testing.T) {
 	if RanSimTest {
 		return
