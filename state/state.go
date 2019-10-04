3b777da04b20debe74168ea2770148aea6e84fa2
Paul Snow <snow.paul@gmail.com>
2019-10-04 17:50:44 -0500

    Xuan Fix for syncing

--- state/state.go
+++ state/state.go
@@ -210,6 +210,8 @@ type State struct {
 	DBFinished      bool
 	RunLeader       bool
 	BootTime        int64 // Time in seconds that we last booted
+	EOMIssueTime    int64
+	EOMSyncEnd      int64
 
 	// Ignore missing messages for a period to allow rebooting a network where your
 	// own messages from the previously executing network can confuse you.
@@ -238,7 +240,6 @@ type State struct {
 	CurrentBlockStartTime   int64
 
 	EOMsyncing   bool
-	EOMSyncTime  int64
 	EOM          bool // Set to true when the first EOM is encountered
 	EOMLimit     int
 	EOMProcessed int
