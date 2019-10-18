6f64700bbf792fbd2369bb7fdb3726f282c3fd6d
stackdump <myork@stackdump.com>
2019-10-18 12:01:26 -0500

    add tweak for logger

--- common/messages/messageTrace.go
+++ common/messages/messageTrace.go
@@ -1,6 +1,9 @@
 package messages
 
-import "github.com/FactomProject/factomd/log"
+import (
+	"github.com/FactomProject/factomd/common/interfaces"
+	"github.com/FactomProject/factomd/log"
+)
 
 /*
 KLUDGE: refactor to expose logging methods
@@ -12,3 +15,13 @@ var StateLogMessage = log.StateLogMessage
 var StateLogPrintf = log.StateLogPrintf
 var LogMessage = log.LogMessage
 
+
+type foo func(data []byte) (interfaces.IMsg, error)
+
+var FP func(data []byte) (interfaces.IMsg, error)
+
+// Hack to get around import loop
+func Unmarshal_Message(data []byte) (interfaces.IMsg, error) {
+	msg, err := FP(data)
+	return msg, err
+}
