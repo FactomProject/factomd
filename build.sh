b674989e840c3d8341acb829ae2f3f1934d5f5aa
stackdump <myork@stackdump.com>
2019-10-17 20:41:19 -0500

    add go generate to build command

--- build.sh
+++ build.sh
@@ -1,1 +1,2 @@
+go generate factomd.go
 go install -ldflags "-X github.com/FactomProject/factomd/engine.Build=`git rev-parse HEAD` -X github.com/FactomProject/factomd/engine.FactomdVersion=`cat VERSION`" -v
