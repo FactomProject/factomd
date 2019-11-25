889db648b401199e303ff1337cee5c265ba22bd2
stackdump <myork@stackdump.com>
2019-11-25 12:42:28 -0600

    restore old behavior when setting RequestTimeout

--- state/dbStateCatchup.go
+++ state/dbStateCatchup.go
@@ -43,7 +43,14 @@ func (list *DBStateList) Catchup() {
 
 	factomSecond := list.State.FactomSecond()
 
-	requestTimeout := list.State.RequestTimeout * factomSecond
+	requestTimeout := list.State.RequestTimeout
+	if requestTimeout < 1*time.Second { // If the timeout is 0 (default), base off blktime
+		// 10min block	== 30s timeout for a request.
+		// 5min block	== 15s timeout for a request.
+		// 1min block	== 3s  timeout for a request.
+		requestTimeout = factomSecond * 5
+		list.State.RequestTimeout = requestTimeout
+	}
 	requestLimit := list.State.RequestLimit
 
 	// Wait for db to be loaded
