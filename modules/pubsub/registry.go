454cb75efcb24681e99322138316fe567d7c9aaf
Paul Bernier <bernier.pja@gmail.com>
2020-08-23 21:55:50 -0700

    remove extra line return

--- modules/pubsub/registry.go
+++ modules/pubsub/registry.go
@@ -127,25 +127,25 @@ func globalPublishWith(path string, p IPublisher, wrappers ...IPublisherWrapper)
 	err := globalReg.Register(path, p)
 	if err != nil {
 		tree := globalReg.PrintTree()
-		registryLogger.WithError(err).Errorf("Publish Tree\n%s\n", tree)
+		registryLogger.WithError(err).Errorf("Publish Tree\n%s", tree)
 		panic(fmt.Sprintf("failed to publish: %s", err.Error()))
 	}
 	return p
 }
 
 func globalPublish(path string, p IPublisher) IPublisher {
-	registryLogger.Debugf("globalPublish: %v\n", path)
+	registryLogger.Debugf("globalPublish: %v", path)
 	err := globalReg.Register(path, p)
 	if err != nil {
 		tree := globalReg.PrintTree()
-		registryLogger.WithError(err).Errorf("Publish Tree\n%s\n", tree)
+		registryLogger.WithError(err).Errorf("Publish Tree\n%s", tree)
 		panic(fmt.Sprintf("failed to publish: %s %s", path, err.Error()))
 	}
 	return p
 }
 
 func globalSubscribe(path string, sub IPubSubscriber) IPubSubscriber {
-	registryLogger.Debugf("globalSubscribe: %v\n", path)
+	registryLogger.Debugf("globalSubscribe: %v", path)
 
 	err := globalReg.SubscribeTo(path, sub)
 	if err != nil {
