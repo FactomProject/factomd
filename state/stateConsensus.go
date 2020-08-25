6f5b1c59cf6d5285b19e2631ec1edb8773bcdccd
WhoSoup <who.soup@gmail.com>
2020-08-25 13:55:27 +0200

    build registry before running state

--- state/stateConsensus.go
+++ state/stateConsensus.go
@@ -881,9 +881,7 @@ func (s *State) MoveStateToHeight(dbheight uint32, newMinute int) {
 					DirectoryBlockHeader: dbstate.DirectoryBlock.GetHeader(),
 					Timestamp:            s.GetTimestamp(),
 				}
-				if s.Pub != nil {
-					s.Pub.Directory.Write(&directory)
-				}
+				s.Pub.Directory.Write(&directory)
 				s.LogPrintf("leader", "%v", directory)
 
 			}
@@ -949,9 +947,7 @@ func (s *State) MoveStateToHeight(dbheight uint32, newMinute int) {
 	s.DBSigLimit = s.EOMLimit               // We add or remove server only on block boundaries
 	s.LogPrintf("dbstateprocess", "MoveStateToHeight(%d-:-%d) leader=%v leaderPL=%p, leaderVMIndex=%d", dbheight, newMinute, s.Leader, s.LeaderPL, s.LeaderVMIndex)
 
-	if s.Pub != nil { // FIXME: should coordinate threads to start processing only after publish
-		s.Pub.BlkSeq.Write(&events.DBHT{DBHeight: s.LLeaderHeight, Minute: s.CurrentMinute})
-	}
+	s.Pub.BlkSeq.Write(&events.DBHT{DBHeight: s.LLeaderHeight, Minute: s.CurrentMinute})
 
 	s.Hold.ExecuteForNewHeight(s.LLeaderHeight, s.CurrentMinute) // execute held inMessages
 	s.Hold.Review()                                              // cleanup old inMessages
