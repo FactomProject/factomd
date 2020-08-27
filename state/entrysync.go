b8f564564a7ce852f7ce45173783866e1834c2a9
WhoSoup <who.soup@gmail.com>
2020-08-27 09:02:27 +0200

    commentary

--- state/entrysync.go
+++ state/entrysync.go
@@ -10,13 +10,37 @@ import (
 	"github.com/FactomProject/factomd/database/databaseOverlay"
 )
 
-const (
-	EntrySyncMax     = 750 // good-ish value
-	EntrySyncRetry   = time.Second * 10
+var (
+	// EntrySyncMax is the maximum amount of entries to requeust concurrently
+	EntrySyncMax = 750 // good-ish value
+	// EntrySyncRetry dictates after what period to retry an unanswered request
+	EntrySyncRetry = time.Second * 10
+	// EntrySyncWarning is the number of failed requests before warning in the console.
 	EntrySyncWarning = 16
 )
 
-// TODO comment
+// EntrySync is responsible for sending the requests to fetch missing entries to the network.
+// (aka 2nd pass sync)
+// The general algorithms are as follows:
+//
+// Sync:
+// Start with the last known complete height
+// 1. Load the DBlock and iterate over all EBlock hashes
+// 2. For each EBlock, iterate over the Entry hashes
+// 3. If the DB has the entry, skip, otherwise add the hash to the ask queue
+// 4. Collate all missing entry hashes for each EBlock and add it to eblock queue
+// 5. Inform state we are processing up to that eblock
+//
+// Ask:
+// 1. Look at the first N records of the ask queue
+// 2. If the entry exists, remove it from queue
+// 3. If it's the first time or time threshold passed, send a request to the network
+//
+// Check:
+// 1. Look at the first record of the eblock queue
+// 2. Go through all missing records and check if they exist
+// 3. If there are still missing records, wait and go back to 1
+// 4. If the eblock is finished, inform the state we have all the entries up to that eblock
 type EntrySync struct {
 	s *State
 
@@ -30,17 +54,20 @@ type EntrySync struct {
 	closer chan interface{}
 }
 
+// an item in the ask queue
 type entrySyncAsk struct {
 	hash  interfaces.IHash
 	time  time.Time
 	tries int
 }
 
+// an item in the eblock queue
 type entrySyncEBlock struct {
 	height  uint32
 	missing []interfaces.IHash
 }
 
+// NewEntrySync creates a new EntrySync
 func NewEntrySync(s *State) *EntrySync {
 	es := new(EntrySync)
 	es.s = s
@@ -49,10 +76,12 @@ func NewEntrySync(s *State) *EntrySync {
 	return es
 }
 
+// Stop initiates shutdown and will stop all associated goroutines
 func (es *EntrySync) Stop() {
 	close(es.closer)
 }
 
+// check routine, for description see EntrySync comment
 func (es *EntrySync) check() {
 	for {
 		select {
@@ -90,6 +119,7 @@ func (es *EntrySync) check() {
 	}
 }
 
+// has checks if the hash exists in the database
 func (es *EntrySync) has(hash interfaces.IHash) bool {
 	if has, err := es.s.DB.DoesKeyExist(databaseOverlay.ENTRY, hash.Bytes()); err != nil {
 		panic(err)
@@ -98,6 +128,7 @@ func (es *EntrySync) has(hash interfaces.IHash) bool {
 	}
 }
 
+// ask routine, for description see EntrySync comment
 func (es *EntrySync) ask() {
 	for {
 		select {
@@ -137,6 +168,7 @@ func (es *EntrySync) ask() {
 	}
 }
 
+// returns the maximum possible height we have eblocks for
 func (es *EntrySync) syncMax() uint32 {
 	end := es.s.GetHighestSavedBlk()
 	if es.s.DBStates.ProcessHeight < end {
@@ -145,6 +177,7 @@ func (es *EntrySync) syncMax() uint32 {
 	return end
 }
 
+// SyncHeight starts the Sync, Ask, and Check routines.
 func (es *EntrySync) SyncHeight() {
 	go es.ask()
 	go es.check()
@@ -184,6 +217,7 @@ func (es *EntrySync) SyncHeight() {
 	}
 }
 
+// process a single eblock, will call syncEntryHash() on each entry, and add an eblock to the queue
 func (es *EntrySync) syncEBlock(height uint32, keymr interfaces.IHash, ts interfaces.Timestamp) bool {
 	eblock, err := es.s.DB.FetchEBlock(keymr)
 	if err != nil { // database corrupt
@@ -222,6 +256,7 @@ func (es *EntrySync) syncEBlock(height uint32, keymr interfaces.IHash, ts interf
 	return true
 }
 
+// process a single entry hash. check for duplicate asks, otherwise add to queue
 func (es *EntrySync) syncEntryHash(hash interfaces.IHash) {
 	es.askMtx.Lock()
 	defer es.askMtx.Unlock()
@@ -238,6 +273,9 @@ func (es *EntrySync) syncEntryHash(hash interfaces.IHash) {
 	es.asks = append(es.asks, ask) // add to end of queue
 }
 
+// AskedFor returns true if the entry sync routine asked for a specific hash
+// and that entry has not yet been found in the database.
+// Returns false otherwise.
 func (es *EntrySync) AskedFor(hash interfaces.IHash) bool {
 	es.askMtx.RLock()
 	defer es.askMtx.RUnlock()
