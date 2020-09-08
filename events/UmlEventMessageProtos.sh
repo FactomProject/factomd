2684a99b74f7ffbd348d1611f0dfc337d51db86b
WhoSoup <who.soup@gmail.com>
2020-09-08 13:12:19 +0200

    live feed replaced by module/livefeed

--- events/UmlEventMessageProtos.sh
+++
@@ -1,9 +0,0 @@
-export PLANTUML_LIMIT_SIZE=8192
-export PATH=/home/sander/DEV/go/src/github.com/protoc-gen-uml/target/universal/stage/bin:$PATH
-protoc -I=. -I=$GOPATH/src -I=$GOPATH/src/github.com/gogo/protobuf/protobuf \
---uml_out=./ \
-eventmessages/sharedTypes.proto \
-eventmessages/factoidBlock.proto \
-eventmessages/adminBlock.proto \
-eventmessages/factomEvents.proto
-plantuml ./complete_model.puml
