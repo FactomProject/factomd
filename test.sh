784a64d1765a3e78743afa6aec4e27b26a5d3208
WhoSoup <34172041+WhoSoup@users.noreply.github.com>
2020-10-23 05:49:03 +0200

    remove isCompilerVersionOK check (#1048) (#1080)
    * allow go 1.15
    
    * do not turn off vet for tests
    
    * remove isCompilerVersionOK
    
    Co-authored-by: Paul Bernier <bernier.pja@gmail.com>

--- test.sh
+++ test.sh
@@ -9,7 +9,7 @@ cd $DIR                                                             # always fro
 set -o pipefail
 
 # base go test command
-GO_TEST="go test -v -timeout=10m -vet=off"
+GO_TEST="go test -v -timeout=10m"
 
 # list modules for CI testing
 function listModules() {
