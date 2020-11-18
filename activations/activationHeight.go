7bf68ef5a95bef035b04bf2cfdaf4cd17edfedde
WhoSoup <34172041+WhoSoup@users.noreply.github.com>
2020-11-18 20:40:32 +0100

    Activation Bug Fix + Cleanup (#1111)
    * make code thread safe, fix exports, fix default
    
    * unit test
    
    * remove output
    
    * report inactive activations
    
    * swap default for consistency
    
    * address pr comments

--- activations/activationHeight.go
+++ activations/activationHeight.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"math"
 	"os"
+	"sync"
 
 	"github.com/FactomProject/factomd/common/globals"
 )
@@ -20,34 +21,34 @@ const (
 	ACTIVATION_TYPE_COUNT    = iota - 1 // Always Last
 )
 
-type Activation struct {
-	Name             string
-	Id               ActivationType
-	Description      string
-	DefaultHeight    int            // height of activation on nets not expressly listed (math.MaxInt32 means never)
-	ActivationHeight map[string]int // this maps a network Name to the height for that network for the feature to activate
+type activation struct {
+	name             string
+	id               ActivationType
+	description      string
+	defaultHeight    int            // height of activation on nets not expressly listed (math.MaxInt32 means never)
+	activationHeight map[string]int // this maps a network Name to the height for that network for the feature to activate
 }
 
-var ActivationMap map[ActivationType]Activation
-var ActivationNameMap map[ActivationType]string
+var activationMap map[ActivationType]activation
+var activationNameMap map[ActivationType]string
 
-// init initializes the two global variables ActivationMap and ActivationNameMap above
+// init initializes the two global variables activationMap and activationNameMap above
 func init() {
 
 	// unordered list of activations
-	var activations []Activation = []Activation{
-		Activation{"TestNetCoinBasePeriod", TESTNET_COINBASE_PERIOD,
+	var activations []activation = []activation{
+		{"TestNetCoinBasePeriod", TESTNET_COINBASE_PERIOD,
 			"Change testnet coin base payout delay to 140 blocks",
-			math.MaxInt32, // inactive unless overridden below
+			0, // always active for consistency
 			map[string]int{
 				"MAIN":                      math.MaxInt32,
 				"LOCAL":                     25,
 				"CUSTOM:fct_community_test": 45335, //  Monday morning September 17
 			},
 		},
-		Activation{"AuthorityMaxDelta", AUTHRORITY_SET_MAX_DELTA,
+		{"AuthorityMaxDelta", AUTHRORITY_SET_MAX_DELTA,
 			"Ensures fewer than half of federated notes are replaced in a single election",
-			math.MaxInt32, // inactive unless overridden below
+			0, // always active for consistency
 			map[string]int{
 				"MAIN":                      222874,
 				"LOCAL":                     25,
@@ -61,61 +62,57 @@ func init() {
 		panic("ACTIVATION_TYPE_COUNT does not match the list of Activations")
 	}
 
-	ActivationMap = make(map[ActivationType]Activation, len(activations))
-	ActivationNameMap = make(map[ActivationType]string, len(activations))
+	activationMap = make(map[ActivationType]activation, len(activations))
+	activationNameMap = make(map[ActivationType]string, len(activations))
 	for _, a := range activations {
-		ActivationMap[a.Id] = a
-		ActivationNameMap[a.Id] = a.Name
+		activationMap[a.id] = a
+		activationNameMap[a.id] = a.name
 	}
 }
 
 // String converts an Activation ID to a name
 func (id ActivationType) String() string {
-
-	n, ok := ActivationNameMap[id]
-	if !ok {
-		//n = fmt.Sprintf("ActivationId(%v)", id)
+	if n, ok := activationNameMap[id]; ok {
+		return n
 	}
-	return n
+	return fmt.Sprintf("ActivationId(%d)", id)
 }
 
-var once bool
+var netNameOnce sync.Once
 var netName string
 
 // networkname returns the network name specified by the user at startup with the factomd '-network' option
 // see constants.go for options: MAIN / LOCAL / TEST / CUSTOM
 func networkname() string {
-	if !once {
-		once = true
+	netNameOnce.Do(func() {
 		netName = globals.Params.NetworkName
 		if netName == "CUSTOM" {
-			netName = fmt.Sprintf("%s:%s", netName, globals.Params.CustomNetName)
+			netName = fmt.Sprintf("CUSTOM:%s", globals.Params.CustomNetName)
 		}
 		fmt.Printf("Using NetworkName \"%s\"\n", netName)
-	}
+	})
 	return netName
 }
 
 // IsActive returns whether the input activation is 'on' at the input height
 func IsActive(id ActivationType, height int) bool {
 	netName := networkname()
-	a, ok := ActivationMap[id]
+	a, ok := activationMap[id]
 
 	if !ok {
-		fmt.Fprintf(os.Stderr, "Invalid %v (%s)\n", id, id.String())
+		fmt.Fprintf(os.Stderr, "Invalid %d (%s)\n", id, id.String())
 		return false
 	}
 
-	h, ok := a.ActivationHeight[netName]
-	if !ok {
-		a.ActivationHeight[netName] = a.DefaultHeight
-		if a.DefaultHeight != math.MaxInt32 {
-			fmt.Fprintf(os.Stderr, "Activation %s does not know network name \"%s\". Activating at %d.\n", id.String(), netName, a.DefaultHeight)
-		} else {
-			fmt.Fprintf(os.Stderr, "Activation %s does not know network name \"%s\". Never activating.\n", id.String(), netName)
-		}
-		return true
+	// has a custom entry
+	if h, ok := a.activationHeight[netName]; ok {
+		return height >= h
 	}
 
-	return height >= h
+	// use default
+	if a.defaultHeight < math.MaxInt32 {
+		return height >= a.defaultHeight
+	}
+
+	return false
 }
