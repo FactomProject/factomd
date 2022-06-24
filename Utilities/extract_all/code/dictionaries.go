bcba93881e8a2e721c016c05c76be355a8ac9737
Paul Snow <snow.paul@gmail.com>
2022-06-24 00:31:27 -0500

    Feat: (snapshot) added ability to stop and resume. Fixed a bug where files of single blocks were made at the end.

--- Utilities/extract_all/code/dictionaries.go
+++ Utilities/extract_all/code/dictionaries.go
@@ -1,6 +1,7 @@
 package code
 
 import (
+	"errors"
 	"fmt"
 	"os"
 	"path"
@@ -30,39 +31,36 @@ func ProcessDictionaries() {
 	Start = time.Now()
 
 	DB = FactomdState.GetDB()
-	currentDBHeight := FactomdState.GetDBHeightComplete()
+	currentDBHeight := FactomdState.GetHighestSavedBlk()
 	fmt.Println("Height Complete ", currentDBHeight)
 
-	fmt.Println("Factoid Addresses:      ", FCTAccountCnt)
+	fmt.Println("Factoid Addresses:      ", FCTAccountCnt) // Add Totals
 	fmt.Println("Entry Credit Addresses: ", ECAccountCnt)
 
 	DBHeight := uint32(0)
 
-	for ; ; DBHeight++ {
-		for i := 0; DBHeight == currentDBHeight; i++ {
-			if i == 0 {
-				fmt.Println("waiting")
-			}
-			time.Sleep(1 * time.Second)
-			currentDBHeight = FactomdState.GetDBHeightComplete()
+	DBHeight = Open(DBHeight) // Finds where we were and starts there.
+	StartHeight := DBHeight
+
+	for {
+		currentDBHeight = FactomdState.GetHighestSavedBlk()
+		for i := uint32(0); DBHeight > currentDBHeight; i++ {
+			fmt.Printf("Highest block: %d Want to process block: %d minute %d\n", currentDBHeight, DBHeight, i+1)
+			time.Sleep(60 * time.Second)
+			currentDBHeight = FactomdState.GetHighestSavedBlk()
 		}
 		// Do any needed output file updating, and
 		// console feedback
-		if DBHeight%DBlockFreq == 0 || currentDBHeight-DBHeight < 2 {
+		if DBHeight != StartHeight && (DBHeight%DBlockFreq == 0) {
 
-			// Split up output files by Directory Block Height
-			filename := fmt.Sprintf("objects-%d.dat", DBHeight)
-			if f, err := os.Create(path.Join(FullDir, filename)); err != nil {
-				panic(fmt.Sprintf("Could not open %s: %v", path.Join(FullDir, filename), err))
-			} else {
-				OutputFile = f
-			}
+			Open(DBHeight)
+
 			if DBHeight > 0 {
 				runtime := time.Now().Sub(Start).Seconds()
 				seconds := int(runtime) % 60
 				minutes := int(runtime) / 60
-				bps := float64(DBHeight) / float64(runtime)    // blocks per second
-				stg := float64(currentDBHeight-DBHeight) / bps // seconds to go
+				bps := float64(DBHeight-StartHeight) / float64(runtime) // blocks per second
+				stg := float64(currentDBHeight-DBHeight) / bps          // seconds to go
 				ToGoseconds := int(stg) % 60
 				ToGominutes := int(stg) / 60
 				fmt.Printf("%5d:%02d ETA: %5d:%02d blocks/sec: %5d Height %7d/%d Entries %9d TXs %7d\n",
@@ -74,6 +72,7 @@ func ProcessDictionaries() {
 				)
 			}
 		}
+
 		DBlock, err := DB.FetchDBlockByHeight(DBHeight)
 		if err != nil {
 			panic("Bad DBHeight")
@@ -84,6 +83,37 @@ func ProcessDictionaries() {
 		ProcessECBlock(DBHeight)
 		ProcessFactoids(DBHeight)
 		ProcessEntries(DBlock)
+
+		DBHeight++
+	}
+}
+
+func Open(dbheight uint32) uint32 {
+	inc := 0
+	for {
+		// Split up output files by Directory Block Height
+		filename := fmt.Sprintf("objects-%d.dat", dbheight)
+		filename = path.Join(FullDir, filename)
+		_, err := os.Stat(filename)
+		if errors.Is(err, os.ErrNotExist) {
+			if dbheight > DBlockFreq && inc > 1 { // If we are catching up to a prior effort
+				dbheight -= DBlockFreq
+				filename = fmt.Sprintf("objects-%d.dat", dbheight)
+				filename = path.Join(FullDir, filename)
+				os.Remove(filename)
+			}
+			// Note that we create and overwrite the last file (which could be incomplete) if
+			// it exists.
+			if f, err := os.Create(filename); err != nil {
+				panic(fmt.Sprintf("Could not open %s: %v", path.Join(FullDir, filename), err))
+			} else {
+				OutputFile.Close()
+				OutputFile = f
+			}
+			return dbheight
+		}
+		dbheight += DBlockFreq
+		inc++
 	}
 }
 
@@ -114,6 +144,18 @@ func ProcessAdmin(dbheight uint32) {
 }
 
 func ProcessECBlock(dbheight uint32) {
+
+	switch {
+	case dbheight >= 70387 && dbheight <= 70410:
+		return
+	}
+
+	defer func() {
+		if e := recover(); e != nil {
+			fmt.Println("==================Error ECBlock", dbheight)
+		}
+	}()
+
 	ECBlock, err := DB.FetchECBlockByHeight(dbheight)
 	if err != nil {
 		panic("Bad Entry Credit Block")
--- Utilities/extract_all/main.go
+++ Utilities/extract_all/main.go
@@ -18,12 +18,11 @@ func main() {
 	// Make sure we have the output directory
 	u, _ := user.Current()
 	code.FullDir = path.Join(u.HomeDir + code.OutputDir)
-	os.RemoveAll(code.FullDir)
 	if err := os.MkdirAll(code.FullDir, os.ModePerm); err != nil {
 		fmt.Println(err)
 	}
 
-	args := []string{"--enablenet=false"} // Enable or disable the network
+	args := []string{"--enablenet=true"} // Enable or disable the network
 	params := engine.ParseCmdLine(args)
 	params.PrettyPrint()
 	code.FactomdState = engine.Factomd(params)
@@ -35,6 +34,7 @@ func main() {
 		for code.FactomdState.GetRunState() != runstate.Stopped {
 			time.Sleep(time.Second)
 		}
+		code.ProcessBalances()
 		fmt.Println("Waiting to Shut Down") // This may not be necessary anymore with the new run state method
 		time.Sleep(time.Second * 5)
 	}()
